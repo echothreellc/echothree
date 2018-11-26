e6694193cd81675ca689d28312be8c795d1a3ab2
Richard Harms <rich@echothree.com>
2018-11-26 10:22:31 -0500

    E3-2444: Additional changes renaming  GraphQLRequest -> GraphQlRequest

--- service/graphql/src/java/com/echothree/service/graphql/GraphQlRequest.java
+++ service/graphql/src/java/com/echothree/service/graphql/GraphQlRequest.java
@@ -1,5 +1,57 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2018 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
 package com.echothree.service.graphql;
 
-public class GraphQLRequest {
+public class GraphQlRequest {
 
+    private String query;
+    private String operationName;
+    private String json;
+
+    public GraphQlRequest() {
+    }
+
+    public GraphQlRequest(String query, String operationName, String json) {
+        this.setQuery(query);
+        this.setOperationName(operationName);
+        this.setJson(json);
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getOperationName() {
+        return operationName;
+    }
+
+    public void setOperationName(String operationName) {
+        this.operationName = operationName;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
 }
