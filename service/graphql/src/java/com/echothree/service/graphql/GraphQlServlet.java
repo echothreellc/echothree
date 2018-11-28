780b2e04b595019517921457243d61f956b1eaa8
Richard Harms <rich@echothree.com>
2018-11-27 23:09:37 -0500

    E3-2444: Trivial code clean-up

--- service/graphql/src/java/com/echothree/service/graphql/GraphQlServlet.java
+++ service/graphql/src/java/com/echothree/service/graphql/GraphQlServlet.java
@@ -39,7 +39,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
