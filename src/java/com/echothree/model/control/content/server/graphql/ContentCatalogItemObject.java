ebe9151d1daf2e58c593f090bc255587bc1c91da
Richard Harms <rich@echothree.com>
2019-01-22 22:23:04 -0600

    E3-2475: Added price-related fields to ContentCatalogItem object

--- src/java/com/echothree/model/control/content/server/graphql/ContentCatalogItemObject.java
+++ src/java/com/echothree/model/control/content/server/graphql/ContentCatalogItemObject.java
@@ -22,13 +22,19 @@ import com.echothree.control.user.inventory.server.command.GetInventoryCondition
 import com.echothree.control.user.item.server.command.GetItemCommand;
 import com.echothree.control.user.uom.server.command.GetUnitOfMeasureTypeCommand;
 import com.echothree.model.control.accounting.server.graphql.CurrencyObject;
+import com.echothree.model.control.content.server.ContentControl;
 import com.echothree.model.control.graphql.server.graphql.BaseEntityInstanceObject;
 import com.echothree.model.control.graphql.server.util.GraphQlContext;
 import com.echothree.model.control.inventory.server.graphql.InventoryConditionObject;
+import com.echothree.model.control.item.common.ItemPriceTypes;
 import com.echothree.model.control.item.server.graphql.ItemObject;
 import com.echothree.model.control.uom.server.graphql.UnitOfMeasureTypeObject;
 import com.echothree.model.data.content.server.entity.ContentCatalogItem;
+import com.echothree.model.data.content.server.entity.ContentCatalogItemFixedPrice;
+import com.echothree.model.data.content.server.entity.ContentCatalogItemVariablePrice;
+import com.echothree.util.server.string.AmountUtils;
 import com.echothree.util.server.control.BaseSingleEntityCommand;
+import com.echothree.util.server.persistence.Session;
 import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
@@ -45,6 +51,44 @@ public class ContentCatalogItemObject
         super(contentCatalogItem.getPrimaryKey());
         
         this.contentCatalogItem = contentCatalogItem;
+    }
+
+    String itemPriceTypeName;
+
+    private String getItemPriceTypeName() {
+        if(itemPriceTypeName == null) {
+            itemPriceTypeName = contentCatalogItem.getItem().getLastDetail().getItemPriceType().getItemPriceTypeName();
+        }
+
+        return itemPriceTypeName;
+    }
+
+    ContentCatalogItemFixedPrice contentCatalogItemFixedPrice;
+
+    private ContentCatalogItemFixedPrice getContentCatalogItemFixedPrice() {
+        if(contentCatalogItemFixedPrice == null) {
+            if(getItemPriceTypeName().equals(ItemPriceTypes.FIXED.name())) {
+                ContentControl contentControl = (ContentControl)Session.getModelController(ContentControl.class);
+
+                contentCatalogItemFixedPrice = contentControl.getContentCatalogItemFixedPrice(contentCatalogItem);
+            }
+        }
+
+        return contentCatalogItemFixedPrice;
+    }
+
+    ContentCatalogItemVariablePrice contentCatalogItemVariablePrice;
+
+    private ContentCatalogItemVariablePrice getContentCatalogItemVariablePrice() {
+        if(contentCatalogItemVariablePrice == null) {
+            if(getItemPriceTypeName().equals(ItemPriceTypes.VARIABLE.name())) {
+                ContentControl contentControl = (ContentControl)Session.getModelController(ContentControl.class);
+
+                contentCatalogItemVariablePrice = contentControl.getContentCatalogItemVariablePrice(contentCatalogItem);
+            }
+        }
+
+        return contentCatalogItemVariablePrice;
     }
 
     private Boolean hasContentCatalogAccess;
@@ -150,6 +194,74 @@ public class ContentCatalogItemObject
     @GraphQLDescription("currency")
     public CurrencyObject getCurrency(final DataFetchingEnvironment env) {
         return getHasCurrencyAccess(env) ? new CurrencyObject(contentCatalogItem.getCurrency()) : null;
+    }
+
+    @GraphQLField
+    @GraphQLDescription("unformattedUnitPrice")
+    public Long getUnformattedUnitPrice(final DataFetchingEnvironment env) {
+        ContentCatalogItemFixedPrice contentCatalogItemFixedPrice = getContentCatalogItemFixedPrice();
+
+        return contentCatalogItemFixedPrice == null ? null : contentCatalogItemFixedPrice.getUnitPrice();
+    }
+
+    @GraphQLField
+    @GraphQLDescription("unitPrice")
+    public String getUnitPrice(final DataFetchingEnvironment env) {
+        ContentCatalogItemFixedPrice contentCatalogItemFixedPrice = getContentCatalogItemFixedPrice();
+
+        return contentCatalogItemFixedPrice == null ? null :
+                AmountUtils.getInstance().formatPriceUnit(contentCatalogItem.getCurrency(), contentCatalogItemFixedPrice.getUnitPrice());
+    }
+
+    @GraphQLField
+    @GraphQLDescription("unformattedMinimumUnitPrice")
+    public Long getUnformattedMinimumUnitPrice(final DataFetchingEnvironment env) {
+        ContentCatalogItemVariablePrice contentCatalogItemVariablePrice = getContentCatalogItemVariablePrice();
+
+        return contentCatalogItemVariablePrice == null ? null : contentCatalogItemVariablePrice.getMinimumUnitPrice();
+    }
+
+    @GraphQLField
+    @GraphQLDescription("minimumUnitPrice")
+    public String getMinimumUnitPrice(final DataFetchingEnvironment env) {
+        ContentCatalogItemVariablePrice contentCatalogItemVariablePrice = getContentCatalogItemVariablePrice();
+
+        return contentCatalogItemVariablePrice == null ? null :
+                AmountUtils.getInstance().formatPriceUnit(contentCatalogItem.getCurrency(), contentCatalogItemVariablePrice.getMinimumUnitPrice());
+    }
+
+    @GraphQLField
+    @GraphQLDescription("unformattedMaximumUnitPrice")
+    public Long getUnformattedMaximumUnitPrice(final DataFetchingEnvironment env) {
+        ContentCatalogItemVariablePrice contentCatalogItemVariablePrice = getContentCatalogItemVariablePrice();
+
+        return contentCatalogItemVariablePrice == null ? null : contentCatalogItemVariablePrice.getMaximumUnitPrice();
+    }
+
+    @GraphQLField
+    @GraphQLDescription("maximumUnitPrice")
+    public String getMaximumUnitPrice(final DataFetchingEnvironment env) {
+        ContentCatalogItemVariablePrice contentCatalogItemVariablePrice = getContentCatalogItemVariablePrice();
+
+        return contentCatalogItemVariablePrice == null ? null :
+                AmountUtils.getInstance().formatPriceUnit(contentCatalogItem.getCurrency(), contentCatalogItemVariablePrice.getMaximumUnitPrice());
+    }
+
+    @GraphQLField
+    @GraphQLDescription("unformattedUnitPriceIncrement")
+    public Long getUnformattedUnitPriceIncrement(final DataFetchingEnvironment env) {
+        ContentCatalogItemVariablePrice contentCatalogItemVariablePrice = getContentCatalogItemVariablePrice();
+
+        return contentCatalogItemVariablePrice == null ? null : contentCatalogItemVariablePrice.getUnitPriceIncrement();
+    }
+
+    @GraphQLField
+    @GraphQLDescription("unitPriceIncrement")
+    public String getUnitPriceIncrement(final DataFetchingEnvironment env) {
+        ContentCatalogItemVariablePrice contentCatalogItemVariablePrice = getContentCatalogItemVariablePrice();
+
+        return contentCatalogItemVariablePrice == null ? null :
+                AmountUtils.getInstance().formatPriceUnit(contentCatalogItem.getCurrency(), contentCatalogItemVariablePrice.getUnitPriceIncrement());
     }
 
 }
