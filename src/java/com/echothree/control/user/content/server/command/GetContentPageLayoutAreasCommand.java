7ed6db8a80822cb0d8fcc4dde77e92ec38bcaeb0
Richard Harms <rich@echothree.com>
2019-02-24 10:09:38 -0600

    E3-2475: Added comment about lack of security restrictions for GetContentPageLayoutArea and GetContentPageLayoutAreas UCs

--- src/java/com/echothree/control/user/content/server/command/GetContentPageLayoutAreasCommand.java
+++ src/java/com/echothree/control/user/content/server/command/GetContentPageLayoutAreasCommand.java
@@ -39,6 +39,7 @@ import java.util.List;
 public class GetContentPageLayoutAreasCommand
         extends BaseMultipleEntitiesCommand<ContentPageLayoutArea, GetContentPageLayoutAreasForm> {
     
+    // No COMMAND_SECURITY_DEFINITION, anyone may execute this command.
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
     
     static {
