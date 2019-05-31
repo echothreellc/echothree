548d2d324b2a6f5192289f821efaab97f5c2e2cf
Richard Harms <rich@echothree.com>
2019-05-31 18:09:37 -0500

    E3-2494: Added Customer Web Address feature

---
+++ test/cucumber/src/feature/CustomerWebAddress.feature
@@ -0,0 +1,21 @@
+Feature: Customer web address
+  A customer wants to add and delete web addresses associated with their account
+
+  Background:
+    Given Test is not currently logged in
+    When the customer Test logs in with the username "TestC@echothree.com" and password "password"
+    Then no error should occur
+
+  Scenario: Existing customer adds and then deletes an web address without a description
+    Given Test is currently logged in
+    And the customer Test adds the web address "http://www.echothree.com/"
+    Then no error should occur
+    And the customer Test deletes the last web address added
+    Then no error should occur
+
+  Scenario: Existing customer adds and then deletes an web address with a description
+    Given Test is currently logged in
+    And the customer Test adds the web address "http://www.echothree.com/" with the description "Additional Web Address"
+    Then no error should occur
+    And the customer Test deletes the last web address added
+    Then no error should occur
---
+++ test/cucumber/src/java/com/echothree/cucumber/contact/CustomerWebAddress.java
@@ -0,0 +1,75 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2019 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.cucumber.contact;
+
+import com.echothree.control.user.contact.common.ContactUtil;
+import com.echothree.control.user.contact.common.result.CreateContactWebAddressResult;
+import com.echothree.cucumber.CustomerPersonas;
+import cucumber.api.java.en.When;
+
+import javax.naming.NamingException;
+
+public class CustomerWebAddress {
+
+    @When("^the customer ([^\"]*) adds the web address \"([^\"]*)\" with the description \"([^\"]*)\"$")
+    public void theCustomerAddsTheWebAddress(String persona, String url, String description)
+            throws NamingException {
+        var contactService = ContactUtil.getHome();
+        var createContactWebAddressForm = contactService.getCreateContactWebAddressForm();
+        var customerPersona = CustomerPersonas.getCustomerPersona(persona);
+
+        createContactWebAddressForm.setUrl(url);
+        createContactWebAddressForm.setDescription(description);
+
+        var commandResult = contactService.createContactWebAddress(customerPersona.userVisitPK, createContactWebAddressForm);
+
+        customerPersona.commandResult = commandResult;
+        var result = (CreateContactWebAddressResult)commandResult.getExecutionResult().getResult();
+
+        customerPersona.lastWebAddressContactMechanismName = result.getContactMechanismName();
+    }
+
+    @When("^the customer ([^\"]*) adds the web address \"([^\"]*)\"$")
+    public void theCustomerAddsTheWebAddress(String persona, String url)
+            throws NamingException {
+        var contactService = ContactUtil.getHome();
+        var createContactWebAddressForm = contactService.getCreateContactWebAddressForm();
+        var customerPersona = CustomerPersonas.getCustomerPersona(persona);
+
+        createContactWebAddressForm.setUrl(url);
+
+        var commandResult = contactService.createContactWebAddress(customerPersona.userVisitPK, createContactWebAddressForm);
+
+        customerPersona.commandResult = commandResult;
+        var result = (CreateContactWebAddressResult)commandResult.getExecutionResult().getResult();
+
+        customerPersona.lastWebAddressContactMechanismName = result.getContactMechanismName();
+    }
+
+    @When("^the customer ([^\"]*) deletes the last web address added$")
+    public void theCustomerDeletesTheLastWebAddress(String persona)
+            throws NamingException {
+        var contactService = ContactUtil.getHome();
+        var deleteContactWebAddressForm = contactService.getDeleteContactMechanismForm();
+        var customerPersona = CustomerPersonas.getCustomerPersona(persona);
+
+        deleteContactWebAddressForm.setContactMechanismName(customerPersona.lastWebAddressContactMechanismName);
+
+        customerPersona.commandResult = contactService.deleteContactMechanism(customerPersona.userVisitPK, deleteContactWebAddressForm);
+    }
+
+}
