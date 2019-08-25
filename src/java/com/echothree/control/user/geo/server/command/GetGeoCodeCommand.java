f7cc81098cd147818cbd63ce51ff1063bda38623
Richard Harms <rich@echothree.com>
2019-08-24 22:29:07 -0500

    E3-2509: Converted GetCodeCode UC to use BaseSingleEntityCommand; allow CUSTOMERs and VENDORs to use GetCodeCode UC

--- src/java/com/echothree/control/user/geo/server/command/GetGeoCodeCommand.java
+++ src/java/com/echothree/control/user/geo/server/command/GetGeoCodeCommand.java
@@ -18,18 +18,17 @@ package com.echothree.control.user.geo.server.command;
 
 import com.echothree.control.user.geo.common.form.GetGeoCodeForm;
 import com.echothree.control.user.geo.common.result.GeoResultFactory;
-import com.echothree.control.user.geo.common.result.GetGeoCodeResult;
 import com.echothree.model.control.geo.server.GeoControl;
+import com.echothree.model.control.geo.server.logic.GeoCodeLogic;
 import com.echothree.model.control.party.common.PartyConstants;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.data.geo.server.entity.GeoCode;
 import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.message.ExecutionErrors;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
@@ -39,7 +38,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class GetGeoCodeCommand
-        extends BaseSimpleCommand<GetGeoCodeForm> {
+        extends BaseSingleEntityCommand<GeoCode, GetGeoCodeForm> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -47,6 +46,8 @@ public class GetGeoCodeCommand
     static {
         COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(Collections.unmodifiableList(Arrays.asList(
                 new PartyTypeDefinition(PartyConstants.PartyType_UTILITY, null),
+                new PartyTypeDefinition(PartyConstants.PartyType_CUSTOMER, null),
+                new PartyTypeDefinition(PartyConstants.PartyType_VENDOR, null),
                 new PartyTypeDefinition(PartyConstants.PartyType_EMPLOYEE, Collections.unmodifiableList(Arrays.asList(
                         new SecurityRoleDefinition(SecurityRoleGroups.GeoCode.name(), SecurityRoles.Review.name())
                         )))
@@ -61,21 +62,23 @@ public class GetGeoCodeCommand
     public GetGeoCodeCommand(UserVisitPK userVisitPK, GetGeoCodeForm form) {
         super(userVisitPK, form, COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
-    @Override
-    protected BaseResult execute() {
-        var geoControl = (GeoControl)Session.getModelController(GeoControl.class);
-        GetGeoCodeResult result = GeoResultFactory.getGetGeoCodeResult();
-        String geoCodeName = form.getGeoCodeName();
-        GeoCode geoCode = geoControl.getGeoCodeByName(geoCodeName);
 
-        if(geoCode != null) {
-            result.setGeoCode(geoControl.getGeoCodeTransfer(getUserVisit(), geoCode));
-        } else {
-            addExecutionError(ExecutionErrors.UnknownGeoCodeName.name(), geoCodeName);
+    @Override
+    protected GeoCode getEntity() {
+        return GeoCodeLogic.getInstance().getGeoCodeByName(this, form.getGeoCodeName());
+    }
+
+    @Override
+    protected BaseResult getTransfer(GeoCode entity) {
+        var result = GeoResultFactory.getGetGeoCodeResult();
+
+        if(entity != null) {
+            var geoControl = (GeoControl)Session.getModelController(GeoControl.class);
+
+            result.setGeoCode(geoControl.getGeoCodeTransfer(getUserVisit(), entity));
         }
-        
+
         return result;
     }
-    
+
 }
