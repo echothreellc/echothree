faa22d0268c2d43c89795c4520af40044dbc4d25
Richard Harms <rich@echothree.com>
2019-11-19 23:13:44 -0500

    E3-633: Added EmployeePersona

---
+++ test/cucumber/src/java/com/echothree/cucumber/EmployeePersona.java
@@ -0,0 +1,34 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2019 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.cucumber;
+
+import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.util.common.command.CommandResult;
+
+public class EmployeePersona {
+    public String persona;
+    public UserVisitPK userVisitPK;
+    public CommandResult commandResult;
+
+    public String lastEmailAddressContactMechanismName;
+    public String lastPostalAddressContactMechanismName;
+    public String lastTelephoneContactMechanismName;
+    public String lastWebAddressContactMechanismName;
+
+    public String lastSalesOrderName;
+    public String lastSalesOrderLineSequence;
+}
