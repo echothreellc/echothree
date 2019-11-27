6f929bbbb2c7cd28285503fd1946f2c7b80f5aea
Richard Harms <rich@echothree.com>
2019-11-26 21:21:22 -0500

    E3-633: Corrected always null error in AssociateReferralLogic.handleAssociateReferral(...)

--- src/java/com/echothree/model/control/associate/server/logic/AssociateReferralLogic.java
+++ src/java/com/echothree/model/control/associate/server/logic/AssociateReferralLogic.java
@@ -19,9 +19,6 @@ package com.echothree.model.control.associate.server.logic;
 import com.echothree.control.user.associate.common.spec.AssociatePartyContactMechanismSpec;
 import com.echothree.model.control.associate.server.AssociateControl;
 import com.echothree.model.control.core.server.CoreControl;
-import com.echothree.model.data.associate.server.entity.Associate;
-import com.echothree.model.data.associate.server.entity.AssociatePartyContactMechanism;
-import com.echothree.model.data.associate.server.entity.AssociateProgram;
 import com.echothree.model.data.associate.server.entity.AssociateReferral;
 import com.echothree.model.data.user.server.entity.UserVisit;
 import com.echothree.util.common.message.ExecutionErrors;
@@ -35,50 +32,46 @@ public class AssociateReferralLogic {
         super();
     }
 
-    private static class AssociateReferralLogicHolder {
-        static AssociateReferralLogic instance = new AssociateReferralLogic();
-    }
-
     public static AssociateReferralLogic getInstance() {
         return AssociateReferralLogicHolder.instance;
     }
-    
+
     public void handleAssociateReferral(final Session session, final ExecutionErrorAccumulator eea, final AssociatePartyContactMechanismSpec spec,
             final UserVisit userVisit, final BasePK targetPK, final BasePK partyPK) {
-        AssociateReferral associateReferral = null;
-        String associateName = spec.getAssociateName();
-        
+        var associateName = spec.getAssociateName();
+        AssociateReferral associateReferral;
+
         if(associateName != null) {
             var associateControl = (AssociateControl)Session.getModelController(AssociateControl.class);
-            String associateProgramName = spec.getAssociateProgramName();
-            AssociateProgram associateProgram = associateProgramName == null? associateControl.getDefaultAssociateProgram():
+            var associateProgramName = spec.getAssociateProgramName();
+            var associateProgram = associateProgramName == null ? associateControl.getDefaultAssociateProgram() :
                 associateControl.getAssociateProgramByName(associateProgramName);
-            
+
             if(associateProgram != null) {
-                Associate associate = associateControl.getAssociateByName(associateProgram, associateName);
-                
+                var associate = associateControl.getAssociateByName(associateProgram, associateName);
+
                 if(associate != null) {
-                    String associatePartyContactMechanismName = spec.getAssociatePartyContactMechanismName();
-                    AssociatePartyContactMechanism associatePartyContactMechanism = associatePartyContactMechanismName == null?
-                        associateControl.getDefaultAssociatePartyContactMechanism(associate):
+                    var associatePartyContactMechanismName = spec.getAssociatePartyContactMechanismName();
+                    var associatePartyContactMechanism = associatePartyContactMechanismName == null ?
+                        associateControl.getDefaultAssociatePartyContactMechanism(associate) :
                         associateControl.getAssociatePartyContactMechanismByName(associate, associatePartyContactMechanismName);
-                    
+
                     if(associatePartyContactMechanismName != null && associatePartyContactMechanism == null) {
                         eea.addExecutionError(ExecutionErrors.UnknownAssociatePartyContactMechanismName.name(), associateProgramName, associateName,
                                 associatePartyContactMechanismName);
                     } else {
                         var coreControl = (CoreControl)Session.getModelController(CoreControl.class);
-                        
+
                         associateReferral = associateControl.createAssociateReferral(associate, associatePartyContactMechanism,
                                 coreControl.getEntityInstanceByBasePK(targetPK), session.START_TIME_LONG, partyPK);
-                        
+
                         userVisit.setAssociateReferral(associateReferral);
                     }
                 } else {
                     eea.addExecutionError(ExecutionErrors.UnknownAssociateName.name(), associateProgramName, associateName);
                 }
             } else {
-                if(associateProgramName == null) {
+                if(associateProgramName != null) {
                     eea.addExecutionError(ExecutionErrors.UnknownAssociateProgramName.name(), associateProgramName);
                 } else {
                     eea.addExecutionError(ExecutionErrors.MissingDefaultAssociateProgram.name());
@@ -86,5 +79,9 @@ public class AssociateReferralLogic {
             }
         }
     }
-    
+
+    private static class AssociateReferralLogicHolder {
+        static AssociateReferralLogic instance = new AssociateReferralLogic();
+    }
+
 }
