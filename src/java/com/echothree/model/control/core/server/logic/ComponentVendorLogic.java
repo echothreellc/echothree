2499959cad71b215937d8bfdbab3ab2cdce1cc43
Richard Harms <rich@echothree.com>
2020-01-16 21:44:59 -0600

    E3-2249: Additional methods for querying ComponentVendors

--- src/java/com/echothree/model/control/core/server/logic/ComponentVendorLogic.java
+++ src/java/com/echothree/model/control/core/server/logic/ComponentVendorLogic.java
@@ -16,12 +16,18 @@
 
 package com.echothree.model.control.core.server.logic;
 
+import com.echothree.control.user.core.common.spec.ComponentVendorUniversalSpec;
+import com.echothree.model.control.core.common.ComponentVendors;
+import com.echothree.model.control.core.common.EntityTypes;
+import com.echothree.model.control.core.common.exception.InvalidParameterCountException;
 import com.echothree.model.control.core.common.exception.UnknownComponentVendorNameException;
 import com.echothree.model.control.core.server.CoreControl;
 import com.echothree.model.data.core.server.entity.ComponentVendor;
+import com.echothree.model.data.core.server.entity.EntityInstance;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.server.control.BaseLogic;
 import com.echothree.util.server.message.ExecutionErrorAccumulator;
+import com.echothree.util.server.persistence.EntityPermission;
 import com.echothree.util.server.persistence.Session;
 
 public class ComponentVendorLogic
@@ -38,16 +44,63 @@ public class ComponentVendorLogic
     public static ComponentVendorLogic getInstance() {
         return ComponentVendorLogicHolder.instance;
     }
-    
-    public ComponentVendor getComponentVendorByName(final ExecutionErrorAccumulator eea, final String componentVendorName) {
+
+    public ComponentVendor getComponentVendorByName(final ExecutionErrorAccumulator eea, final String componentVendorName,
+            final EntityPermission entityPermission) {
         var coreControl = (CoreControl)Session.getModelController(CoreControl.class);
-        ComponentVendor componentVendor = coreControl.getComponentVendorByName(componentVendorName);
+        var componentVendor = coreControl.getComponentVendorByName(componentVendorName, entityPermission);
 
         if(componentVendor == null) {
             handleExecutionError(UnknownComponentVendorNameException.class, eea, ExecutionErrors.UnknownComponentVendorName.name(), componentVendorName);
         }
 
         return componentVendor;
+    }
+
+    public ComponentVendor getComponentVendorByName(final ExecutionErrorAccumulator eea, final String componentVendorName) {
+        return getComponentVendorByName(eea, componentVendorName, EntityPermission.READ_ONLY);
+    }
+
+    public ComponentVendor getComponentVendorByNameForUpdate(final ExecutionErrorAccumulator eea, final String componentVendorName) {
+        return getComponentVendorByName(eea, componentVendorName, EntityPermission.READ_WRITE);
+    }
+
+    public ComponentVendor getComponentVendorByUniversalSpec(final ExecutionErrorAccumulator eea,
+            final ComponentVendorUniversalSpec universalSpec, final EntityPermission entityPermission) {
+        ComponentVendor componentVendor = null;
+        var coreControl = (CoreControl)Session.getModelController(CoreControl.class);
+        var componentVendorName = universalSpec.getComponentVendorName();
+        int parameterCount = (componentVendorName == null ? 0 : 1) + EntityInstanceLogic.getInstance().countPossibleEntitySpecs(universalSpec);
+
+        switch(parameterCount) {
+            case 1:
+                if(componentVendorName == null) {
+                    EntityInstance entityInstance = EntityInstanceLogic.getInstance().getEntityInstance(eea, universalSpec,
+                            ComponentVendors.ECHOTHREE.name(), EntityTypes.ComponentVendor.name());
+
+                    if(!eea.hasExecutionErrors()) {
+                        componentVendor = coreControl.getComponentVendorByEntityInstance(entityInstance, entityPermission);
+                    }
+                } else {
+                    componentVendor = getComponentVendorByName(eea, componentVendorName, entityPermission);
+                }
+                break;
+            default:
+                handleExecutionError(InvalidParameterCountException.class, eea, ExecutionErrors.InvalidParameterCount.name());
+                break;
+        }
+
+        return componentVendor;
+    }
+
+    public ComponentVendor getComponentVendorByUniversalSpec(final ExecutionErrorAccumulator eea,
+            final ComponentVendorUniversalSpec universalSpec) {
+        return getComponentVendorByUniversalSpec(eea, universalSpec, EntityPermission.READ_ONLY);
+    }
+
+    public ComponentVendor getComponentVendorByUniversalSpecForUpdate(final ExecutionErrorAccumulator eea,
+            final ComponentVendorUniversalSpec universalSpec) {
+        return getComponentVendorByUniversalSpec(eea, universalSpec, EntityPermission.READ_WRITE);
     }
 
 }
