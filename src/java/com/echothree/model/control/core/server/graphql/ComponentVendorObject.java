2adce3b63e9ead2542fc13b890b30dc703f5142e
Richard Harms <rich@echothree.com>
2020-01-26 21:44:13 -0600

    E3-2250: Added entityTypes and entityTypeCount to componentVendor

--- src/java/com/echothree/model/control/core/server/graphql/ComponentVendorObject.java
+++ src/java/com/echothree/model/control/core/server/graphql/ComponentVendorObject.java
@@ -16,14 +16,27 @@
 
 package com.echothree.model.control.core.server.graphql;
 
+import com.echothree.control.user.core.server.command.GetEntityTypeCommand;
+import com.echothree.control.user.uom.server.command.GetUnitOfMeasureTypeCommand;
+import com.echothree.model.control.core.server.CoreControl;
 import com.echothree.model.control.graphql.server.graphql.BaseEntityInstanceObject;
+import com.echothree.model.control.graphql.server.util.GraphQlContext;
+import com.echothree.model.control.uom.server.UomControl;
+import com.echothree.model.control.uom.server.graphql.UnitOfMeasureTypeObject;
 import com.echothree.model.data.core.server.entity.ComponentVendor;
 import com.echothree.model.data.core.server.entity.ComponentVendorDetail;
+import com.echothree.model.data.core.server.entity.EntityType;
+import com.echothree.model.data.uom.server.entity.UnitOfMeasureType;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
+import com.echothree.util.server.persistence.Session;
 import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.annotations.annotationTypes.GraphQLNonNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @GraphQLDescription("component vendor object")
 @GraphQLName("ComponentVendor")
@@ -47,6 +60,21 @@ public class ComponentVendorObject
         
         return componentVendorDetail;
     }
+
+    private Boolean hasEntityTypeAccess;
+
+    private boolean getHasEntityTypeAccess(final DataFetchingEnvironment env) {
+        if(hasEntityTypeAccess == null) {
+            GraphQlContext context = env.getContext();
+            var baseSingleEntityCommand = new GetEntityTypeCommand(context.getUserVisitPK(), null);
+
+            baseSingleEntityCommand.security();
+
+            hasEntityTypeAccess = !baseSingleEntityCommand.hasSecurityMessages();
+        }
+
+        return hasEntityTypeAccess;
+    }
     
     @GraphQLField
     @GraphQLDescription("component vendor name")
@@ -60,6 +88,32 @@ public class ComponentVendorObject
     @GraphQLNonNull
     public String getDescription(final DataFetchingEnvironment env) {
         return getComponentVendorDetail().getDescription();
+    }
+
+    @GraphQLField
+    @GraphQLDescription("entity types")
+    public List<EntityTypeObject> getEntityTypes(final DataFetchingEnvironment env) {
+        if(getHasEntityTypeAccess(env)) {
+            var coreControl = (CoreControl)Session.getModelController(CoreControl.class);
+            var entities = coreControl.getEntityTypesByComponentVendor(componentVendor);
+            var entityTypes = entities.stream().map(EntityTypeObject::new).collect(Collectors.toCollection(() -> new ArrayList<>(entities.size())));
+
+            return entityTypes;
+        } else {
+            return null;
+        }
+    }
+
+    @GraphQLField
+    @GraphQLDescription("entity type count")
+    public Long getEntityTypeCount(final DataFetchingEnvironment env) {
+        if(getHasEntityTypeAccess(env)) {
+            var coreControl = (CoreControl)Session.getModelController(CoreControl.class);
+
+            return coreControl.countEntityTypesByComponentVendor(componentVendor);
+        } else {
+            return null;
+        }
     }
     
 }
