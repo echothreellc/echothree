842a23f6639a3c9c43cfff15ac35964491ba6a7a
Richard Harms <rich@echothree.com>
2020-01-29 22:15:04 -0600

    E3-2250: Added test for entityTypeCount and entityTypes additions to componentVendor

--- test/junit/src/java/com/echothree/control/user/test/core/graphql/ComponentVendorTest.java
+++ test/junit/src/java/com/echothree/control/user/test/core/graphql/ComponentVendorTest.java
@@ -71,6 +71,21 @@ public class ComponentVendorTest
         assertThat(componentVendorName).isEqualTo(ComponentVendors.ECHOTHREE.toString());
     }
 
+    @Test
+    public void componentVendorQueryWithEntityTypes()
+            throws Exception {
+        var loginBody = executeUsingPost("mutation { employeeLogin(input: { username: \"test e\", password: \"password\", companyName: \"TEST_COMPANY\", clientMutationId: \"1\" }) { hasErrors } }");
+        Assert.assertFalse(getBoolean(loginBody, "data.employeeLogin.hasErrors"));
 
+        var componentVendorBody = executeUsingPost("query { componentVendor(componentVendorName: \"" + ComponentVendors.ECHOTHREE + "\") { componentVendorName entityTypeCount entityTypes { entityTypeName } } }");
+
+        var componentVendorName = getString(componentVendorBody, "data.componentVendor.componentVendorName");
+        var entityTypeCount = getLong(componentVendorBody, "data.componentVendor.entityTypeCount");
+        var entityTypes = getList(componentVendorBody, "data.componentVendor.entityTypes");
+
+        assertThat(componentVendorName).isEqualTo(ComponentVendors.ECHOTHREE.toString());
+        assertThat(entityTypeCount).isGreaterThan(0);
+        assertThat(entityTypes).size().isEqualTo(entityTypeCount);
+    }
 
 }
