f05542f01bbabba1afe58e6aed826a898b0d2d4a
Richard Harms <rich@echothree.com>
2020-03-22 22:50:11 -0500

    E3-2546: Additional Clean-Up for 2020-03-22

--- ui/cli/dataloader/src/java/com/echothree/ui/cli/dataloader/data/InitialDataParser.java
+++ ui/cli/dataloader/src/java/com/echothree/ui/cli/dataloader/data/InitialDataParser.java
@@ -31,6 +31,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import javax.naming.NamingException;
+import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXNotRecognizedException;
@@ -67,9 +68,6 @@ public class InitialDataParser
     protected static final String DYNAMIC_VALIDATION_FEATURE_ID = "http://apache.org/xml/features/validation/dynamic";
     
     // default settings
-    
-    /** Default parser name. */
-    protected static final String DEFAULT_PARSER_NAME = "org.apache.xerces.parsers.SAXParser";
     
     /** Default namespaces support (true). */
     protected static final boolean DEFAULT_NAMESPACES = true;
@@ -322,41 +320,35 @@ public class InitialDataParser
     public void execute()
             throws IOException, SAXException {
         XMLReader parser = null;
-        boolean namespaces = DEFAULT_NAMESPACES;
-        boolean namespacePrefixes = DEFAULT_NAMESPACE_PREFIXES;
-        boolean validation = DEFAULT_VALIDATION;
-        boolean schemaValidation = DEFAULT_SCHEMA_VALIDATION;
-        boolean schemaFullChecking = DEFAULT_SCHEMA_FULL_CHECKING;
-        boolean dynamicValidation = DEFAULT_DYNAMIC_VALIDATION;
 
         // create parser
         try {
-            parser = XMLReaderFactory.createXMLReader(DEFAULT_PARSER_NAME);
+            parser = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
         } catch(Exception e) {
-            System.err.println("error: Unable to instantiate parser (" + DEFAULT_PARSER_NAME + ")");
+            System.err.println("error: Unable to instantiate parser");
         }
 
         // set parser features
         try {
-            parser.setFeature(NAMESPACES_FEATURE_ID, namespaces);
+            parser.setFeature(NAMESPACES_FEATURE_ID, DEFAULT_NAMESPACES);
         } catch(SAXException e) {
             System.err.println("warning: Parser does not support feature (" + NAMESPACES_FEATURE_ID + ")");
         }
 
         try {
-            parser.setFeature(NAMESPACE_PREFIXES_FEATURE_ID, namespacePrefixes);
+            parser.setFeature(NAMESPACE_PREFIXES_FEATURE_ID, DEFAULT_NAMESPACE_PREFIXES);
         } catch(SAXException e) {
             System.err.println("warning: Parser does not support feature (" + NAMESPACE_PREFIXES_FEATURE_ID + ")");
         }
 
         try {
-            parser.setFeature(VALIDATION_FEATURE_ID, validation);
+            parser.setFeature(VALIDATION_FEATURE_ID, DEFAULT_VALIDATION);
         } catch(SAXException e) {
             System.err.println("warning: Parser does not support feature (" + VALIDATION_FEATURE_ID + ")");
         }
 
         try {
-            parser.setFeature(SCHEMA_VALIDATION_FEATURE_ID, schemaValidation);
+            parser.setFeature(SCHEMA_VALIDATION_FEATURE_ID, DEFAULT_SCHEMA_VALIDATION);
         } catch(SAXNotRecognizedException e) {
             // ignore
         } catch(SAXNotSupportedException e) {
@@ -364,7 +356,7 @@ public class InitialDataParser
         }
 
         try {
-            parser.setFeature(SCHEMA_FULL_CHECKING_FEATURE_ID, schemaFullChecking);
+            parser.setFeature(SCHEMA_FULL_CHECKING_FEATURE_ID, DEFAULT_SCHEMA_FULL_CHECKING);
         } catch(SAXNotRecognizedException e) {
             // ignore
         } catch(SAXNotSupportedException e) {
@@ -372,7 +364,7 @@ public class InitialDataParser
         }
 
         try {
-            parser.setFeature(DYNAMIC_VALIDATION_FEATURE_ID, dynamicValidation);
+            parser.setFeature(DYNAMIC_VALIDATION_FEATURE_ID, DEFAULT_DYNAMIC_VALIDATION);
         } catch(SAXNotRecognizedException e) {
             // ignore
         } catch(SAXNotSupportedException e) {
