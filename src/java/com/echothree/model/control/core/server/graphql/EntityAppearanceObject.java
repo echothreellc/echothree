1a38b1c63eaf553a76a31a62be9f1a4e2f14217a
Richard Harms <rich@echothree.com>
2020-04-12 13:03:35 -0500

    E3-2258: Security checks added to entityAppearance object

--- src/java/com/echothree/model/control/core/server/graphql/EntityAppearanceObject.java
+++ src/java/com/echothree/model/control/core/server/graphql/EntityAppearanceObject.java
@@ -16,11 +16,16 @@
 
 package com.echothree.model.control.core.server.graphql;
 
+import com.echothree.control.user.core.server.command.GetAppearanceCommand;
+import com.echothree.control.user.core.server.command.GetEntityInstanceCommand;
+import com.echothree.model.control.graphql.server.util.GraphQlContext;
 import com.echothree.model.data.core.server.entity.EntityAppearance;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
 import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.schema.DataFetchingEnvironment;
 
 @GraphQLDescription("entity appearance object")
 @GraphQLName("EntityAppearance")
@@ -31,19 +36,49 @@ public class EntityAppearanceObject {
     public EntityAppearanceObject(EntityAppearance entityAppearance) {
         this.entityAppearance = entityAppearance;
     }
-    
+
+    private Boolean hasEntityInstanceAccess;
+
+    private boolean getHasEntityInstanceAccess(final DataFetchingEnvironment env) {
+        if(hasEntityInstanceAccess == null) {
+            GraphQlContext context = env.getContext();
+            BaseSingleEntityCommand baseSingleEntityCommand = new GetEntityInstanceCommand(context.getUserVisitPK(), null);
+
+            baseSingleEntityCommand.security();
+
+            hasEntityInstanceAccess = !baseSingleEntityCommand.hasSecurityMessages();
+        }
+
+        return hasEntityInstanceAccess;
+    }
+
+    private Boolean hasAppearanceAccess;
+
+    private boolean getHasAppearanceAccess(final DataFetchingEnvironment env) {
+        if(hasAppearanceAccess == null) {
+            GraphQlContext context = env.getContext();
+            BaseSingleEntityCommand baseSingleEntityCommand = new GetAppearanceCommand(context.getUserVisitPK(), null);
+
+            baseSingleEntityCommand.security();
+
+            hasAppearanceAccess = !baseSingleEntityCommand.hasSecurityMessages();
+        }
+
+        return hasAppearanceAccess;
+    }
+
     @GraphQLField
     @GraphQLDescription("entity instance")
     @GraphQLNonNull
-    public EntityInstanceObject getEntityInstance() {
-        return new EntityInstanceObject(entityAppearance.getEntityInstance());
+    public EntityInstanceObject getEntityInstance(final DataFetchingEnvironment env) {
+        return getHasEntityInstanceAccess(env) ? new EntityInstanceObject(entityAppearance.getEntityInstance()) : null;
     }
     
     @GraphQLField
     @GraphQLDescription("appearance")
     @GraphQLNonNull
-    public AppearanceObject getAppearance() {
-        return new AppearanceObject(entityAppearance.getAppearance());
+    public AppearanceObject getAppearance(final DataFetchingEnvironment env) {
+        return getHasAppearanceAccess(env) ? new AppearanceObject(entityAppearance.getAppearance()) : null;
     }
     
 }
