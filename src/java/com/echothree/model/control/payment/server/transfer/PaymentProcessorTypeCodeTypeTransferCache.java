376b1146a356a851c83aa0c75eaf6b8b6d023673
Richard Harms <rich@echothree.com>
2020-05-10 22:07:32 -0500

    Added PaymentProcessorTypeCodeTypeControl and supporting classes

---
+++ src/java/com/echothree/model/control/payment/server/transfer/PaymentProcessorTypeCodeTypeTransferCache.java
@@ -0,0 +1,60 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2020 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.payment.server.transfer;
+
+import com.echothree.model.control.payment.common.transfer.PaymentProcessorTypeCodeTypeTransfer;
+import com.echothree.model.control.payment.server.PaymentControl;
+import com.echothree.model.control.payment.server.PaymentProcessorTypeCodeTypeControl;
+import com.echothree.model.control.payment.server.PaymentProcessorTypeControl;
+import com.echothree.model.data.payment.server.entity.PaymentProcessorTypeCodeType;
+import com.echothree.model.data.user.server.entity.UserVisit;
+import com.echothree.util.server.persistence.Session;
+
+public class PaymentProcessorTypeCodeTypeTransferCache
+        extends BasePaymentTransferCache<PaymentProcessorTypeCodeType, PaymentProcessorTypeCodeTypeTransfer> {
+
+    PaymentProcessorTypeControl paymentProcessorTypeControl = (PaymentProcessorTypeControl) Session.getModelController(PaymentProcessorTypeControl.class);
+    PaymentProcessorTypeCodeTypeControl paymentProcessorTypeCodeTypeControl = (PaymentProcessorTypeCodeTypeControl) Session.getModelController(PaymentProcessorTypeCodeTypeControl.class);
+
+    /** Creates a new instance of PaymentProcessorTypeTransferCache */
+    public PaymentProcessorTypeCodeTypeTransferCache(UserVisit userVisit, PaymentControl paymentControl) {
+        super(userVisit, paymentControl);
+        
+        setIncludeEntityInstance(true);
+    }
+    
+    @Override
+    public PaymentProcessorTypeCodeTypeTransfer getTransfer(PaymentProcessorTypeCodeType paymentProcessorTypeCodeType) {
+        var paymentProcessorTypeCodeTypeTransfer = get(paymentProcessorTypeCodeType);
+        
+        if(paymentProcessorTypeCodeTypeTransfer == null) {
+            var paymentProcessorTypeCodeTypeDetail = paymentProcessorTypeCodeType.getLastDetail();
+            var paymentProcessorTypeTransfer = paymentProcessorTypeControl.getPaymentProcessorTypeTransfer(userVisit, paymentProcessorTypeCodeTypeDetail.getPaymentProcessorType());
+            var paymentProcessorTypeCodeTypeName = paymentProcessorTypeCodeTypeDetail.getPaymentProcessorTypeCodeTypeName();
+            var isDefault = paymentProcessorTypeCodeTypeDetail.getIsDefault();
+            var sortOrder = paymentProcessorTypeCodeTypeDetail.getSortOrder();
+            var description = paymentProcessorTypeCodeTypeControl.getBestPaymentProcessorTypeCodeTypeDescription(paymentProcessorTypeCodeType, getLanguage());
+            
+            paymentProcessorTypeCodeTypeTransfer = new PaymentProcessorTypeCodeTypeTransfer(paymentProcessorTypeTransfer,
+                    paymentProcessorTypeCodeTypeName, isDefault, sortOrder, description);
+            put(paymentProcessorTypeCodeType, paymentProcessorTypeCodeTypeTransfer);
+        }
+        
+        return paymentProcessorTypeCodeTypeTransfer;
+    }
+    
+}
