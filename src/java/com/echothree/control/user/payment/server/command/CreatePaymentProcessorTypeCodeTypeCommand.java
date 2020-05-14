699c55a122c512cda0b0e32bb2a4ba5c85c54b29
Richard Harms <rich@echothree.com>
2020-05-13 22:25:11 -0500

    Added CreatePaymentProcessorTypeCodeType and CreatePaymentProcessorTypeCodeTypeDescription UCs

---
+++ src/java/com/echothree/control/user/payment/server/command/CreatePaymentProcessorTypeCodeTypeCommand.java
@@ -0,0 +1,86 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2020 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.control.user.payment.server.command;
+
+import com.echothree.control.user.payment.common.form.CreatePaymentProcessorTypeCodeTypeForm;
+import com.echothree.control.user.payment.common.result.PaymentResultFactory;
+import com.echothree.model.control.party.common.PartyTypes;
+import com.echothree.model.control.payment.server.logic.PaymentProcessorTypeCodeTypeLogic;
+import com.echothree.model.control.security.common.SecurityRoleGroups;
+import com.echothree.model.control.security.common.SecurityRoles;
+import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.util.common.command.BaseResult;
+import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.util.common.validation.FieldType;
+import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.CommandSecurityDefinition;
+import com.echothree.util.server.control.PartyTypeDefinition;
+import com.echothree.util.server.control.SecurityRoleDefinition;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CreatePaymentProcessorTypeCodeTypeCommand
+        extends BaseSimpleCommand<CreatePaymentProcessorTypeCodeTypeForm> {
+    
+    private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
+    private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
+    
+    static {
+        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(Collections.unmodifiableList(Arrays.asList(
+                new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
+                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), Collections.unmodifiableList(Arrays.asList(
+                        new SecurityRoleDefinition(SecurityRoleGroups.PaymentProcessorTypeCodeType.name(), SecurityRoles.Create.name())
+                        )))
+                )));
+        
+        FORM_FIELD_DEFINITIONS = Collections.unmodifiableList(Arrays.asList(
+                new FieldDefinition("PaymentProcessorTypeName", FieldType.ENTITY_NAME, true, null, null),
+                new FieldDefinition("PaymentProcessorTypeCodeTypeName", FieldType.ENTITY_NAME, true, null, null),
+                new FieldDefinition("IsDefault", FieldType.BOOLEAN, true, null, null),
+                new FieldDefinition("SortOrder", FieldType.SIGNED_INTEGER, true, null, null),
+                new FieldDefinition("Description", FieldType.STRING, false, 1L, 80L)
+                ));
+    }
+    
+    /** Creates a new instance of CreatePaymentProcessorTypeCodeTypeCommand */
+    public CreatePaymentProcessorTypeCodeTypeCommand(UserVisitPK userVisitPK, CreatePaymentProcessorTypeCodeTypeForm form) {
+        super(userVisitPK, form, COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, false);
+    }
+    
+    @Override
+    protected BaseResult execute() {
+        var result = PaymentResultFactory.getCreatePaymentProcessorTypeCodeTypeResult();
+        var paymentProcessorTypeName = form.getPaymentProcessorTypeName();
+        var paymentProcessorTypeCodeTypeName = form.getPaymentProcessorTypeCodeTypeName();
+        var isDefault = Boolean.valueOf(form.getIsDefault());
+        var sortOrder = Integer.valueOf(form.getSortOrder());
+        var description = form.getDescription();
+
+        var paymentProcessorTypeCodeType = PaymentProcessorTypeCodeTypeLogic.getInstance().createPaymentProcessorTypeCodeType(this,
+                paymentProcessorTypeName, paymentProcessorTypeCodeTypeName, isDefault, sortOrder, getPreferredLanguage(),
+                description, getPartyPK());
+
+        if(paymentProcessorTypeCodeType != null && !hasExecutionErrors()) {
+            result.setPaymentProcessorTypeCodeTypeName(paymentProcessorTypeCodeType.getLastDetail().getPaymentProcessorTypeCodeTypeName());
+            result.setEntityRef(paymentProcessorTypeCodeType.getPrimaryKey().getEntityRef());
+        }
+
+        return result;
+    }
+    
+}
