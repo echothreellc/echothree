97e7b5bd29d599ed13afec98897adff38b4bb5ce
Richard Harms <rich@echothree.com>
2020-05-24 19:44:58 -0500

    Added PaymentProcessorTransactionCode TO

---
+++ src/java/com/echothree/model/control/payment/common/transfer/PaymentProcessorTransactionCodeTransfer.java
@@ -0,0 +1,50 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2020 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.payment.common.transfer;
+
+import com.echothree.util.common.transfer.BaseTransfer;
+
+public class PaymentProcessorTransactionCodeTransfer
+        extends BaseTransfer {
+    
+    private PaymentProcessorTransactionTransfer paymentProcessorTransaction;
+    private PaymentProcessorTypeCodeTransfer paymentProcessorTypeCode;
+
+    /** Creates a new instance of PaymentProcessorTransactionCodeTransfer */
+    public PaymentProcessorTransactionCodeTransfer(PaymentProcessorTransactionTransfer paymentProcessorTransaction,
+            PaymentProcessorTypeCodeTransfer paymentProcessorTypeCode) {
+        this.paymentProcessorTransaction = paymentProcessorTransaction;
+        this.paymentProcessorTypeCode = paymentProcessorTypeCode;
+    }
+
+    public PaymentProcessorTransactionTransfer getPaymentProcessorTransaction() {
+        return paymentProcessorTransaction;
+    }
+
+    public void setPaymentProcessorTransaction(PaymentProcessorTransactionTransfer paymentProcessorTransaction) {
+        this.paymentProcessorTransaction = paymentProcessorTransaction;
+    }
+
+    public PaymentProcessorTypeCodeTransfer getPaymentProcessorTypeCode() {
+        return paymentProcessorTypeCode;
+    }
+
+    public void setPaymentProcessorTypeCode(PaymentProcessorTypeCodeTransfer paymentProcessorTypeCode) {
+        this.paymentProcessorTypeCode = paymentProcessorTypeCode;
+    }
+
+}
---
+++ src/java/com/echothree/model/control/payment/server/transfer/PaymentProcessorTransactionCodeTransferCache.java
@@ -0,0 +1,56 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2020 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.payment.server.transfer;
+
+import com.echothree.model.control.payment.common.transfer.PaymentProcessorTransactionCodeTransfer;
+import com.echothree.model.control.payment.server.control.PaymentControl;
+import com.echothree.model.control.payment.server.control.PaymentProcessorTransactionControl;
+import com.echothree.model.control.payment.server.control.PaymentProcessorTypeCodeControl;
+import com.echothree.model.data.payment.server.entity.PaymentProcessorTransactionCode;
+import com.echothree.model.data.user.server.entity.UserVisit;
+import com.echothree.util.server.persistence.Session;
+
+public class PaymentProcessorTransactionCodeTransferCache
+        extends BasePaymentTransferCache<PaymentProcessorTransactionCode, PaymentProcessorTransactionCodeTransfer> {
+
+    PaymentProcessorTransactionControl paymentProcessorTypeControl = (PaymentProcessorTransactionControl) Session.getModelController(PaymentProcessorTransactionControl.class);
+    PaymentProcessorTypeCodeControl paymentProcessorTypeCodeControl = (PaymentProcessorTypeCodeControl) Session.getModelController(PaymentProcessorTypeCodeControl.class);
+
+    /** Creates a new instance of PaymentProcessorTypeTransferCache */
+    public PaymentProcessorTransactionCodeTransferCache(UserVisit userVisit, PaymentControl paymentControl) {
+        super(userVisit, paymentControl);
+        
+        setIncludeEntityInstance(true);
+    }
+    
+    @Override
+    public PaymentProcessorTransactionCodeTransfer getTransfer(PaymentProcessorTransactionCode paymentProcessorTransactionCode) {
+        var paymentProcessorTransactionCodeTransfer = get(paymentProcessorTransactionCode);
+        
+        if(paymentProcessorTransactionCodeTransfer == null) {
+            var paymentProcessorTransactionTransfer = paymentProcessorTypeControl.getPaymentProcessorTransactionTransfer(userVisit, paymentProcessorTransactionCode.getPaymentProcessorTransaction());
+            var paymentProcessorActionTypeTransfer = paymentProcessorTypeCodeControl.getPaymentProcessorTypeCodeTransfer(userVisit, paymentProcessorTransactionCode.getPaymentProcessorTypeCode());
+
+            paymentProcessorTransactionCodeTransfer = new PaymentProcessorTransactionCodeTransfer(paymentProcessorTransactionTransfer,
+                    paymentProcessorActionTypeTransfer);
+            put(paymentProcessorTransactionCode, paymentProcessorTransactionCodeTransfer);
+        }
+        
+        return paymentProcessorTransactionCodeTransfer;
+    }
+    
+}
