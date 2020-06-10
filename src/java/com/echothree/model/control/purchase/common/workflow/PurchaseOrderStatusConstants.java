f35d705fd0e7402b4b12917f470bdc31d7affac9
Richard Harms <rich@echothree.com>
2020-06-09 19:42:36 -0500

    #67: Added PurchaseOrderStatusConstants for PURCHASE_ORDER_STATUS workflow

---
+++ src/java/com/echothree/model/control/purchase/common/workflow/PurchaseOrderStatusConstants.java
@@ -0,0 +1,31 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2020 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.purchase.common.workflow;
+
+public interface PurchaseOrderStatusConstants {
+    
+    String Workflow_PURCHASE_ORDER_STATUS = "PURCHASE_ORDER_STATUS";
+    
+    String WorkflowStep_ENTRY = "ENTRY";
+    String WorkflowStep_COMPLETE = "COMPLETE";
+    
+    String WorkflowEntrance_ENTRY = "ENTRY";
+    String WorkflowEntrance_COMPLETE= "COMPLETE";
+    
+    String WorkflowDestination_ENTRY_TO_COMPLETE = "ENTRY_TO_COMPLETE";
+
+}
