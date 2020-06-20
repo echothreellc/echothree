053674a3b3b6904675d4cefeed909517e32a4871
Richard Harms <rich@echothree.com>
2020-06-19 22:36:45 -0500

    #83: Filling in Whens for item prices

--- test/cucumber/src/java/com/echothree/cucumber/item/ItemPriceSteps.java
+++ test/cucumber/src/java/com/echothree/cucumber/item/ItemPriceSteps.java
@@ -17,6 +17,7 @@
 package com.echothree.cucumber.item;
 
 import com.echothree.control.user.core.common.CoreUtil;
+import com.echothree.control.user.core.common.result.EditEntityListItemResult;
 import com.echothree.control.user.item.common.ItemUtil;
 import com.echothree.control.user.item.common.result.CreateItemResult;
 import com.echothree.control.user.item.common.result.EditItemPriceResult;
@@ -61,7 +62,7 @@ public class ItemPriceSteps implements En {
                     persona.createItemPriceForm = null;
                 });
 
-        When("^the user begins deleting an entity list item$",
+        When("^the user begins deleting an item price$",
                 () -> {
                     var persona = CurrentPersona.persona;
 
@@ -72,7 +73,7 @@ public class ItemPriceSteps implements En {
                     persona.deleteItemPriceForm = ItemUtil.getHome().getDeleteItemPriceForm();
                 });
 
-        When("^the user deletes the entity list item$",
+        When("^the user deletes the item price$",
                 () -> {
                     var persona = CurrentPersona.persona;
                     var deleteItemPriceForm = persona.deleteItemPriceForm;
@@ -84,7 +85,7 @@ public class ItemPriceSteps implements En {
                     persona.deleteItemPriceForm = null;
                 });
 
-        When("^the user begins specifying an entity list item to edit$",
+        When("^the user begins specifying an item price to edit$",
                 () -> {
                     var persona = CurrentPersona.persona;
 
@@ -95,90 +96,201 @@ public class ItemPriceSteps implements En {
                     persona.itemPriceSpec = ItemUtil.getHome().getItemPriceSpec();
                 });
 
+        When("^the user begins editing the item price",
+                () -> {
+                    var persona = CurrentPersona.persona;
+                    var spec = persona.itemPriceSpec;
 
-        // Edit goes here, alter everything that follows to fill in correct one.
+                    assertThat(spec).isNotNull();
+
+                    var commandForm = ItemUtil.getHome().getEditItemPriceForm();
+
+                    commandForm.setSpec(spec);
+                    commandForm.setEditMode(EditMode.LOCK);
+
+                    var commandResult = ItemUtil.getHome().editItemPrice(persona.userVisitPK, commandForm);
+                    LastCommandResult.commandResult = commandResult;
+
+                    var executionResult = commandResult.getExecutionResult();
+                    var result = (EditItemPriceResult)executionResult.getResult();
+
+                    if(!executionResult.getHasErrors()) {
+                        persona.itemPriceEdit = result.getEdit();
+                    }
+                });
+
+        When("^the user finishes editing the item price",
+                () -> {
+                    var persona = CurrentPersona.persona;
+                    var spec = persona.itemPriceSpec;
+                    var edit = persona.itemPriceEdit;
+
+                    assertThat(edit).isNotNull();
+
+                    var commandForm = ItemUtil.getHome().getEditItemPriceForm();
+
+                    commandForm.setSpec(spec);
+                    commandForm.setEdit(edit);
+                    commandForm.setEditMode(EditMode.UPDATE);
+
+                    var commandResult = ItemUtil.getHome().editItemPrice(persona.userVisitPK, commandForm);
+                    LastCommandResult.commandResult = commandResult;
+
+                    persona.itemPriceSpec = null;
+                    persona.itemPriceEdit = null;
+                });
 
 
-        When("^the user sets the item price's item to ([^\"]*)$",
+        When("^the user sets the item price's item to ([a-zA-Z0-9-_]*)$",
                 (String itemName) -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var deleteItemPriceForm = persona.deleteItemPriceForm;
+                    var itemPriceSpec = persona.itemPriceSpec;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
+                    assertThat(createItemPriceForm != null || deleteItemPriceForm != null || itemPriceSpec != null).isTrue();
 
-                    persona.createItemPriceForm.setItemName(itemName);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setItemName(itemName);
+                    } else if(deleteItemPriceForm != null) {
+                        deleteItemPriceForm.setItemName(itemName);
+                    } else {
+                        itemPriceSpec.setItemName(itemName);
+                    }
                 });
 
         When("^the user sets the item price's item to the last item added$",
-                (String itemName) -> {
+                () -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var deleteItemPriceForm = persona.deleteItemPriceForm;
+                    var itemPriceSpec = persona.itemPriceSpec;
+                    var lastItemName = persona.lastItemName;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
-                    assertThat(persona.lastItemName).isNotNull();
+                    assertThat(createItemPriceForm != null || deleteItemPriceForm != null || itemPriceSpec != null).isTrue();
+                    assertThat(lastItemName).isNotNull();
 
-                    persona.createItemPriceForm.setItemName(persona.lastItemName);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setItemName(lastItemName);
+                    } else if(deleteItemPriceForm != null) {
+                        deleteItemPriceForm.setItemName(lastItemName);
+                    } else {
+                        itemPriceSpec.setItemName(lastItemName);
+                    }
                 });
 
-        When("^the user sets the item price's inventory condition to ([^\"]*)$",
+        When("^the user sets the item price's inventory condition to ([a-zA-Z0-9-_]*)$",
                 (String inventoryConditionName) -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var deleteItemPriceForm = persona.deleteItemPriceForm;
+                    var itemPriceSpec = persona.itemPriceSpec;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
+                    assertThat(createItemPriceForm != null || deleteItemPriceForm != null || itemPriceSpec != null).isTrue();
 
-                    persona.createItemPriceForm.setInventoryConditionName(inventoryConditionName);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setInventoryConditionName(inventoryConditionName);
+                    } else if(deleteItemPriceForm != null) {
+                        deleteItemPriceForm.setInventoryConditionName(inventoryConditionName);
+                    } else {
+                        itemPriceSpec.setInventoryConditionName(inventoryConditionName);
+                    }
                 });
 
-        When("^the user sets the item price's unit of measure type to ([^\"]*)$",
+        When("^the user sets the item price's unit of measure type to ([a-zA-Z0-9-_]*)$",
                 (String unitOfMeasureTypeName) -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var deleteItemPriceForm = persona.deleteItemPriceForm;
+                    var itemPriceSpec = persona.itemPriceSpec;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
+                    assertThat(createItemPriceForm != null || deleteItemPriceForm != null || itemPriceSpec != null).isTrue();
 
-                    persona.createItemPriceForm.setUnitOfMeasureTypeName(unitOfMeasureTypeName);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setUnitOfMeasureTypeName(unitOfMeasureTypeName);
+                    } else if(deleteItemPriceForm != null) {
+                        deleteItemPriceForm.setUnitOfMeasureTypeName(unitOfMeasureTypeName);
+                    } else {
+                        itemPriceSpec.setUnitOfMeasureTypeName(unitOfMeasureTypeName);
+                    }
                 });
 
-        When("^the user sets the item price's currency to ([^\"]*)$",
+        When("^the user sets the item price's currency to ([a-zA-Z0-9-_]*)$",
                 (String currencyIsoName) -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var deleteItemPriceForm = persona.deleteItemPriceForm;
+                    var itemPriceSpec = persona.itemPriceSpec;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
+                    assertThat(createItemPriceForm != null || deleteItemPriceForm != null || itemPriceSpec != null).isTrue();
 
-                    persona.createItemPriceForm.setCurrencyIsoName(currencyIsoName);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setCurrencyIsoName(currencyIsoName);
+                    } else if(deleteItemPriceForm != null) {
+                        deleteItemPriceForm.setCurrencyIsoName(currencyIsoName);
+                    } else {
+                        itemPriceSpec.setCurrencyIsoName(currencyIsoName);
+                    }
                 });
 
         When("^the user sets the item price's unit price to ([^\"]*)$",
                 (String unitPrice) -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var itemPriceEdit = persona.itemPriceEdit;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
+                    assertThat(createItemPriceForm != null || itemPriceEdit != null).isTrue();
 
-                    persona.createItemPriceForm.setUnitPrice(unitPrice);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setUnitPrice(unitPrice);
+                    } else {
+                        itemPriceEdit.setUnitPrice(unitPrice);
+                    }
                 });
 
         When("^the user sets the item price's minimum unit price to ([^\"]*)$",
                 (String minimumUnitPrice) -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var itemPriceEdit = persona.itemPriceEdit;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
+                    assertThat(createItemPriceForm != null || itemPriceEdit != null).isTrue();
 
-                    persona.createItemPriceForm.setMinimumUnitPrice(minimumUnitPrice);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setMinimumUnitPrice(minimumUnitPrice);
+                    } else {
+                        itemPriceEdit.setMinimumUnitPrice(minimumUnitPrice);
+                    }
                 });
 
         When("^the user sets the item price's maximum unit price to ([^\"]*)$",
                 (String maximumUnitPrice) -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var itemPriceEdit = persona.itemPriceEdit;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
+                    assertThat(createItemPriceForm != null || itemPriceEdit != null).isTrue();
 
-                    persona.createItemPriceForm.setMaximumUnitPrice(maximumUnitPrice);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setMaximumUnitPrice(maximumUnitPrice);
+                    } else {
+                        itemPriceEdit.setMaximumUnitPrice(maximumUnitPrice);
+                    }
                 });
 
         When("^the user sets the item price's unit price increment to ([^\"]*)$",
                 (String unitPriceIncrement) -> {
                     var persona = CurrentPersona.persona;
+                    var createItemPriceForm = persona.createItemPriceForm;
+                    var itemPriceEdit = persona.itemPriceEdit;
 
-                    assertThat(persona.createItemPriceForm).isNotNull();
+                    assertThat(createItemPriceForm != null || itemPriceEdit != null).isTrue();
 
-                    persona.createItemPriceForm.setUnitPriceIncrement(unitPriceIncrement);
+                    if(createItemPriceForm != null) {
+                        createItemPriceForm.setUnitPriceIncrement(unitPriceIncrement);
+                    } else {
+                        itemPriceEdit.setUnitPriceIncrement(unitPriceIncrement);
+                    }
                 });
     }
 
