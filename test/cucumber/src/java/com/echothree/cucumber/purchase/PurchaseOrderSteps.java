bf9df339d585ae925635c90ef3dbfc15b5d95349
Richard Harms <rich@echothree.com>
2020-08-13 22:13:19 -0500

    #38: Started adding purchase order steps

---
+++ test/cucumber/src/java/com/echothree/cucumber/purchase/PurchaseOrderSteps.java
@@ -0,0 +1,252 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2020 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.cucumber.purchase;
+
+import com.echothree.control.user.purchase.common.PurchaseUtil;
+import com.echothree.cucumber.util.command.LastCommandResult;
+import com.echothree.cucumber.util.persona.CurrentPersona;
+import com.echothree.util.common.command.EditMode;
+import io.cucumber.java8.En;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PurchaseOrderSteps implements En {
+
+    public PurchaseOrderSteps() {
+        When("^the user begins entering a new purchase order$",
+                () -> {
+                    var persona = CurrentPersona.persona;
+
+                    assertThat(persona.createPurchaseOrderForm).isNull();
+                    assertThat(persona.setPurchaseOrderStatusForm).isNull();
+                    assertThat(persona.purchaseOrderSpec).isNull();
+
+                    persona.createPurchaseOrderForm = PurchaseUtil.getHome().getCreatePurchaseOrderForm();
+                });
+
+        When("^the user adds the new purchase order$",
+                () -> {
+                    var persona = CurrentPersona.persona;
+                    var createPurchaseOrderForm = persona.createPurchaseOrderForm;
+
+                    assertThat(createPurchaseOrderForm).isNotNull();
+
+                    LastCommandResult.commandResult = PurchaseUtil.getHome().createPurchaseOrder(persona.userVisitPK, createPurchaseOrderForm);
+
+                    persona.createPurchaseOrderForm = null;
+                });
+
+        When("^the user begins setting the status of a purchase order$",
+                () -> {
+                    var persona = CurrentPersona.persona;
+
+                    assertThat(persona.createPurchaseOrderForm).isNull();
+                    assertThat(persona.setPurchaseOrderStatusForm).isNull();
+                    assertThat(persona.purchaseOrderSpec).isNull();
+
+                    persona.setPurchaseOrderStatusForm = PurchaseUtil.getHome().getSetPurchaseOrderStatusForm();
+                });
+
+        When("^the user sets the status of the purchase order$",
+                () -> {
+                    var persona = CurrentPersona.persona;
+                    var setPurchaseOrderStatusForm = persona.setPurchaseOrderStatusForm;
+
+                    assertThat(setPurchaseOrderStatusForm).isNotNull();
+
+                    LastCommandResult.commandResult = PurchaseUtil.getHome().setPurchaseOrderStatus(persona.userVisitPK, setPurchaseOrderStatusForm);
+
+                    persona.setPurchaseOrderStatusForm = null;
+                });
+
+        When("^the user begins specifying a purchase order to edit$",
+                () -> {
+                    var persona = CurrentPersona.persona;
+
+                    assertThat(persona.createPurchaseOrderForm).isNull();
+                    assertThat(persona.setPurchaseOrderStatusForm).isNull();
+                    assertThat(persona.purchaseOrderSpec).isNull();
+
+                    persona.purchaseOrderSpec = PurchaseUtil.getHome().getPurchaseOrderSpec();
+                });
+
+//        When("^the user begins editing the purchase order$",
+//                () -> {
+//                    var persona = CurrentPersona.persona;
+//                    var spec = persona.purchaseOrderSpec;
+//
+//                    assertThat(spec).isNotNull();
+//
+//                    var commandForm = PurchaseUtil.getHome().getEditPurchaseOrderForm();
+//
+//                    commandForm.setSpec(spec);
+//                    commandForm.setEditMode(EditMode.LOCK);
+//
+//                    var commandResult = PurchaseUtil.getHome().editPurchaseOrder(persona.userVisitPK, commandForm);
+//                    LastCommandResult.commandResult = commandResult;
+//
+//                    var executionResult = commandResult.getExecutionResult();
+//                    var result = (EditPurchaseOrderResult)executionResult.getResult();
+//
+//                    if(!executionResult.getHasErrors()) {
+//                        persona.purchaseOrderEdit = result.getEdit();
+//                    }
+//                });
+//
+//        When("^the user finishes editing the purchase order$",
+//                () -> {
+//                    var persona = CurrentPersona.persona;
+//                    var spec = persona.purchaseOrderSpec;
+//                    var edit = persona.purchaseOrderEdit;
+//
+//                    assertThat(spec).isNotNull();
+//                    assertThat(edit).isNotNull();
+//
+//                    var commandForm = PurchaseUtil.getHome().getEditPurchaseOrderForm();
+//
+//                    commandForm.setSpec(spec);
+//                    commandForm.setEdit(edit);
+//                    commandForm.setEditMode(EditMode.UPDATE);
+//
+//                    var commandResult = PurchaseUtil.getHome().editPurchaseOrder(persona.userVisitPK, commandForm);
+//                    LastCommandResult.commandResult = commandResult;
+//
+//                    persona.purchaseOrderSpec = null;
+//                    persona.purchaseOrderEdit = null;
+//                });
+
+        When("^the user sets the purchase order's vendor name to ([a-zA-Z0-9-_]*)$",
+                (String vendorName) -> {
+                    var persona = CurrentPersona.persona;
+                    var createPurchaseOrderForm = persona.createPurchaseOrderForm;
+
+                    assertThat(createPurchaseOrderForm).isNotNull();
+
+                    createPurchaseOrderForm.setVendorName(vendorName);
+                });
+
+        When("^the user sets the purchase order's purchase order name to the last purchase order added$",
+                () -> {
+                    var persona = CurrentPersona.persona;
+                    var setPurchaseOrderStatusForm = persona.setPurchaseOrderStatusForm;
+                    var purchaseOrderSpec = persona.purchaseOrderSpec;
+
+                    assertThat(setPurchaseOrderStatusForm != null || purchaseOrderSpec != null).isTrue();
+
+                    if(setPurchaseOrderStatusForm != null) {
+                        setPurchaseOrderStatusForm.setOrderName(persona.lastPurchaseOrderName);
+                    } else {
+                        purchaseOrderSpec.setOrderName(persona.lastPurchaseOrderName);
+                    }
+                });
+
+        When("^the user sets the purchase order's purchase order name to ([a-zA-Z0-9-_]*)$",
+                (String purchaseOrderName) -> {
+                    var persona = CurrentPersona.persona;
+                    var setPurchaseOrderStatusForm = persona.setPurchaseOrderStatusForm;
+                    var purchaseOrderSpec = persona.purchaseOrderSpec;
+
+                    assertThat(setPurchaseOrderStatusForm != null || purchaseOrderSpec != null).isTrue();
+
+                    if(setPurchaseOrderStatusForm != null) {
+                        setPurchaseOrderStatusForm.setOrderName(purchaseOrderName);
+                    } else {
+                        purchaseOrderSpec.setOrderName(purchaseOrderName);
+                    }
+                });
+
+//        When("^the user sets the purchase order's new purchase order name to ([a-zA-Z0-9-_]*)$",
+//                (String purchaseOrderName) -> {
+//                    var persona = CurrentPersona.persona;
+//                    var purchaseOrderEdit = persona.purchaseOrderEdit;
+//
+//                    assertThat(purchaseOrderEdit).isNotNull();
+//
+//                    purchaseOrderEdit.setPurchaseOrderName(purchaseOrderName);
+//                });
+//
+//        When("^the user sets the purchase order's status to ([a-zA-Z0-9-_]*)$",
+//                (String purchaseOrderStatusChoice) -> {
+//                    var persona = CurrentPersona.persona;
+//                    var setPurchaseOrderStatusForm = persona.setPurchaseOrderStatusForm;
+//
+//                    assertThat(setPurchaseOrderStatusForm).isNotNull();
+//
+//                    setPurchaseOrderStatusForm.setPurchaseOrderStatusChoice(purchaseOrderStatusChoice);
+//                });
+//
+//        When("^the user sets the purchase order's description to \"([^\"]*)\"$",
+//                (String description) -> {
+//                    var persona = CurrentPersona.persona;
+//                    var createPurchaseOrderForm = persona.createPurchaseOrderForm;
+//                    var purchaseOrderEdit = persona.purchaseOrderEdit;
+//
+//                    assertThat(createPurchaseOrderForm != null || purchaseOrderEdit != null).isTrue();
+//
+//                    if(createPurchaseOrderForm != null) {
+//                        createPurchaseOrderForm.setDescription(description);
+//                    } else {
+//                        purchaseOrderEdit.setDescription(description);
+//                    }
+//                });
+//
+//        When("^the user sets the purchase order's priority to \"([^\"]*)\"$",
+//                (String priority) -> {
+//                    var persona = CurrentPersona.persona;
+//                    var createPurchaseOrderForm = persona.createPurchaseOrderForm;
+//                    var purchaseOrderEdit = persona.purchaseOrderEdit;
+//
+//                    assertThat(createPurchaseOrderForm != null || purchaseOrderEdit != null).isTrue();
+//
+//                    if(createPurchaseOrderForm != null) {
+//                        createPurchaseOrderForm.setPriority(priority);
+//                    } else {
+//                        purchaseOrderEdit.setPriority(priority);
+//                    }
+//                });
+//
+//        When("^the user sets the purchase order's cancellation policy to ([a-zA-Z0-9-_]*)$",
+//                (String cancellationPolicyName) -> {
+//                    var persona = CurrentPersona.persona;
+//                    var createPurchaseOrderForm = persona.createPurchaseOrderForm;
+//                    var purchaseOrderEdit = persona.purchaseOrderEdit;
+//
+//                    assertThat(createPurchaseOrderForm != null || purchaseOrderEdit != null).isTrue();
+//
+//                    if(createPurchaseOrderForm != null) {
+//                        createPurchaseOrderForm.setCancellationPolicyName(cancellationPolicyName);
+//                    } else {
+//                        purchaseOrderEdit.setCancellationPolicyName(cancellationPolicyName);
+//                    }
+//                });
+//
+//        When("^the user sets the purchase order's return policy to ([a-zA-Z0-9-_]*)$",
+//                (String returnPolicyName) -> {
+//                    var persona = CurrentPersona.persona;
+//                    var createPurchaseOrderForm = persona.createPurchaseOrderForm;
+//                    var purchaseOrderEdit = persona.purchaseOrderEdit;
+//
+//                    assertThat(createPurchaseOrderForm != null || purchaseOrderEdit != null).isTrue();
+//
+//                    if(createPurchaseOrderForm != null) {
+//                        createPurchaseOrderForm.setReturnPolicyName(returnPolicyName);
+//                    } else {
+//                        purchaseOrderEdit.setReturnPolicyName(returnPolicyName);
+//                    }
+//                });
+    }
+
+}
