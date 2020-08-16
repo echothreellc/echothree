8aea174480f53a372a4cb580ab210e3eb8c2bf15
Richard Harms <rich@echothree.com>
2020-08-15 21:31:41 -0500

    #111: Added script to configure WildFly and deploy application

---
+++ bin/configure-wildfly.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+$JBOSS_HOME/bin/jboss-cli.sh --connect --file="doc/configuration/wildfly/infinispan-cli.txt"
+$JBOSS_HOME/bin/jboss-cli.sh --connect --command="reload"
+
+$JBOSS_HOME/bin/jboss-cli.sh --connect --command="deploy build/lib/mysql-connector-java.jar"
+$JBOSS_HOME/bin/jboss-cli.sh --connect --file="doc/configuration/wildfly/datasources-cli.txt"
+
+$JBOSS_HOME/bin/jboss-cli.sh --connect --command="deploy build/ears/echothree.ear
+
+$JBOSS_HOME/bin/jboss-cli.sh --connect --command="deploy build/service/job/jar/echothree-job.jar
+
+$JBOSS_HOME/bin/jboss-cli.sh --connect --command="deploy build/ui/web/cms/war/cms.war
+$JBOSS_HOME/bin/jboss-cli.sh --connect --command="deploy build/service/graphql/war/graphql.war
+$JBOSS_HOME/bin/jboss-cli.sh --connect --command="deploy build/ui/web/letter/war/letter.war
+$JBOSS_HOME/bin/jboss-cli.sh --connect --command="deploy build/ui/web/main/war/main.war
+
