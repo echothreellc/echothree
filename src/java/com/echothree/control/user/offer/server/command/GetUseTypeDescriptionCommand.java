0b8bd51aeaf520092429422f0bc8466673b9cbf5
Richard Harms <rich@echothree.com>
2020-09-17 21:53:39 -0500

    #138: Corrected import for OfferControl in GetUseTypeDescription UC

--- src/java/com/echothree/control/user/offer/server/command/GetUseTypeDescriptionCommand.java
+++ src/java/com/echothree/control/user/offer/server/command/GetUseTypeDescriptionCommand.java
@@ -19,7 +19,7 @@ package com.echothree.control.user.offer.server.command;
 import com.echothree.control.user.offer.common.form.GetUseTypeDescriptionForm;
 import com.echothree.control.user.offer.common.result.OfferResultFactory;
 import com.echothree.control.user.offer.common.result.GetUseTypeDescriptionResult;
-import com.echothree.model.control.offer.server.OfferControl;
+import com.echothree.model.control.offer.server.control.OfferControl;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.party.server.PartyControl;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
