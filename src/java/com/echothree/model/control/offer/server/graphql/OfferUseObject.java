e3f6218b1e973af6d691e9bb72192eab0ecab01c
Richard Harms <rich@echothree.com>
2020-10-19 12:45:49 -0500

    #133: Added OfferUse object

---
+++ src/java/com/echothree/model/control/offer/server/graphql/OfferUseObject.java
@@ -0,0 +1,75 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2020 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.offer.server.graphql;
+
+import com.echothree.model.control.graphql.server.graphql.BaseEntityInstanceObject;
+import com.echothree.model.control.graphql.server.util.GraphQlContext;
+import com.echothree.model.control.offer.server.control.UseControl;
+import com.echothree.model.control.user.server.control.UserControl;
+import com.echothree.model.data.offer.server.entity.OfferUse;
+import com.echothree.model.data.offer.server.entity.OfferUseDetail;
+import com.echothree.model.data.offer.server.entity.Use;
+import com.echothree.model.data.offer.server.entity.UseDetail;
+import com.echothree.util.server.persistence.Session;
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.schema.DataFetchingEnvironment;
+
+@GraphQLDescription("offer use object")
+@GraphQLName("OfferUse")
+public class OfferUseObject
+        extends BaseEntityInstanceObject {
+
+    private final OfferUse offerUse; // Always Present
+
+    public OfferUseObject(OfferUse offerUse) {
+        super(offerUse.getPrimaryKey());
+        
+        this.offerUse = offerUse;
+    }
+
+    private OfferUseDetail offerUseDetail; // Optional, offerUse getOfferUseDetail()
+    
+    private OfferUseDetail getOfferUseDetail() {
+        if(offerUseDetail == null) {
+            offerUseDetail = offerUse.getLastDetail();
+        }
+        
+        return offerUseDetail;
+    }
+
+    @GraphQLField
+    @GraphQLDescription("offer")
+    public OfferObject getOffer(final DataFetchingEnvironment env) {
+        return OfferSecurityUtils.getInstance().getHasUseAccess(env) ? new OfferObject(getOfferUseDetail().getOffer()) : null;
+    }
+
+    @GraphQLField
+    @GraphQLDescription("use")
+    public UseObject getUse(final DataFetchingEnvironment env) {
+        return OfferSecurityUtils.getInstance().getHasUseAccess(env) ? new UseObject(getOfferUseDetail().getUse()) : null;
+    }
+    
+//    @GraphQLField
+//    @GraphQLDescription("sales order sequence")
+//    public SequenceObject getSalesOrderSequence(final DataFetchingEnvironment env) {
+//        return SequenceSecurityUtils.getInstance().getHasSequenceAccess(env) ? new SequenceObject(getOfferSequenceDetail().getSequence()) : null;
+//    }
+
+}
