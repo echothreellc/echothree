ad9c76858d4e8608c22636fc383d272ae75b1035
Richard Harms <rich@echothree.com>
2020-10-27 12:50:53 -0500

    #150: Added GraphQL object and queries for Selector Kinds

---
+++ src/java/com/echothree/model/control/selector/server/graphql/SelectorKindObject.java
@@ -0,0 +1,87 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2020 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.selector.server.graphql;
+
+import com.echothree.model.control.selector.server.control.SelectorControl;
+import com.echothree.model.control.graphql.server.graphql.BaseEntityInstanceObject;
+import com.echothree.model.control.graphql.server.util.GraphQlContext;
+import com.echothree.model.control.user.server.control.UserControl;
+import com.echothree.model.data.selector.server.entity.SelectorKind;
+import com.echothree.model.data.selector.server.entity.SelectorKindDetail;
+import com.echothree.util.server.persistence.Session;
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.schema.DataFetchingEnvironment;
+
+@GraphQLDescription("selector kind object")
+@GraphQLName("SelectorKind")
+public class SelectorKindObject
+        extends BaseEntityInstanceObject {
+    
+    private final SelectorKind selectorKind; // Always Present
+    
+    public SelectorKindObject(SelectorKind selectorKind) {
+        super(selectorKind.getPrimaryKey());
+        
+        this.selectorKind = selectorKind;
+    }
+
+    private SelectorKindDetail selectorKindDetail; // Optional, use getSelectorKindDetail()
+    
+    private SelectorKindDetail getSelectorKindDetail() {
+        if(selectorKindDetail == null) {
+            selectorKindDetail = selectorKind.getLastDetail();
+        }
+        
+        return selectorKindDetail;
+    }
+    
+    @GraphQLField
+    @GraphQLDescription("selector kind name")
+    @GraphQLNonNull
+    public String getSelectorKindName() {
+        return getSelectorKindDetail().getSelectorKindName();
+    }
+    
+    @GraphQLField
+    @GraphQLDescription("is default")
+    @GraphQLNonNull
+    public boolean getIsDefault() {
+        return getSelectorKindDetail().getIsDefault();
+    }
+    
+    @GraphQLField
+    @GraphQLDescription("sort order")
+    @GraphQLNonNull
+    public int getSortOrder() {
+        return getSelectorKindDetail().getSortOrder();
+    }
+    
+    @GraphQLField
+    @GraphQLDescription("description")
+    @GraphQLNonNull
+    public String getDescription(final DataFetchingEnvironment env) {
+        var selectorControl = (SelectorControl)Session.getModelController(SelectorControl.class);
+        var userControl = (UserControl)Session.getModelController(UserControl.class);
+        GraphQlContext context = env.getContext();
+        
+        return selectorControl.getBestSelectorKindDescription(selectorKind, userControl.getPreferredLanguageFromUserVisit(context.getUserVisit()));
+    }
+    
+}
