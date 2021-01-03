323eec9e65f98465dd1c86002d06e0d011eb676f
Richard Harms <rich@echothree.com>
2021-01-02 21:26:14 -0600

    #260: Added null check for stateZipCodeList in run(), SmartQueues can return null

--- ui/cli/dataloader/src/java/com/echothree/ui/cli/dataloader/util/zipcode/ConsumerThread.java
+++ ui/cli/dataloader/src/java/com/echothree/ui/cli/dataloader/util/zipcode/ConsumerThread.java
@@ -16,16 +16,7 @@
 
 package com.echothree.ui.cli.dataloader.util.zipcode;
 
-import com.echothree.control.user.geo.common.form.AddCityToCountyForm;
-import com.echothree.control.user.geo.common.form.AddZipCodeToCityForm;
-import com.echothree.control.user.geo.common.form.CreateCityForm;
-import com.echothree.control.user.geo.common.form.CreateCountyForm;
-import com.echothree.control.user.geo.common.form.CreateZipCodeForm;
 import com.echothree.control.user.geo.common.form.GeoFormFactory;
-import com.echothree.control.user.geo.common.form.GetCityForm;
-import com.echothree.control.user.geo.common.form.GetCountyForm;
-import com.echothree.control.user.geo.common.form.GetStateForm;
-import com.echothree.control.user.geo.common.form.GetZipCodeForm;
 import com.echothree.control.user.geo.common.result.CreateCityResult;
 import com.echothree.control.user.geo.common.result.CreateCountyResult;
 import com.echothree.control.user.geo.common.result.CreateZipCodeResult;
@@ -35,26 +26,24 @@ import com.echothree.control.user.geo.common.result.GetStateResult;
 import com.echothree.control.user.geo.common.result.GetZipCodeResult;
 import com.echothree.model.data.user.common.pk.UserVisitPK;
 import com.echothree.util.common.collection.SmartQueue;
-import com.echothree.util.common.string.StringUtils;
-import com.echothree.util.common.command.CommandResult;
-import com.echothree.util.common.command.ExecutionResult;
 import com.echothree.util.common.message.Message;
 import com.echothree.util.common.message.Messages;
-import java.util.ArrayList;
+import com.echothree.util.common.string.StringUtils;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class ConsumerThread
         extends Thread {
     
-    private SmartQueue queue;
-    private ZipCodeParser zipCodeParser;
+    private final SmartQueue<List<ZipCodeData>> queue;
+    private final ZipCodeParser zipCodeParser;
     private UserVisitPK userVisitPK;
     
     /** Creates a new instance of ConsumerThread */
-    public ConsumerThread(SmartQueue queue, ZipCodeParser zipCodeParser) {
+    public ConsumerThread(SmartQueue<List<ZipCodeData>> queue, ZipCodeParser zipCodeParser) {
         this.queue = queue;
         this.zipCodeParser = zipCodeParser;
     }
@@ -76,23 +65,23 @@ public class ConsumerThread
     }
     
     private String loadZipCode(ZipCodeData zipCodeData) {
-        String zipCodeName = zipCodeData.getZipCode();
-        String zipCodeGeoCodeName = (String)zipCodeParser.getZipCodeGeoCodeNames().get(zipCodeName);
+        var zipCodeName = zipCodeData.getZipCode();
+        var zipCodeGeoCodeName = (String)zipCodeParser.getZipCodeGeoCodeNames().get(zipCodeName);
         
         if(zipCodeGeoCodeName == null) {
-            GetZipCodeForm getZipCodeForm = GeoFormFactory.getGetZipCodeForm();
+            var getZipCodeForm = GeoFormFactory.getGetZipCodeForm();
             
             getZipCodeForm.setCountryGeoCodeName(zipCodeParser.getCountryGeoCodeName());
             getZipCodeForm.setZipCodeName(zipCodeName);
-            
-            CommandResult commandResult = zipCodeParser.getGeoService().getZipCode(getUserVisit(), getZipCodeForm);
-            ExecutionResult executionResult = commandResult.getExecutionResult();
-            GetZipCodeResult getZipCodeResult = (GetZipCodeResult)executionResult.getResult();
+
+            var commandResult = zipCodeParser.getGeoService().getZipCode(getUserVisit(), getZipCodeForm);
+            var executionResult = commandResult.getExecutionResult();
+            var getZipCodeResult = (GetZipCodeResult)executionResult.getResult();
             var postalCode = getZipCodeResult.getPostalCode();
             zipCodeGeoCodeName = postalCode == null ? null : postalCode.getGeoCodeName();
 
             if(zipCodeGeoCodeName == null) {
-                CreateZipCodeForm createZipCodeForm = GeoFormFactory.getCreateZipCodeForm();
+                var createZipCodeForm = GeoFormFactory.getCreateZipCodeForm();
                 
                 createZipCodeForm.setCountryGeoCodeName(zipCodeParser.getCountryGeoCodeName());
                 createZipCodeForm.setZipCodeName(zipCodeName);
@@ -102,7 +91,7 @@ public class ConsumerThread
                 
                 commandResult = zipCodeParser.getGeoService().createZipCode(getUserVisit(), createZipCodeForm);
                 executionResult = commandResult.getExecutionResult();
-                CreateZipCodeResult createZipCodeResult = (CreateZipCodeResult)executionResult.getResult();
+                var createZipCodeResult = (CreateZipCodeResult)executionResult.getResult();
                 zipCodeGeoCodeName = createZipCodeResult.getGeoCodeName();
             }
             
@@ -118,13 +107,13 @@ public class ConsumerThread
     }
     
     private String loadCounty(String stateGeoCodeName, ZipCodeData zipCodeData) {
-        String description = zipCodeData.getCountyName();
+        var description = zipCodeData.getCountyName();
         String countyGeoCodeName = null;
         
         if(description != null) {
-            String countyNumber = zipCodeData.getCountyNumber();
-            String countyName = StringUtils.getInstance().cleanStringToName(description);
-            Map countiesByState = (Map)zipCodeParser.getCountyGeoCodeNames().get(stateGeoCodeName);
+            var countyNumber = zipCodeData.getCountyNumber();
+            var countyName = StringUtils.getInstance().cleanStringToName(description);
+            var countiesByState = (Map)zipCodeParser.getCountyGeoCodeNames().get(stateGeoCodeName);
             
             if(countiesByState == null) {
                 countiesByState = Collections.synchronizedMap(new HashMap());
@@ -134,19 +123,19 @@ public class ConsumerThread
             countyGeoCodeName = (String)countiesByState.get(countyName);
             
             if(countyGeoCodeName == null && countyNumber.length() > 0 && countyName.length() > 0) {
-                GetCountyForm getCountyForm = GeoFormFactory.getGetCountyForm();
+                var getCountyForm = GeoFormFactory.getGetCountyForm();
                 
                 getCountyForm.setStateGeoCodeName(stateGeoCodeName);
                 getCountyForm.setCountyName(countyName);
-                
-                CommandResult commandResult = zipCodeParser.getGeoService().getCounty(getUserVisit(), getCountyForm);
-                ExecutionResult executionResult = commandResult.getExecutionResult();
-                GetCountyResult getCountyResult = (GetCountyResult)executionResult.getResult();
+
+                var commandResult = zipCodeParser.getGeoService().getCounty(getUserVisit(), getCountyForm);
+                var executionResult = commandResult.getExecutionResult();
+                var getCountyResult = (GetCountyResult)executionResult.getResult();
                 var country = getCountyResult.getCounty();
                 countyGeoCodeName = country == null ? null : country.getGeoCodeName();
                 
                 if(countyGeoCodeName == null) {
-                    CreateCountyForm createCountyForm = GeoFormFactory.getCreateCountyForm();
+                    var createCountyForm = GeoFormFactory.getCreateCountyForm();
                     
                     createCountyForm.setStateGeoCodeName(stateGeoCodeName);
                     createCountyForm.setCountyName(countyName);
@@ -157,7 +146,7 @@ public class ConsumerThread
                     
                     commandResult = zipCodeParser.getGeoService().createCounty(getUserVisit(), createCountyForm);
                     executionResult = commandResult.getExecutionResult();
-                    CreateCountyResult createCountyResult = (CreateCountyResult)executionResult.getResult();
+                    var createCountyResult = (CreateCountyResult)executionResult.getResult();
                     countyGeoCodeName = createCountyResult.getGeoCodeName();
                 }
                 
@@ -174,8 +163,8 @@ public class ConsumerThread
     }
     
     private String loadCity(String stateGeoCodeName, ZipCodeData zipCodeData) {
-        String description = zipCodeData.getCityStateName();
-        String cityName = StringUtils.getInstance().cleanStringToName(description);
+        var description = zipCodeData.getCityStateName();
+        var cityName = StringUtils.getInstance().cleanStringToName(description);
         Map citiesByState = (Map)zipCodeParser.getCityGeoCodeNames().get(stateGeoCodeName);
         
         if(citiesByState == null) {
@@ -186,19 +175,19 @@ public class ConsumerThread
         var cityGeoCodeName = (String)citiesByState.get(cityName);
         
         if(cityGeoCodeName == null) {
-            GetCityForm getCityForm = GeoFormFactory.getGetCityForm();
+            var getCityForm = GeoFormFactory.getGetCityForm();
             
             getCityForm.setStateGeoCodeName(stateGeoCodeName);
             getCityForm.setCityName(cityName);
-            
-            CommandResult commandResult = zipCodeParser.getGeoService().getCity(getUserVisit(), getCityForm);
-            ExecutionResult executionResult = commandResult.getExecutionResult();
-            GetCityResult getCityResult = (GetCityResult)executionResult.getResult();
+
+            var commandResult = zipCodeParser.getGeoService().getCity(getUserVisit(), getCityForm);
+            var executionResult = commandResult.getExecutionResult();
+            var getCityResult = (GetCityResult)executionResult.getResult();
             var city = getCityResult.getCity();
             cityGeoCodeName = city == null ? null : city.getGeoCodeName();
             
             if(cityGeoCodeName == null) {
-                CreateCityForm createCityForm = GeoFormFactory.getCreateCityForm();
+                var createCityForm = GeoFormFactory.getCreateCityForm();
                 
                 createCityForm.setStateGeoCodeName(stateGeoCodeName);
                 createCityForm.setCityName(cityName);
@@ -208,7 +197,7 @@ public class ConsumerThread
                 
                 commandResult = zipCodeParser.getGeoService().createCity(getUserVisit(), createCityForm);
                 executionResult = commandResult.getExecutionResult();
-                CreateCityResult createCityResult = (CreateCityResult)executionResult.getResult();
+                var createCityResult = (CreateCityResult)executionResult.getResult();
                 cityGeoCodeName = createCityResult.getGeoCodeName();
             }
             
@@ -224,18 +213,18 @@ public class ConsumerThread
     }
     
     private String getStateGeoCodeName(ZipCodeData zipCodeData) {
-        String postal2Letter = zipCodeData.getStateAbbreviation();
-        String stateGeoCodeName = (String)zipCodeParser.getStateGeoCodeNames().get(postal2Letter);
+        var postal2Letter = zipCodeData.getStateAbbreviation();
+        var stateGeoCodeName = (String)zipCodeParser.getStateGeoCodeNames().get(postal2Letter);
         
         if(stateGeoCodeName == null) {
-            GetStateForm getStateForm = GeoFormFactory.getGetStateForm();
+            var getStateForm = GeoFormFactory.getGetStateForm();
             
             getStateForm.setCountryGeoCodeName(zipCodeParser.getCountryGeoCodeName());
             getStateForm.setPostal2Letter(postal2Letter);
-            
-            CommandResult commandResult = zipCodeParser.getGeoService().getState(getUserVisit(), getStateForm);
-            ExecutionResult executionResult = commandResult.getExecutionResult();
-            GetStateResult getStateResult = (GetStateResult)executionResult.getResult();
+
+            var commandResult = zipCodeParser.getGeoService().getState(getUserVisit(), getStateForm);
+            var executionResult = commandResult.getExecutionResult();
+            var getStateResult = (GetStateResult)executionResult.getResult();
             var state = getStateResult.getState();
             stateGeoCodeName = state == null ? null : state.getGeoCodeName();
             
@@ -253,55 +242,56 @@ public class ConsumerThread
     @Override
     public void run() {
         while(!queue.isEmpty() || !queue.onEnd()) {
-            ArrayList stateZipCodeList = (ArrayList)queue.take();
-            
-            for(Iterator iter = stateZipCodeList.iterator(); iter.hasNext();) {
-                ZipCodeData zipCodeData = (ZipCodeData)iter.next();
-                String zipCodeGeoCodeName = loadZipCode(zipCodeData);
-                
-                if(zipCodeGeoCodeName != null) {
-                    String stateGeoCodeName = getStateGeoCodeName(zipCodeData);
-                    
-                    if(stateGeoCodeName != null) {
-                        AddZipCodeToCityForm addZipCodeToCityForm = GeoFormFactory.getAddZipCodeToCityForm();
-                        String countyGeoCodeName = loadCounty(stateGeoCodeName, zipCodeData);
-                        String cityGeoCodeName = loadCity(stateGeoCodeName, zipCodeData);
-                        
-                        // TODO: there should be a way to test to see if the relationship between zip code and city exists first
-                        addZipCodeToCityForm.setCityGeoCodeName(cityGeoCodeName);
-                        addZipCodeToCityForm.setZipCodeGeoCodeName(zipCodeGeoCodeName);
-                        CommandResult commandResult = zipCodeParser.getGeoService().addZipCodeToCity(getUserVisit(), addZipCodeToCityForm);
-                        ExecutionResult executionResult = commandResult.getExecutionResult();
-                        
-                        if(executionResult.getHasErrors()) {
-                            Messages messages = executionResult.getExecutionErrors();
-                            
-                            for(Iterator executionErrors = messages.get(Messages.EXECUTION_ERROR); executionErrors.hasNext();) {
-                                Message message = (Message)executionErrors.next();
-                                
-                                if(!message.getKey().equals("AddZipCodeToCity.DuplicateGeoCodeRelationship")) {
-                                    zipCodeParser.getLog().error("addZipCodeToCity error: " + message + ", zipCodeData = " + zipCodeData);
+            var stateZipCodeList = queue.take();
+
+            if(stateZipCodeList != null) {
+                for(var zipCodeData : stateZipCodeList) {
+                    var zipCodeGeoCodeName = loadZipCode(zipCodeData);
+
+                    if(zipCodeGeoCodeName != null) {
+                        var stateGeoCodeName = getStateGeoCodeName(zipCodeData);
+
+                        if(stateGeoCodeName != null) {
+                            var addZipCodeToCityForm = GeoFormFactory.getAddZipCodeToCityForm();
+                            var countyGeoCodeName = loadCounty(stateGeoCodeName, zipCodeData);
+                            var cityGeoCodeName = loadCity(stateGeoCodeName, zipCodeData);
+
+                            // TODO: there should be a way to test to see if the relationship between zip code and city exists first
+                            addZipCodeToCityForm.setCityGeoCodeName(cityGeoCodeName);
+                            addZipCodeToCityForm.setZipCodeGeoCodeName(zipCodeGeoCodeName);
+                            var commandResult = zipCodeParser.getGeoService().addZipCodeToCity(getUserVisit(), addZipCodeToCityForm);
+                            var executionResult = commandResult.getExecutionResult();
+
+                            if(executionResult.getHasErrors()) {
+                                var messages = executionResult.getExecutionErrors();
+
+                                for(Iterator executionErrors = messages.get(Messages.EXECUTION_ERROR); executionErrors.hasNext(); ) {
+                                    var message = (Message)executionErrors.next();
+
+                                    if(!message.getKey().equals("AddZipCodeToCity.DuplicateGeoCodeRelationship")) {
+                                        zipCodeParser.getLog().error("addZipCodeToCity error: " + message + ", zipCodeData = " + zipCodeData);
+                                    }
                                 }
                             }
-                        }
-                        
-                        if(countyGeoCodeName != null) {
-                            AddCityToCountyForm addCityToCountyForm = GeoFormFactory.getAddCityToCountyForm();
-                            
-                            // TODO: there should be a way to test to see if the relationship between city and county exists first
-                            addCityToCountyForm.setCityGeoCodeName(cityGeoCodeName);
-                            addCityToCountyForm.setCountyGeoCodeName(countyGeoCodeName);
-                            commandResult = zipCodeParser.getGeoService().addCityToCounty(getUserVisit(), addCityToCountyForm);
-                            executionResult = commandResult.getExecutionResult();
-                            
-                            if(executionResult.getHasErrors()) {
-                                Messages messages = executionResult.getExecutionErrors();
-                                
-                                for(Iterator executionErrors = messages.get(Messages.EXECUTION_ERROR); executionErrors.hasNext();) {
-                                    Message message = (Message)executionErrors.next();
-                                    
-                                    if(!message.getKey().equals("AddCityToCounty.DuplicateGeoCodeRelationship")) {
-                                        zipCodeParser.getLog().error("addCityToCounty error: " + message + ", zipCodeData = " + zipCodeData);
+
+                            if(countyGeoCodeName != null) {
+                                var addCityToCountyForm = GeoFormFactory.getAddCityToCountyForm();
+
+                                // TODO: there should be a way to test to see if the relationship between city and county exists first
+                                addCityToCountyForm.setCityGeoCodeName(cityGeoCodeName);
+                                addCityToCountyForm.setCountyGeoCodeName(countyGeoCodeName);
+                                commandResult = zipCodeParser.getGeoService().addCityToCounty(getUserVisit(), addCityToCountyForm);
+                                executionResult = commandResult.getExecutionResult();
+
+                                if(executionResult.getHasErrors()) {
+                                    var messages = executionResult.getExecutionErrors();
+
+                                    for(Iterator executionErrors = messages.get(Messages.EXECUTION_ERROR); executionErrors.hasNext(); ) {
+                                        var message = (Message)executionErrors.next();
+
+                                        if(!message.getKey().equals("AddCityToCounty.DuplicateGeoCodeRelationship")) {
+                                            zipCodeParser.getLog().error("addCityToCounty error: " + message + ", zipCodeData = " + zipCodeData);
+                                        }
                                     }
                                 }
                             }
