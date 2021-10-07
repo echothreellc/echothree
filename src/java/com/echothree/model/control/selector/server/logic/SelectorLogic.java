eabfb5ead6621b0fd4e2cb8036f9a3f73c0ad8ba
Richard Harms <rich@echothree.com>
2021-10-07 18:49:45 -0500

    #152: Added additional methods to get Selectors in SelectorLogic

--- src/java/com/echothree/model/control/selector/server/logic/SelectorLogic.java
+++ src/java/com/echothree/model/control/selector/server/logic/SelectorLogic.java
@@ -16,14 +16,26 @@
 
 package com.echothree.model.control.selector.server.logic;
 
+import com.echothree.control.user.selector.common.spec.SelectorUniversalSpec;
+import com.echothree.model.control.core.common.ComponentVendors;
+import com.echothree.model.control.core.common.EntityTypes;
+import com.echothree.model.control.core.common.exception.InvalidParameterCountException;
+import com.echothree.model.control.core.server.logic.EntityInstanceLogic;
+import com.echothree.model.control.selector.common.exception.UnknownDefaultSelectorException;
+import com.echothree.model.control.selector.common.exception.UnknownDefaultSelectorKindException;
+import com.echothree.model.control.selector.common.exception.UnknownDefaultSelectorTypeException;
 import com.echothree.model.control.selector.common.exception.UnknownSelectorNameException;
 import com.echothree.model.control.selector.server.control.SelectorControl;
+import com.echothree.model.data.core.server.entity.EntityInstance;
 import com.echothree.model.data.selector.server.entity.Selector;
+import com.echothree.model.data.selector.server.entity.SelectorKind;
 import com.echothree.model.data.selector.server.entity.SelectorType;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.server.control.BaseLogic;
 import com.echothree.util.server.message.ExecutionErrorAccumulator;
+import com.echothree.util.server.persistence.EntityPermission;
 import com.echothree.util.server.persistence.Session;
+import com.echothree.util.server.validation.ParameterUtils;
 
 public class SelectorLogic
         extends BaseLogic {
@@ -39,11 +51,11 @@ public class SelectorLogic
     public static SelectorLogic getInstance() {
         return SelectorLogicHolder.instance;
     }
-    
-    public Selector getSelectorByName(final ExecutionErrorAccumulator eea, final SelectorType selectorType,
-            final String selectorName) {
+
+    public Selector getSelectorByName(final ExecutionErrorAccumulator eea, final SelectorType selectorType, final String selectorName,
+            final EntityPermission entityPermission) {
         var selectorControl = Session.getModelController(SelectorControl.class);
-        var selector = selectorControl.getSelectorByName(selectorType, selectorName);
+        var selector = selectorControl.getSelectorByName(selectorType, selectorName, entityPermission);
 
         if(selector == null) {
             var selectorTypeDetail = selectorType.getLastDetail();
@@ -56,16 +68,115 @@ public class SelectorLogic
         return selector;
     }
 
+    public Selector getSelectorByName(final ExecutionErrorAccumulator eea, final SelectorType selectorType, final String selectorName) {
+        return getSelectorByName(eea, selectorType, selectorName, EntityPermission.READ_ONLY);
+    }
+
+    public Selector getSelectorByNameForUpdate(final ExecutionErrorAccumulator eea, final SelectorType selectorType, final String selectorName) {
+        return getSelectorByName(eea, selectorType, selectorName, EntityPermission.READ_WRITE);
+    }
+
     public Selector getSelectorByName(final ExecutionErrorAccumulator eea, final String selectorKindName,
-            final String selectorTypeName, final String selectorName) {
+            final String selectorTypeName, final String selectorName, final EntityPermission entityPermission) {
         var selectorType = SelectorTypeLogic.getInstance().getSelectorTypeByName(eea, selectorKindName, selectorTypeName);
         Selector selector = null;
 
-        if(eea == null || !eea.hasExecutionErrors()) {
-            selector = getSelectorByName(eea, selectorType, selectorName);
+        if(!eea.hasExecutionErrors()) {
+            selector = getSelectorByName(eea, selectorType, selectorName, entityPermission);
         }
 
         return selector;
+    }
+
+    public Selector getSelectorByName(final ExecutionErrorAccumulator eea, final String selectorKindName,
+            final String selectorType, final String selectorName) {
+        return getSelectorByName(eea, selectorKindName, selectorType, selectorName, EntityPermission.READ_ONLY);
+    }
+
+    public Selector getSelectorByNameForUpdate(final ExecutionErrorAccumulator eea, final String selectorKindName,
+            final String selectorType, final String selectorName) {
+        return getSelectorByName(eea, selectorKindName, selectorType, selectorName, EntityPermission.READ_WRITE);
+    }
+
+    public Selector getSelectorByUniversalSpec(final ExecutionErrorAccumulator eea, final SelectorUniversalSpec universalSpec,
+            final boolean allowDefault, final EntityPermission entityPermission) {
+        var selectorControl = Session.getModelController(SelectorControl.class);
+        var selectorKindName = universalSpec.getSelectorKindName();
+        var selectorTypeName = universalSpec.getSelectorTypeName();
+        var selectorName = universalSpec.getSelectorName();
+        var nameParameterCount= ParameterUtils.getInstance().countNonNullParameters(selectorKindName, selectorTypeName, selectorName);
+        var possibleEntitySpecs= EntityInstanceLogic.getInstance().countPossibleEntitySpecs(universalSpec);
+        Selector selector = null;
+
+        if(nameParameterCount < 4 && possibleEntitySpecs == 0) {
+            SelectorKind selectorKind = null;
+            SelectorType selectorType = null;
+
+            if(selectorKindName == null) {
+                if(allowDefault) {
+                    selectorKind = selectorControl.getDefaultSelectorKind();
+
+                    if(selectorKind == null) {
+                        handleExecutionError(UnknownDefaultSelectorKindException.class, eea, ExecutionErrors.UnknownDefaultSelectorKind.name());
+                    }
+                } else {
+                    handleExecutionError(InvalidParameterCountException.class, eea, ExecutionErrors.InvalidParameterCount.name());
+                }
+            } else {
+                selectorKind = SelectorKindLogic.getInstance().getSelectorKindByName(eea, selectorKindName);
+            }
+
+            if(selectorTypeName == null) {
+                if(allowDefault) {
+                    selectorType = selectorControl.getDefaultSelectorType(selectorKind);
+
+                    if(selectorType == null) {
+                        handleExecutionError(UnknownDefaultSelectorTypeException.class, eea, ExecutionErrors.UnknownDefaultSelectorType.name());
+                    }
+                } else {
+                    handleExecutionError(InvalidParameterCountException.class, eea, ExecutionErrors.InvalidParameterCount.name());
+                }
+            } else {
+                selectorType = SelectorTypeLogic.getInstance().getSelectorTypeByName(eea, selectorKind, selectorTypeName);
+            }
+
+            if(!eea.hasExecutionErrors()) {
+                if(selectorName == null) {
+                    if(allowDefault) {
+                        selector = selectorControl.getDefaultSelector(selectorType, entityPermission);
+
+                        if(selector == null) {
+                            handleExecutionError(UnknownDefaultSelectorException.class, eea, ExecutionErrors.UnknownDefaultSelector.name());
+                        }
+                    } else {
+                        handleExecutionError(InvalidParameterCountException.class, eea, ExecutionErrors.InvalidParameterCount.name());
+                    }
+                } else {
+                    selector = getSelectorByName(eea, selectorType, selectorName, entityPermission);
+                }
+            }
+        } else if(nameParameterCount == 0 && possibleEntitySpecs == 1) {
+            EntityInstance entityInstance = EntityInstanceLogic.getInstance().getEntityInstance(eea, universalSpec,
+                    ComponentVendors.ECHOTHREE.name(), EntityTypes.Selector.name());
+
+            if(!eea.hasExecutionErrors()) {
+                selector = selectorControl.getSelectorByEntityInstance(entityInstance, entityPermission);
+            }
+        } else {
+            handleExecutionError(InvalidParameterCountException.class, eea, ExecutionErrors.InvalidParameterCount.name());
+        }
+
+        return selector;
+    }
+
+    public Selector getSelectorByUniversalSpec(final ExecutionErrorAccumulator eea, final SelectorUniversalSpec universalSpec,
+            boolean allowDefault) {
+        return getSelectorByUniversalSpec(eea, universalSpec, allowDefault, EntityPermission.READ_ONLY);
+    }
+
+    public Selector getSelectorByUniversalSpecForUpdate(final ExecutionErrorAccumulator eea, final SelectorUniversalSpec universalSpec,
+            boolean allowDefault) {
+        return getSelectorByUniversalSpec(eea, universalSpec, allowDefault, EntityPermission.READ_WRITE);
     }
 
 }
