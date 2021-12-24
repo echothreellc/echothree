b2d0329b6e5652c63df94ee90f380ca4e2c4a76f
Richard Harms <rich@echothree.com>
2021-12-24 15:47:52 -0600

    #370: Converted additional tests to use text blocks

--- test/junit/src/java/com/echothree/control/user/test/core/graphql/EntityInstanceTest.java
+++ test/junit/src/java/com/echothree/control/user/test/core/graphql/EntityInstanceTest.java
@@ -30,88 +30,170 @@ public class EntityInstanceTest
     @Test
     public void entityTypeWithInstancesQueryUsingNames()
             throws Exception {
-        var loginBody = executeUsingPost("mutation { employeeLogin(input: { username: \"test e\", password: \"password\", companyName: \"TEST_COMPANY\", clientMutationId: \"1\" }) { hasErrors } }");
+        var loginBody = executeUsingPost("""
+                mutation {
+                    employeeLogin(input: { username: "test e", password: "password", companyName: "TEST_COMPANY", clientMutationId: "1" }) {
+                        hasErrors
+                    }
+                }
+                """);
+        
         assertThat(getBoolean(loginBody, "data.employeeLogin.hasErrors")).isFalse();
 
-        var entityTypeBody = executeUsingPost("query { entityType(componentVendorName: \"" + ComponentVendors.ECHOTHREE + "\", entityTypeName: \"" + EntityTypes.GlAccount + "\") { componentVendor { componentVendorName } entityTypeName entityInstanceCount entityInstances { ulid } } }");
+        var entityTypeBody = executeUsingPost("""
+                query {
+                    entityType(componentVendorName: "%s", entityTypeName: "%s") {
+                        componentVendor {
+                            componentVendorName
+                        }
+                        entityTypeName
+                        entityInstanceCount
+                        entityInstances {
+                            ulid
+                        }
+                    }
+                }
+                """.formatted(ComponentVendors.ECHOTHREE, EntityTypes.GlAccount));
 
-        var componentVendorName = getString(entityTypeBody, "data.entityType.componentVendor.componentVendorName");
-        var entityTypeName = getString(entityTypeBody, "data.entityType.entityTypeName");
         var entityInstanceCount = getLong(entityTypeBody, "data.entityType.entityInstanceCount");
-        var entityInstances = getList(entityTypeBody, "data.entityType.entityInstances");
-
-        assertThat(componentVendorName).isEqualTo(ComponentVendors.ECHOTHREE.toString());
-        assertThat(entityTypeName).isEqualTo(EntityTypes.GlAccount.toString());
+        assertThat(getString(entityTypeBody, "data.entityType.componentVendor.componentVendorName")).isEqualTo(ComponentVendors.ECHOTHREE.toString());
+        assertThat(getString(entityTypeBody, "data.entityType.entityTypeName")).isEqualTo(EntityTypes.GlAccount.toString());
         assertThat(entityInstanceCount).isGreaterThan(0);
-        assertThat(entityInstances).size().isEqualTo(entityInstanceCount);
+        assertThat(getList(entityTypeBody, "data.entityType.entityInstances")).size().isEqualTo(entityInstanceCount);
     }
 
     @Test
     public void entityInstancesQueryUsingNamesNoAuth()
             throws Exception {
-        var entityInstancesBody = executeUsingPost("query { entityInstances(componentVendorName: \"" + ComponentVendors.ECHOTHREE + "\", entityTypeName: \"" + EntityTypes.GlAccount + "\") { ulid } }");
+        var entityInstancesBody = executeUsingPost("""
+                query {
+                    entityInstances(componentVendorName: "%s", entityTypeName: "%s") {
+                        ulid
+                    }
+                }
+                """.formatted(ComponentVendors.ECHOTHREE, EntityTypes.GlAccount));
 
-        var entityInstances = getList(entityInstancesBody, "data.entityInstances");
-
-        assertThat(entityInstances).size().isEqualTo(0);
+        assertThat(getList(entityInstancesBody, "data.entityInstances")).size().isEqualTo(0);
     }
 
     @Test
     public void entityInstancesQueryUsingNames()
             throws Exception {
-        var loginBody = executeUsingPost("mutation { employeeLogin(input: { username: \"test e\", password: \"password\", companyName: \"TEST_COMPANY\", clientMutationId: \"1\" }) { hasErrors } }");
+        var loginBody = executeUsingPost("""
+                mutation {
+                    employeeLogin(input: { username: "test e", password: "password", companyName: "TEST_COMPANY", clientMutationId: "1" }) {
+                        hasErrors
+                    }
+                }
+                """);
+        
         assertThat(getBoolean(loginBody, "data.employeeLogin.hasErrors")).isFalse();
 
-        var entityInstancesBody = executeUsingPost("query { entityInstances(componentVendorName: \"" + ComponentVendors.ECHOTHREE + "\", entityTypeName: \"" + EntityTypes.GlAccount + "\") { ulid } }");
+        var entityInstancesBody = executeUsingPost("""
+                query {
+                    entityInstances(componentVendorName: "%s", entityTypeName: "%s") {
+                       ulid
+                    }
+                }
+                """.formatted(ComponentVendors.ECHOTHREE, EntityTypes.GlAccount));
 
-        var entityInstances = getList(entityInstancesBody, "data.entityInstances");
-
-        assertThat(entityInstances).size().isGreaterThan(0);
+        assertThat(getList(entityInstancesBody, "data.entityInstances")).size().isGreaterThan(0);
     }
 
     @Test
     public void entityInstanceQueryUsingIdNoAuth()
             throws Exception {
-        var loginBody = executeUsingPost("mutation { employeeLogin(input: { username: \"test e\", password: \"password\", companyName: \"TEST_COMPANY\", clientMutationId: \"1\" }) { hasErrors } }");
+        var loginBody = executeUsingPost("""
+                mutation {
+                    employeeLogin(input: { username: "test e", password: "password", companyName: "TEST_COMPANY", clientMutationId: "1" }) {
+                        hasErrors
+                    }
+                }
+                """);
+        
         assertThat(getBoolean(loginBody, "data.employeeLogin.hasErrors")).isFalse();
 
-        var entityTypeBody = executeUsingPost("query { entityType(componentVendorName: \"" + ComponentVendors.ECHOTHREE + "\", entityTypeName: \"" + EntityTypes.GlAccount + "\") { entityInstances { ulid } } }");
+        var entityTypeBody = executeUsingPost("""
+                query {
+                    entityType(componentVendorName: "%s", entityTypeName: "%s") {
+                        entityInstances {
+                            ulid
+                        }
+                    }
+                }
+                """.formatted(ComponentVendors.ECHOTHREE, EntityTypes.GlAccount));
 
         var entityInstances = getList(entityTypeBody, "data.entityType.entityInstances");
         var entityInstance = getObject(entityInstances, "[0]");
         var ulid = getString(entityInstance, "ulid");
-
-        var logoutBody = executeUsingPost("mutation { logout(input: { clientMutationId: \"1\" }) { hasErrors } }");
+        var logoutBody = executeUsingPost("""
+                mutation {
+                    logout(input: { clientMutationId: "1" }) {
+                        hasErrors
+                    }
+                }
+                """);
+        
         assertThat(getBoolean(logoutBody, "data.logout.hasErrors")).isFalse();
 
-        var entityInstanceBody = executeUsingPost("query { entityInstance(id: \"" + ulid + "\") { entityType { entityTypeName componentVendor { componentVendorName } } } }");
+        var entityInstanceBody = executeUsingPost("""
+                query {
+                    entityInstance(id: "%s") {
+                        entityType {
+                            entityTypeName
+                            componentVendor {
+                                componentVendorName
+                            }
+                        }
+                    }
+                }
+                """.formatted(ulid));
 
-        var componentVendorName = getString(entityInstanceBody, "data.entityInstance.entityType.componentVendor.componentVendorName");
-        var entityTypeName = getString(entityInstanceBody, "data.entityInstance.entityType.entityTypeName");
-
-        assertThat(componentVendorName).isEqualTo(ComponentVendors.ECHOTHREE.toString());
-        assertThat(entityTypeName).isEqualTo(EntityTypes.GlAccount.toString());
+        assertThat(getString(entityInstanceBody, "data.entityInstance.entityType.componentVendor.componentVendorName")).isEqualTo(ComponentVendors.ECHOTHREE.toString());
+        assertThat(getString(entityInstanceBody, "data.entityInstance.entityType.entityTypeName")).isEqualTo(EntityTypes.GlAccount.toString());
     }
 
     @Test
     public void entityInstanceQueryUsingId()
             throws Exception {
-        var loginBody = executeUsingPost("mutation { employeeLogin(input: { username: \"test e\", password: \"password\", companyName: \"TEST_COMPANY\", clientMutationId: \"1\" }) { hasErrors } }");
+        var loginBody = executeUsingPost("""
+            mutation {
+                employeeLogin(input: { username: "test e", password: "password", companyName: "TEST_COMPANY", clientMutationId: "1" }) {
+                    hasErrors
+                }
+            }
+            """);
+        
         assertThat(getBoolean(loginBody, "data.employeeLogin.hasErrors")).isFalse();
 
-        var entityTypeBody = executeUsingPost("query { entityType(componentVendorName: \"" + ComponentVendors.ECHOTHREE + "\", entityTypeName: \"" + EntityTypes.GlAccount + "\") { entityInstances { ulid } } }");
+        var entityTypeBody = executeUsingPost("""
+            query {
+                entityType(componentVendorName: "%s", entityTypeName: "%s") {
+                    entityInstances {
+                        ulid
+                    }
+                }
+            }
+            """.formatted(ComponentVendors.ECHOTHREE, EntityTypes.GlAccount));
 
         var entityInstances = getList(entityTypeBody, "data.entityType.entityInstances");
         var entityInstance = getObject(entityInstances, "[0]");
         var ulid = getString(entityInstance, "ulid");
+        var entityInstanceBody = executeUsingPost("""
+            query {
+                entityInstance(id: "%s) {
+                    entityType {
+                        entityTypeName
+                        componentVendor {
+                            componentVendorName
+                        }
+                    }
+                }
+            }
+            """.formatted(ulid));
 
-        var entityInstanceBody = executeUsingPost("query { entityInstance(id: \"" + ulid + "\") { entityType { entityTypeName componentVendor { componentVendorName } } } }");
-
-        var componentVendorName = getString(entityInstanceBody, "data.entityInstance.entityType.componentVendor.componentVendorName");
-        var entityTypeName = getString(entityInstanceBody, "data.entityInstance.entityType.entityTypeName");
-
-        assertThat(componentVendorName).isEqualTo(ComponentVendors.ECHOTHREE.toString());
-        assertThat(entityTypeName).isEqualTo(EntityTypes.GlAccount.toString());
+        assertThat(getString(entityInstanceBody, "data.entityInstance.entityType.componentVendor.componentVendorName")).isEqualTo(ComponentVendors.ECHOTHREE.toString());
+        assertThat(getString(entityInstanceBody, "data.entityInstance.entityType.entityTypeName")).isEqualTo(EntityTypes.GlAccount.toString());
     }
 
 }
