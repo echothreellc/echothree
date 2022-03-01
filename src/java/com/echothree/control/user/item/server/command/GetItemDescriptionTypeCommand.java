90b18b617d0f9338ecb001bce6aa64e7d6c2363b
Richard Harms <rich@echothree.com>
2022-03-01 13:23:52 -0500

    #412: Added comment for missing COMMAND_SECURITY_DEFINITION

--- src/java/com/echothree/control/user/item/server/command/GetItemDescriptionTypeCommand.java
+++ src/java/com/echothree/control/user/item/server/command/GetItemDescriptionTypeCommand.java
@@ -35,7 +35,8 @@ import java.util.List;
 
 public class GetItemDescriptionTypeCommand
         extends BaseSimpleCommand<GetItemDescriptionTypeForm> {
-    
+
+    // No COMMAND_SECURITY_DEFINITION, anyone may execute this command.
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
     
     static {
