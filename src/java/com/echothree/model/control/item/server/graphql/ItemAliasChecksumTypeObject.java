5eed0f2a3ced42b616ed088f60af68ed7648af8b
Richard Harms <rich@echothree.com>
2022-08-01 22:10:45 -0500

    #506: Added ItemAliasChecksumType object

---
+++ src/java/com/echothree/model/control/item/server/graphql/ItemAliasChecksumTypeObject.java
@@ -0,0 +1,74 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2022 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.item.server.graphql;
+
+import com.echothree.model.control.graphql.server.graphql.BaseEntityInstanceObject;
+import com.echothree.model.control.item.server.control.ItemControl;
+import com.echothree.model.control.user.server.control.UserControl;
+import com.echothree.model.data.item.server.entity.ItemAliasChecksumType;
+import com.echothree.util.server.persistence.Session;
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.schema.DataFetchingEnvironment;
+
+@GraphQLDescription("item alias checksum type object")
+@GraphQLName("ItemAliasChecksumType")
+public class ItemAliasChecksumTypeObject
+        extends BaseEntityInstanceObject {
+    
+    private final ItemAliasChecksumType itemAliasChecksumType; // Always Present
+    
+    public ItemAliasChecksumTypeObject(ItemAliasChecksumType itemAliasChecksumType) {
+        super(itemAliasChecksumType.getPrimaryKey());
+        
+        this.itemAliasChecksumType = itemAliasChecksumType;
+    }
+
+    @GraphQLField
+    @GraphQLDescription("item alias checksum type name")
+    @GraphQLNonNull
+    public String getItemAliasChecksumTypeName() {
+        return itemAliasChecksumType.getItemAliasChecksumTypeName();
+    }
+
+    @GraphQLField
+    @GraphQLDescription("is default")
+    @GraphQLNonNull
+    public boolean getIsDefault() {
+        return itemAliasChecksumType.getIsDefault();
+    }
+    
+    @GraphQLField
+    @GraphQLDescription("sort order")
+    @GraphQLNonNull
+    public int getSortOrder() {
+        return itemAliasChecksumType.getSortOrder();
+    }
+    
+    @GraphQLField
+    @GraphQLDescription("description")
+    @GraphQLNonNull
+    public String getDescription(final DataFetchingEnvironment env) {
+        var itemControl = Session.getModelController(ItemControl.class);
+        var userControl = Session.getModelController(UserControl.class);
+
+        return itemControl.getBestItemAliasChecksumTypeDescription(itemAliasChecksumType, userControl.getPreferredLanguageFromUserVisit(getUserVisit(env)));
+    }
+    
+}
