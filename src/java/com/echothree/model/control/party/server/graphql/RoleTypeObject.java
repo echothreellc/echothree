c26f94057039b04e879e81f6979b4ac62cce88dc
Richard Harms <rich@echothree.com>
2023-01-19 21:50:11 -0600

    #635: Added Role Type object

---
+++ src/java/com/echothree/model/control/party/server/graphql/RoleTypeObject.java
@@ -0,0 +1,62 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2023 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.party.server.graphql;
+
+import com.echothree.model.control.graphql.server.util.BaseGraphQl;
+import com.echothree.model.control.party.server.control.PartyControl;
+import com.echothree.model.control.user.server.control.UserControl;
+import com.echothree.model.data.party.server.entity.RoleType;
+import com.echothree.util.server.persistence.Session;
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.schema.DataFetchingEnvironment;
+
+@GraphQLDescription("role type object")
+@GraphQLName("RoleType")
+public class RoleTypeObject
+        extends BaseGraphQl {
+
+    private final RoleType roleType; // Always Present
+    
+    public RoleTypeObject(RoleType roleType) {
+        this.roleType = roleType;
+    }
+    
+    @GraphQLField
+    @GraphQLDescription("party type name")
+    @GraphQLNonNull
+    public String getRoleTypeName() {
+        return roleType.getRoleTypeName();
+    }
+    
+    public RoleTypeObject getParentRoleType() {
+        return new RoleTypeObject(roleType.getParentRoleType());
+    }
+    
+    @GraphQLField
+    @GraphQLDescription("description")
+    @GraphQLNonNull
+    public String getDescription(final DataFetchingEnvironment env) {
+        var partyControl = Session.getModelController(PartyControl.class);
+        var userControl = Session.getModelController(UserControl.class);
+
+        return partyControl.getBestRoleTypeDescription(roleType, userControl.getPreferredLanguageFromUserVisit(getUserVisit(env)));
+    }
+
+}
