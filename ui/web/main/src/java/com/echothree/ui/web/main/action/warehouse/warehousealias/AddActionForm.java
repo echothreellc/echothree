7b2539c5d6e3aba6fa4f99f42a9d2115c8850ae0
Richard Harms <rich@echothree.com>
2023-06-23 17:31:49 -0500

    #809: Added Warehouse Aliases

---
+++ ui/web/main/src/java/com/echothree/ui/web/main/action/warehouse/warehousealias/AddActionForm.java
@@ -0,0 +1,115 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2023 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.ui.web.main.action.warehouse.warehousealias;
+
+import com.echothree.control.user.party.common.PartyUtil;
+import com.echothree.control.user.party.common.form.GetPartyAliasTypeChoicesForm;
+import com.echothree.control.user.party.common.result.GetPartyAliasTypeChoicesResult;
+import com.echothree.model.control.party.common.PartyTypes;
+import com.echothree.model.control.party.common.choice.PartyAliasTypeChoicesBean;
+import com.echothree.util.common.command.CommandResult;
+import com.echothree.util.common.command.ExecutionResult;
+import com.echothree.view.client.web.struts.BaseActionForm;
+import com.echothree.view.client.web.struts.sprout.annotation.SproutForm;
+import java.util.List;
+import javax.naming.NamingException;
+import org.apache.struts.util.LabelValueBean;
+
+@SproutForm(name="WarehouseAliasAdd")
+public class AddActionForm
+        extends BaseActionForm {
+    
+    private PartyAliasTypeChoicesBean partyAliasTypeChoices;
+
+    private String partyName;
+    private String partyAliasTypeChoice;
+    private String alias;
+
+    public void setupPartyAliasTypeChoices()
+            throws NamingException {
+        if(partyAliasTypeChoices == null) {
+            GetPartyAliasTypeChoicesForm form = PartyUtil.getHome().getGetPartyAliasTypeChoicesForm();
+
+            form.setPartyTypeName(PartyTypes.WAREHOUSE.name());
+            form.setDefaultPartyAliasTypeChoice(partyAliasTypeChoice);
+            form.setAllowNullChoice(Boolean.FALSE.toString());
+
+            CommandResult commandResult = PartyUtil.getHome().getPartyAliasTypeChoices(userVisitPK, form);
+            ExecutionResult executionResult = commandResult.getExecutionResult();
+            GetPartyAliasTypeChoicesResult getPartyAliasTypeChoicesResult = (GetPartyAliasTypeChoicesResult)executionResult.getResult();
+            partyAliasTypeChoices = getPartyAliasTypeChoicesResult.getPartyAliasTypeChoices();
+
+            if(partyAliasTypeChoice == null) {
+                partyAliasTypeChoice = partyAliasTypeChoices.getDefaultValue();
+            }
+        }
+    }
+    
+    /**
+     * Returns the partyName.
+     * @return the partyName
+     */
+    public String getPartyName() {
+        return partyName;
+    }
+
+    /**
+     * Sets the partyName.
+     * @param partyName the partyName to set
+     */
+    public void setPartyName(String partyName) {
+        this.partyName = partyName;
+    }
+
+    public List<LabelValueBean> getPartyAliasTypeChoices()
+            throws NamingException {
+        List<LabelValueBean> choices = null;
+        
+        setupPartyAliasTypeChoices();
+        if(partyAliasTypeChoices != null)
+            choices = convertChoices(partyAliasTypeChoices);
+        
+        return choices;
+    }
+    
+    public void setPartyAliasTypeChoice(String partyAliasTypeChoice) {
+        this.partyAliasTypeChoice = partyAliasTypeChoice;
+    }
+    
+    public String getPartyAliasTypeChoice()
+            throws NamingException {
+        setupPartyAliasTypeChoices();
+        return partyAliasTypeChoice;
+    }
+
+    /**
+     * Returns the alias.
+     * @return the alias
+     */
+    public String getAlias() {
+        return alias;
+    }
+
+    /**
+     * Sets the alias.
+     * @param alias the alias to set
+     */
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+    
+}
---
+++ ui/web/main/src/java/com/echothree/ui/web/main/action/warehouse/warehousealias/WarehouseAliasUtil.java
@@ -0,0 +1,86 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2023 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.ui.web.main.action.warehouse.warehousealias;
+
+import com.echothree.control.user.warehouse.common.WarehouseUtil;
+import com.echothree.control.user.warehouse.common.form.GetWarehouseForm;
+import com.echothree.control.user.warehouse.common.result.GetWarehouseResult;
+import com.echothree.control.user.party.common.PartyUtil;
+import com.echothree.control.user.party.common.form.GetPartyAliasForm;
+import com.echothree.control.user.party.common.result.GetPartyAliasResult;
+import com.echothree.model.control.warehouse.common.transfer.WarehouseTransfer;
+import com.echothree.ui.web.main.framework.AttributeConstants;
+import com.echothree.ui.web.main.framework.MainBaseAction;
+import com.echothree.ui.web.main.framework.ParameterConstants;
+import com.echothree.util.common.command.CommandResult;
+import com.echothree.util.common.command.ExecutionResult;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
+
+public class WarehouseAliasUtil {
+
+    private WarehouseAliasUtil() {
+        super();
+    }
+
+    private static class WarehouseAliasUtilHolder {
+        static WarehouseAliasUtil instance = new WarehouseAliasUtil();
+    }
+
+    public static WarehouseAliasUtil getInstance() {
+        return WarehouseAliasUtilHolder.instance;
+    }
+
+    public void setupWarehouse(HttpServletRequest request, String partyName)
+            throws NamingException {
+        GetWarehouseForm commandForm = WarehouseUtil.getHome().getGetWarehouseForm();
+
+        commandForm.setPartyName(partyName);
+
+        CommandResult commandResult = WarehouseUtil.getHome().getWarehouse(MainBaseAction.getUserVisitPK(request), commandForm);
+
+        if(!commandResult.hasErrors()) {
+            ExecutionResult executionResult = commandResult.getExecutionResult();
+            GetWarehouseResult result = (GetWarehouseResult)executionResult.getResult();
+            WarehouseTransfer warehouse = result.getWarehouse();
+
+            if(warehouse != null) {
+                request.setAttribute(AttributeConstants.WAREHOUSE, warehouse);
+            }
+        }
+    }
+
+    public void setupWarehouse(HttpServletRequest request)
+            throws NamingException {
+        setupWarehouse(request, request.getParameter(ParameterConstants.PARTY_NAME));
+    }
+
+    public void setupPartyAliasTransfer(HttpServletRequest request, String partyName, String partyAliasTypeName)
+            throws NamingException {
+        GetPartyAliasForm commandForm = PartyUtil.getHome().getGetPartyAliasForm();
+
+        commandForm.setPartyName(partyName);
+        commandForm.setPartyAliasTypeName(partyAliasTypeName);
+
+        CommandResult commandResult = PartyUtil.getHome().getPartyAlias(MainBaseAction.getUserVisitPK(request), commandForm);
+        ExecutionResult executionResult = commandResult.getExecutionResult();
+        GetPartyAliasResult result = (GetPartyAliasResult)executionResult.getResult();
+
+        request.setAttribute(AttributeConstants.PARTY_ALIAS, result.getPartyAlias());
+    }
+
+}
