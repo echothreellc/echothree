00b15417b78f89cac72f1e4568393c6b9cf9b844
Richard Harms <rich@echothree.com>
2023-12-29 07:17:19 -0600

    #858: Added GetLocationNameElement UC

---
+++ src/java/com/echothree/control/user/warehouse/server/command/GetLocationNameElementCommand.java
@@ -0,0 +1,104 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2024 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.control.user.warehouse.server.command;
+
+import com.echothree.control.user.warehouse.common.form.GetLocationNameElementForm;
+import com.echothree.control.user.warehouse.common.result.GetLocationNameElementResult;
+import com.echothree.control.user.warehouse.common.result.WarehouseResultFactory;
+import com.echothree.model.control.core.common.EventTypes;
+import com.echothree.model.control.party.common.PartyTypes;
+import com.echothree.model.control.security.common.SecurityRoleGroups;
+import com.echothree.model.control.security.common.SecurityRoles;
+import com.echothree.model.control.warehouse.server.control.WarehouseControl;
+import com.echothree.model.data.party.server.entity.Party;
+import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.warehouse.server.entity.LocationNameElement;
+import com.echothree.model.data.warehouse.server.entity.LocationType;
+import com.echothree.model.data.warehouse.server.entity.Warehouse;
+import com.echothree.util.common.command.BaseResult;
+import com.echothree.util.common.message.ExecutionErrors;
+import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.util.common.validation.FieldType;
+import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.CommandSecurityDefinition;
+import com.echothree.util.server.control.PartyTypeDefinition;
+import com.echothree.util.server.control.SecurityRoleDefinition;
+import com.echothree.util.server.persistence.Session;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class GetLocationNameElementCommand
+        extends BaseSimpleCommand<GetLocationNameElementForm> {
+
+    private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
+    private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
+    
+    static {
+        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
+                new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
+                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
+                        new SecurityRoleDefinition(SecurityRoleGroups.LocationNameElement.name(), SecurityRoles.Review.name())
+                ))
+        ));
+
+        FORM_FIELD_DEFINITIONS = Collections.unmodifiableList(Arrays.asList(
+            new FieldDefinition("WarehouseName", FieldType.ENTITY_NAME, true, null, null),
+            new FieldDefinition("LocationTypeName", FieldType.ENTITY_NAME, true, null, null),
+            new FieldDefinition("LocationNameElementName", FieldType.ENTITY_NAME, true, null, null)
+        ));
+    }
+    
+    /** Creates a new instance of GetLocationNameElementCommand */
+    public GetLocationNameElementCommand(UserVisitPK userVisitPK, GetLocationNameElementForm form) {
+        super(userVisitPK, form, COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
+    }
+    
+    @Override
+    protected BaseResult execute() {
+        var warehouseControl = Session.getModelController(WarehouseControl.class);
+        GetLocationNameElementResult result = WarehouseResultFactory.getGetLocationNameElementResult();
+        String warehouseName = form.getWarehouseName();
+        Warehouse warehouse = warehouseControl.getWarehouseByName(warehouseName);
+        
+        if(warehouse != null) {
+            Party warehouseParty = warehouse.getParty();
+            String locationTypeName = form.getLocationTypeName();
+            LocationType locationType = warehouseControl.getLocationTypeByName(warehouseParty, locationTypeName);
+
+            if(locationType != null) {
+                String locationNameElementName = form.getLocationNameElementName();
+                LocationNameElement locationNameElement = warehouseControl.getLocationNameElementByName(locationType, locationNameElementName);
+
+                if(locationNameElement != null) {
+                    result.setLocationNameElement(warehouseControl.getLocationNameElementTransfer(getUserVisit(), locationNameElement));
+
+                    sendEvent(locationNameElement.getPrimaryKey(), EventTypes.READ, null, null, getPartyPK());
+                } else {
+                    addExecutionError(ExecutionErrors.UnknownLocationNameElementName.name(), locationNameElementName);
+                }
+            } else {
+                addExecutionError(ExecutionErrors.UnknownLocationTypeName.name(), locationTypeName);
+            }
+        } else {
+            addExecutionError(ExecutionErrors.UnknownWarehouseName.name(), warehouseName);
+        }
+        
+        return result;
+    }
+    
+}
