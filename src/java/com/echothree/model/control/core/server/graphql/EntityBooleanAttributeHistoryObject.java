638f0a43bfeca582b8172683812d92ab66b22f07
Richard Harms <rich@echothree.com>
2024-06-14 18:13:21 -0500

    #1297: Added history to Entity Boolean Attributes

---
+++ src/java/com/echothree/model/control/core/server/graphql/EntityBooleanAttributeHistoryObject.java
@@ -0,0 +1,50 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2024 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.core.server.graphql;
+
+import com.echothree.model.control.graphql.server.graphql.TimeObject;
+import com.echothree.model.data.core.server.entity.EntityBooleanAttribute;
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.schema.DataFetchingEnvironment;
+
+@GraphQLDescription("entity boolean attribute history object")
+@GraphQLName("EntityBooleanAttributeHistory")
+public class EntityBooleanAttributeHistoryObject
+        extends BaseEntityBooleanAttributeObject {
+
+    public EntityBooleanAttributeHistoryObject(EntityBooleanAttribute entityBooleanAttribute) {
+        super(entityBooleanAttribute);
+    }
+
+    @GraphQLField
+    @GraphQLDescription("from time")
+    @GraphQLNonNull
+    public TimeObject getFromTime(final DataFetchingEnvironment env) {
+        return new TimeObject(entityBooleanAttribute.getFromTime());
+    }
+
+    @GraphQLField
+    @GraphQLDescription("thru time")
+    @GraphQLNonNull
+    public TimeObject getThruTime(final DataFetchingEnvironment env) {
+        return new TimeObject(entityBooleanAttribute.getThruTime());
+    }
+
+}
