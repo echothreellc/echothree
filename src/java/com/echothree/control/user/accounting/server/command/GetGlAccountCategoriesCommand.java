c0efcf259395c049691dff63eb17c0a01c4694c5
Richard Harms <rich@echothree.com>
2024-07-16 21:42:25 -0500

    #1310: Converted GetGlAccountCategories UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/accounting/server/command/GetGlAccountCategoriesCommand.java
+++ src/java/com/echothree/control/user/accounting/server/command/GetGlAccountCategoriesCommand.java
@@ -18,54 +18,74 @@ package com.echothree.control.user.accounting.server.command;
 
 import com.echothree.control.user.accounting.common.form.GetGlAccountCategoriesForm;
 import com.echothree.control.user.accounting.common.result.AccountingResultFactory;
-import com.echothree.control.user.accounting.common.result.GetGlAccountCategoriesResult;
 import com.echothree.model.control.accounting.server.control.AccountingControl;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
+import com.echothree.model.data.accounting.server.entity.GlAccountCategory;
 import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
 import com.echothree.util.server.persistence.Session;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 
 public class GetGlAccountCategoriesCommand
-        extends BaseSimpleCommand<GetGlAccountCategoriesForm> {
-    
+        extends BasePaginatedMultipleEntitiesCommand<GlAccountCategory, GetGlAccountCategoriesForm> {
+
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
 
     static {
-        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(Collections.unmodifiableList(Arrays.asList(
+        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
-                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), Collections.unmodifiableList(Arrays.asList(
+                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
                         new SecurityRoleDefinition(SecurityRoleGroups.GlAccountCategory.name(), SecurityRoles.List.name())
-                        )))
-                )));
-        
-        FORM_FIELD_DEFINITIONS = Collections.unmodifiableList(Arrays.asList(
-                ));
+                ))
+        ));
+
+        FORM_FIELD_DEFINITIONS = List.of();
     }
-    
+
     /** Creates a new instance of GetGlAccountCategoriesCommand */
     public GetGlAccountCategoriesCommand(UserVisitPK userVisitPK, GetGlAccountCategoriesForm form) {
         super(userVisitPK, form, COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
     @Override
-    protected BaseResult execute() {
+    protected void handleForm() {
+        // No form fields.
+    }
+
+    @Override
+    protected Long getTotalEntities() {
         var accountingControl = Session.getModelController(AccountingControl.class);
-        GetGlAccountCategoriesResult result = AccountingResultFactory.getGetGlAccountCategoriesResult();
-        
-        result.setGlAccountCategories(accountingControl.getGlAccountCategoryTransfers(getUserVisit()));
-        
+
+        return accountingControl.countGlAccountCategories();
+    }
+
+    @Override
+    protected Collection<GlAccountCategory> getEntities() {
+        var accountingControl = Session.getModelController(AccountingControl.class);
+
+        return accountingControl.getGlAccountCategories();
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<GlAccountCategory> entities) {
+        var result = AccountingResultFactory.getGetGlAccountCategoriesResult();
+
+        if(entities != null) {
+            var accountingControl = Session.getModelController(AccountingControl.class);
+
+            result.setGlAccountCategories(accountingControl.getGlAccountCategoryTransfers(getUserVisit(), entities));
+        }
+
         return result;
     }
-    
+
 }
