256f6faba5968431f8a870bb00cf28f7e6bde3c8
Richard Harms <rich@echothree.com>
2024-09-22 13:36:06 -0500

    #1334: Removed redundant 'extends Objects'

--- ui/cli/dataloader/src/java/com/echothree/ui/cli/dataloader/util/hts/HtsCountryParser.java
+++ ui/cli/dataloader/src/java/com/echothree/ui/cli/dataloader/util/hts/HtsCountryParser.java
@@ -52,7 +52,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class HtsCountryParser<H extends Object> {
+public abstract class HtsCountryParser<H> {
     
     protected UserVisitPK userVisitPK;
     protected GeoService geoService;
