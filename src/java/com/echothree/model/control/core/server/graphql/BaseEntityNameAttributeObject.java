e61cce2d1d972e00a7aacd15082f0b3e3e881ce5
Richard Harms <rich@echothree.com>
2024-10-13 20:20:10 -0500

    #1368: Added history field to EntityNameAttribute object

---
+++ src/java/com/echothree/model/control/core/server/graphql/BaseEntityNameAttributeObject.java
@@ -0,0 +1,54 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2024 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.core.server.graphql;
+
+import com.echothree.model.control.graphql.server.util.BaseGraphQl;
+import com.echothree.model.data.core.server.entity.EntityNameAttribute;
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.schema.DataFetchingEnvironment;
+
+public class BaseEntityNameAttributeObject
+        implements BaseGraphQl {
+
+    protected final EntityNameAttribute entityNameAttribute; // Always Present
+
+    public BaseEntityNameAttributeObject(EntityNameAttribute entityNameAttribute) {
+        this.entityNameAttribute = entityNameAttribute;
+    }
+
+    @GraphQLField
+    @GraphQLDescription("entity attribute")
+    public EntityAttributeObject getEntityAttribute(final DataFetchingEnvironment env) {
+        return CoreSecurityUtils.getHasEntityAttributeAccess(env) ? new EntityAttributeObject(entityNameAttribute.getEntityAttribute(), entityNameAttribute.getEntityInstance()) : null;
+    }
+
+    @GraphQLField
+    @GraphQLDescription("entity instance")
+    public EntityInstanceObject getEntityInstance(final DataFetchingEnvironment env) {
+        return CoreSecurityUtils.getHasEntityInstanceAccess(env) ? new EntityInstanceObject(entityNameAttribute.getEntityInstance()) : null;
+    }
+
+    @GraphQLField
+    @GraphQLDescription("name attribute")
+    @GraphQLNonNull
+    public String getNameAttribute() {
+        return entityNameAttribute.getNameAttribute();
+    }
+
+}
