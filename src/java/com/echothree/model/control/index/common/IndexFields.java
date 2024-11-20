80c2a724f46a5ca454fb0abd73ccffc9c4c0f38b
Richard Harms <rich@echothree.com>
2024-11-19 19:52:53 -0500

    #1402: Added indexing of Entity Attribute Groups

--- src/java/com/echothree/model/control/index/common/IndexFields.java
+++ src/java/com/echothree/model/control/index/common/IndexFields.java
@@ -50,6 +50,7 @@ public enum IndexFields {
     description,
     emailAddress,
     entityAliasTypeName,
+    entityAttributeGroupName,
     entityAttributeName,
     entityAttributeTypeName,
     entityInstanceId,
