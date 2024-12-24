4a73c4cb50b4f495fbbbd049bc00845726c4aae6
Richard Harms <rich@echothree.com>
2024-12-24 17:02:48 -0600

    #1373: Added Unit of Measure Type Subscriber to track changes to Kinds

---
+++ src/java/com/echothree/model/control/uom/server/eventbus/UnitOfMeasureTypeModificationSubscriber.java
@@ -0,0 +1,60 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2025 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.uom.server.eventbus;
+
+import com.echothree.model.control.core.common.EventTypes;
+import com.echothree.model.control.core.server.control.CoreControl;
+import com.echothree.model.control.core.server.eventbus.BaseEventSubscriber;
+import com.echothree.model.control.core.server.eventbus.Function5Arity;
+import com.echothree.model.control.core.server.eventbus.SentEvent;
+import com.echothree.model.control.core.server.eventbus.SentEventSubscriber;
+import com.echothree.model.control.uom.server.control.UomControl;
+import com.echothree.model.data.core.server.entity.EntityInstance;
+import com.echothree.model.data.core.server.entity.Event;
+import com.echothree.model.data.uom.common.UnitOfMeasureKindConstants;
+import com.echothree.util.server.persistence.PersistenceUtils;
+import com.echothree.util.server.persistence.Session;
+import com.google.common.eventbus.Subscribe;
+
+@SentEventSubscriber
+public class UnitOfMeasureTypeModificationSubscriber
+        extends BaseEventSubscriber {
+
+    @Subscribe
+    public void receiveSentEventForUnitOfMeasureKinds(SentEvent se) {
+        decodeEventAndApply(se, touchUnitOfMeasureTypesIfUnitOfMeasureKind);
+    }
+
+    private static final Function5Arity<Event, EntityInstance, EventTypes, String, String>
+            touchUnitOfMeasureTypesIfUnitOfMeasureKind = (event, entityInstance, eventType, componentVendorName, entityTypeName) -> {
+        if(UnitOfMeasureKindConstants.COMPONENT_VENDOR_NAME.equals(componentVendorName)
+                && UnitOfMeasureKindConstants.ENTITY_TYPE_NAME.equals(entityTypeName)
+                && (eventType == EventTypes.MODIFY || eventType == EventTypes.TOUCH)) {
+            var coreControl = Session.getModelController(CoreControl.class);
+            var uomControl = Session.getModelController(UomControl.class);
+            var unitOfMeasureKind = uomControl.getUnitOfMeasureKindByEntityInstance(entityInstance);
+            var unitOfMeasureTypes = uomControl.getUnitOfMeasureTypesByUnitOfMeasureKind(unitOfMeasureKind);
+
+            for(var unitOfMeasureType : unitOfMeasureTypes) {
+                coreControl.sendEvent(unitOfMeasureType.getPrimaryKey(), EventTypes.TOUCH,
+                        unitOfMeasureKind.getPrimaryKey(), eventType,
+                        PersistenceUtils.getInstance().getBasePKFromEntityInstance(event.getCreatedBy()));
+            }
+        }
+    };
+
+}
