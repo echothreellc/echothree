4ddb37dd9b2896d6c0ed6d0d849bba94a5dc6b4c
Richard Harms <rich@echothree.com>
2025-01-27 07:46:10 -0600

    #1499: Converted GetTransactionGroups UC to extend BaseMultipleEntitiesCommand

--- src/java/com/echothree/control/user/accounting/server/command/GetTransactionGroupsCommand.java
+++ src/java/com/echothree/control/user/accounting/server/command/GetTransactionGroupsCommand.java
@@ -19,38 +19,66 @@ package com.echothree.control.user.accounting.server.command;
 import com.echothree.control.user.accounting.common.form.GetTransactionGroupsForm;
 import com.echothree.control.user.accounting.common.result.AccountingResultFactory;
 import com.echothree.model.control.accounting.server.control.AccountingControl;
+import com.echothree.model.control.party.common.PartyTypes;
+import com.echothree.model.control.security.common.SecurityRoleGroups;
+import com.echothree.model.control.security.common.SecurityRoles;
+import com.echothree.model.data.accounting.server.entity.TransactionGroup;
+import com.echothree.model.data.accounting.server.factory.TransactionGroupFactory;
 import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.util.server.control.BaseMultipleEntitiesCommand;
+import com.echothree.util.server.control.CommandSecurityDefinition;
+import com.echothree.util.server.control.PartyTypeDefinition;
+import com.echothree.util.server.control.SecurityRoleDefinition;
 import com.echothree.util.server.persistence.Session;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 
 public class GetTransactionGroupsCommand
-        extends BaseSimpleCommand<GetTransactionGroupsForm> {
-    
+        extends BaseMultipleEntitiesCommand<TransactionGroup, GetTransactionGroupsForm> {
+
+    private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
 
     static {
-        FORM_FIELD_DEFINITIONS = Collections.unmodifiableList(Arrays.asList(
-                ));
+        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
+                new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
+                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
+                        new SecurityRoleDefinition(SecurityRoleGroups.TransactionGroup.name(), SecurityRoles.List.name())
+                ))
+        ));
+
+        FORM_FIELD_DEFINITIONS = List.of();
     }
-    
+
     /** Creates a new instance of GetTransactionGroupsCommand */
     public GetTransactionGroupsCommand(UserVisitPK userVisitPK, GetTransactionGroupsForm form) {
-        super(userVisitPK, form, null, FORM_FIELD_DEFINITIONS, true);
+        super(userVisitPK, form, COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
     @Override
-    protected BaseResult execute() {
+    protected Collection<TransactionGroup> getEntities() {
         var accountingControl = Session.getModelController(AccountingControl.class);
+
+        return accountingControl.getTransactionGroups();
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<TransactionGroup> entities) {
         var result = AccountingResultFactory.getGetTransactionGroupsResult();
-        
-        result.setTransactionGroups(accountingControl.getTransactionGroupTransfers(getUserVisit()));
-        
+
+        if(entities != null) {
+            var accountingControl = Session.getModelController(AccountingControl.class);
+
+            if(session.hasLimit(TransactionGroupFactory.class)) {
+                result.setTransactionGroupCount(accountingControl.countTransactionGroups());
+            }
+
+            result.setTransactionGroups(accountingControl.getTransactionGroupTransfers(getUserVisit(), entities));
+        }
+
         return result;
     }
-    
+
 }
