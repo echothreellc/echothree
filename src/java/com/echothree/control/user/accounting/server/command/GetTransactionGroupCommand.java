d709ad102db6316a93a702b74a4eda8e580e166a
Richard Harms <rich@echothree.com>
2025-01-27 07:24:15 -0600

    #1500: Converted GetTransactionGroup UC to extend BaseSingleEntityCommand

--- src/java/com/echothree/control/user/accounting/server/command/GetTransactionGroupCommand.java
+++ src/java/com/echothree/control/user/accounting/server/command/GetTransactionGroupCommand.java
@@ -19,49 +19,69 @@ package com.echothree.control.user.accounting.server.command;
 import com.echothree.control.user.accounting.common.form.GetTransactionGroupForm;
 import com.echothree.control.user.accounting.common.result.AccountingResultFactory;
 import com.echothree.model.control.accounting.server.control.AccountingControl;
+import com.echothree.model.control.accounting.server.logic.TransactionGroupLogic;
 import com.echothree.model.control.core.common.EventTypes;
+import com.echothree.model.control.party.common.PartyTypes;
+import com.echothree.model.control.security.common.SecurityRoleGroups;
+import com.echothree.model.control.security.common.SecurityRoles;
+import com.echothree.model.data.accounting.server.entity.TransactionGroup;
 import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.message.ExecutionErrors;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
+import com.echothree.util.server.control.CommandSecurityDefinition;
+import com.echothree.util.server.control.PartyTypeDefinition;
+import com.echothree.util.server.control.SecurityRoleDefinition;
 import com.echothree.util.server.persistence.Session;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class GetTransactionGroupCommand
-        extends BaseSimpleCommand<GetTransactionGroupForm> {
-    
+        extends BaseSingleEntityCommand<TransactionGroup, GetTransactionGroupForm> {
+
+    private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
     
     static {
-        FORM_FIELD_DEFINITIONS = Collections.unmodifiableList(Arrays.asList(
-            new FieldDefinition("TransactionGroupName", FieldType.ENTITY_NAME, true, null, null)
+        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
+                new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
+                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
+                        new SecurityRoleDefinition(SecurityRoleGroups.UserVisitGroup.name(), SecurityRoles.Review.name())
+                ))
         ));
+
+        FORM_FIELD_DEFINITIONS = List.of(
+                new FieldDefinition("TransactionGroupName", FieldType.ENTITY_NAME, false, null, null),
+                new FieldDefinition("EntityRef", FieldType.ENTITY_REF, false, null, null),
+                new FieldDefinition("Uuid", FieldType.UUID, false, null, null)
+        );
     }
     
     /** Creates a new instance of GetTransactionGroupCommand */
     public GetTransactionGroupCommand(UserVisitPK userVisitPK, GetTransactionGroupForm form) {
-        super(userVisitPK, form, null, FORM_FIELD_DEFINITIONS, true);
+        super(userVisitPK, form, COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
     @Override
-    protected BaseResult execute() {
+    protected TransactionGroup getEntity() {
+        var transactionGroup = TransactionGroupLogic.getInstance().getTransactionGroupByUniversalSpec(this, form);
+
+        if(transactionGroup != null) {
+            sendEvent(transactionGroup.getPrimaryKey(), EventTypes.READ, null, null, getPartyPK());
+        }
+
+        return transactionGroup;
+    }
+
+    @Override
+    protected BaseResult getResult(TransactionGroup transactionGroup) {
         var accountingControl = Session.getModelController(AccountingControl.class);
         var result = AccountingResultFactory.getGetTransactionGroupResult();
-        var transactionGroupName = form.getTransactionGroupName();
-        var transactionGroup = accountingControl.getTransactionGroupByName(transactionGroupName);
-        
+
         if(transactionGroup != null) {
             result.setTransactionGroup(accountingControl.getTransactionGroupTransfer(getUserVisit(), transactionGroup));
-            
-            sendEvent(transactionGroup.getPrimaryKey(), EventTypes.READ, null, null, getPartyPK());
-        } else {
-            addExecutionError(ExecutionErrors.UnknownTransactionGroupName.name(), transactionGroupName);
         }
-        
+
         return result;
     }
     
