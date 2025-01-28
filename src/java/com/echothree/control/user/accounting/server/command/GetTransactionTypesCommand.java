02a0abce8c1001db11a29e85305275c289c94e55
Richard Harms <rich@echothree.com>
2025-01-27 21:20:10 -0600

    #1503: Converted GetTransactionTypes UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/accounting/server/command/GetTransactionTypesCommand.java
+++ src/java/com/echothree/control/user/accounting/server/command/GetTransactionTypesCommand.java
@@ -19,38 +19,78 @@ package com.echothree.control.user.accounting.server.command;
 import com.echothree.control.user.accounting.common.form.GetTransactionTypesForm;
 import com.echothree.control.user.accounting.common.result.AccountingResultFactory;
 import com.echothree.model.control.accounting.server.control.AccountingControl;
+import com.echothree.model.control.party.common.PartyTypes;
+import com.echothree.model.control.security.common.SecurityRoleGroups;
+import com.echothree.model.control.security.common.SecurityRoles;
+import com.echothree.model.data.accounting.server.entity.TransactionType;
+import com.echothree.model.data.accounting.server.factory.TransactionTypeFactory;
 import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
+import com.echothree.util.server.control.CommandSecurityDefinition;
+import com.echothree.util.server.control.PartyTypeDefinition;
+import com.echothree.util.server.control.SecurityRoleDefinition;
 import com.echothree.util.server.persistence.Session;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 
 public class GetTransactionTypesCommand
-        extends BaseSimpleCommand<GetTransactionTypesForm> {
-    
+        extends BasePaginatedMultipleEntitiesCommand<TransactionType, GetTransactionTypesForm> {
+
+    private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
 
     static {
-        FORM_FIELD_DEFINITIONS = Collections.unmodifiableList(Arrays.asList(
-                ));
+        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
+                new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
+                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
+                        new SecurityRoleDefinition(SecurityRoleGroups.TransactionType.name(), SecurityRoles.List.name())
+                ))
+        ));
+
+        FORM_FIELD_DEFINITIONS = List.of();
     }
-    
+
     /** Creates a new instance of GetTransactionTypesCommand */
     public GetTransactionTypesCommand(UserVisitPK userVisitPK, GetTransactionTypesForm form) {
-        super(userVisitPK, form, null, FORM_FIELD_DEFINITIONS, true);
+        super(userVisitPK, form, COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
     @Override
-    protected BaseResult execute() {
+    protected void handleForm() {
+        // No form fields.
+    }
+
+    @Override
+    protected Long getTotalEntities() {
         var accountingControl = Session.getModelController(AccountingControl.class);
+
+        return accountingControl.countTransactionTypes();
+    }
+
+    @Override
+    protected Collection<TransactionType> getEntities() {
+        var accountingControl = Session.getModelController(AccountingControl.class);
+
+        return accountingControl.getTransactionTypes();
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<TransactionType> entities) {
         var result = AccountingResultFactory.getGetTransactionTypesResult();
-        
-        result.setTransactionTypes(accountingControl.getTransactionTypeTransfers(getUserVisit()));
-        
+
+        if(entities != null) {
+            var accountingControl = Session.getModelController(AccountingControl.class);
+
+            if(session.hasLimit(TransactionTypeFactory.class)) {
+                result.setTransactionTypeCount(accountingControl.countTransactionTypes());
+            }
+
+            result.setTransactionTypes(accountingControl.getTransactionTypeTransfers(getUserVisit(), entities));
+        }
+
         return result;
     }
-    
+
 }
