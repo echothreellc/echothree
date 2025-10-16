2d6da1a33458e4cd2d5f48a2faccc2b7a3645992
Richard Harms <rich@echothree.com>
2025-10-15 19:06:36 -0500

    #1280: Converted GetWishlistTypes to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/wishlist/server/command/GetWishlistTypesCommand.java
+++ src/java/com/echothree/control/user/wishlist/server/command/GetWishlistTypesCommand.java
@@ -19,17 +19,16 @@ package com.echothree.control.user.wishlist.server.command;
 import com.echothree.control.user.wishlist.common.form.GetWishlistTypesForm;
 import com.echothree.control.user.wishlist.common.result.WishlistResultFactory;
 import com.echothree.model.control.wishlist.server.control.WishlistControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
 import com.echothree.model.data.wishlist.server.entity.WishlistType;
 import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
-import com.echothree.util.server.control.BaseMultipleEntitiesCommand;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
 import com.echothree.util.server.persistence.Session;
 import java.util.Collection;
 import java.util.List;
 
 public class GetWishlistTypesCommand
-        extends BaseMultipleEntitiesCommand<WishlistType, GetWishlistTypesForm> {
+        extends BasePaginatedMultipleEntitiesCommand<WishlistType, GetWishlistTypesForm> {
 
     // No COMMAND_SECURITY_DEFINITION, anyone may execute this command.
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -44,6 +43,18 @@ public class GetWishlistTypesCommand
     }
 
     @Override
+    protected void handleForm() {
+        // No form fields.
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        var wishlistControl = Session.getModelController(WishlistControl.class);
+
+        return wishlistControl.countWishlistTypes();
+    }
+
+    @Override
     protected Collection<WishlistType> getEntities() {
         var wishlistControl = Session.getModelController(WishlistControl.class);
 
@@ -53,12 +64,9 @@ public class GetWishlistTypesCommand
     @Override
     protected BaseResult getResult(Collection<WishlistType> entities) {
         var result = WishlistResultFactory.getGetWishlistTypesResult();
+        var wishlistControl = Session.getModelController(WishlistControl.class);
 
-        if(entities != null) {
-            var wishlistControl = Session.getModelController(WishlistControl.class);
-
-            result.setWishlistTypes(wishlistControl.getWishlistTypeTransfers(getUserVisit(), entities));
-        }
+        result.setWishlistTypes(wishlistControl.getWishlistTypeTransfers(getUserVisit(), entities));
 
         return result;
     }
