397d50f03025b3875eaf7ec45287468186e21ea0
Richard Harms <rich@echothree.com>
2025-10-25 14:32:47 -0500

    #1262: Converted GetUnitOfMeasureTypes UC to extend BaseAbstractEditCommand

--- src/java/com/echothree/control/user/uom/server/command/GetUnitOfMeasureTypesCommand.java
+++ src/java/com/echothree/control/user/uom/server/command/GetUnitOfMeasureTypesCommand.java
@@ -18,63 +18,96 @@ package com.echothree.control.user.uom.server.command;
 
 import com.echothree.control.user.uom.common.form.GetUnitOfMeasureTypesForm;
 import com.echothree.control.user.uom.common.result.UomResultFactory;
+import com.echothree.model.control.party.common.PartyTypes;
+import com.echothree.model.control.security.common.SecurityRoleGroups;
+import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.control.uom.server.control.UomControl;
 import com.echothree.model.control.uom.server.logic.UnitOfMeasureKindLogic;
 import com.echothree.model.data.uom.server.entity.UnitOfMeasureKind;
 import com.echothree.model.data.uom.server.entity.UnitOfMeasureType;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.uom.server.factory.UnitOfMeasureTypeFactory;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseMultipleEntitiesCommand;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
+import com.echothree.util.server.control.CommandSecurityDefinition;
+import com.echothree.util.server.control.PartyTypeDefinition;
+import com.echothree.util.server.control.SecurityRoleDefinition;
 import com.echothree.util.server.persistence.Session;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 public class GetUnitOfMeasureTypesCommand
-        extends BaseMultipleEntitiesCommand<UnitOfMeasureType, GetUnitOfMeasureTypesForm> {
-    
+        extends BasePaginatedMultipleEntitiesCommand<UnitOfMeasureType, GetUnitOfMeasureTypesForm> {
+
+    private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
-    
+
     static {
-        FORM_FIELD_DEFINITIONS = Collections.unmodifiableList(Arrays.asList(
+        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
+                new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
+                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
+                        new SecurityRoleDefinition(SecurityRoleGroups.UnitOfMeasureType.name(), SecurityRoles.List.name())
+                ))
+        ));
+
+        FORM_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("UnitOfMeasureKindName", FieldType.ENTITY_NAME, true, null, null)
-                ));
+        );
     }
-    
+
     /** Creates a new instance of GetUnitOfMeasureTypesCommand */
     public GetUnitOfMeasureTypesCommand() {
-        super(null, FORM_FIELD_DEFINITIONS, true);
+        super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
     UnitOfMeasureKind unitOfMeasureKind;
-    
+
+    @Override
+    protected void handleForm() {
+        var unitOfMeasureKindName = form.getUnitOfMeasureKindName();
+
+        unitOfMeasureKind = UnitOfMeasureKindLogic.getInstance().getUnitOfMeasureKindByName(this, unitOfMeasureKindName);
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        var uomControl = Session.getModelController(UomControl.class);
+
+        return hasExecutionErrors() ? null :
+                uomControl.countUnitOfMeasureTypesByUnitOfMeasureKind(unitOfMeasureKind);
+    }
+
     @Override
     protected Collection<UnitOfMeasureType> getEntities() {
         Collection<UnitOfMeasureType> entities = null;
-        
-        unitOfMeasureKind = UnitOfMeasureKindLogic.getInstance().getUnitOfMeasureKindByName(this, form.getUnitOfMeasureKindName());
 
         if(!hasExecutionErrors()) {
             var uomControl = Session.getModelController(UomControl.class);
-            
+
             entities = uomControl.getUnitOfMeasureTypesByUnitOfMeasureKind(unitOfMeasureKind);
         }
-        
+
         return entities;
     }
-    
+
     @Override
     protected BaseResult getResult(Collection<UnitOfMeasureType> entities) {
         var result = UomResultFactory.getGetUnitOfMeasureTypesResult();
-        var uomControl = Session.getModelController(UomControl.class);
-        
-        result.setUnitOfMeasureKind(uomControl.getUnitOfMeasureKindTransfer(getUserVisit(), unitOfMeasureKind));
-        result.setUnitOfMeasureTypes(uomControl.getUnitOfMeasureTypeTransfers(getUserVisit(), entities));
-        
+
+        if(entities != null) {
+            var uomControl = Session.getModelController(UomControl.class);
+            var userVisit = getUserVisit();
+
+            if(session.hasLimit(UnitOfMeasureTypeFactory.class)) {
+                result.setUnitOfMeasureTypeCount(getTotalEntities());
+            }
+
+            result.setUnitOfMeasureKind(uomControl.getUnitOfMeasureKindTransfer(userVisit, unitOfMeasureKind));
+            result.setUnitOfMeasureTypes(uomControl.getUnitOfMeasureTypeTransfers(userVisit, entities));
+        }
+
         return result;
     }
-    
+
 }
