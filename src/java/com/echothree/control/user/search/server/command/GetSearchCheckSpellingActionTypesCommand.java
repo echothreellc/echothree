15fbbbbc87ef928fd0fb59c6b09aaa48b6d735a4
Richard Harms <rich@echothree.com>
2025-10-26 12:34:06 -0500

    #1201: Converted GetSearchCheckSpellingActionTypes UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/search/server/command/GetSearchCheckSpellingActionTypesCommand.java
+++ src/java/com/echothree/control/user/search/server/command/GetSearchCheckSpellingActionTypesCommand.java
@@ -18,30 +18,54 @@ package com.echothree.control.user.search.server.command;
 
 import com.echothree.control.user.search.common.form.GetSearchCheckSpellingActionTypesForm;
 import com.echothree.control.user.search.common.result.SearchResultFactory;
+import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.search.server.control.SearchControl;
+import com.echothree.model.control.security.common.SecurityRoleGroups;
+import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.data.search.server.entity.SearchCheckSpellingActionType;
 import com.echothree.model.data.search.server.factory.SearchCheckSpellingActionTypeFactory;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
 import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
-import com.echothree.util.server.control.BaseMultipleEntitiesCommand;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
+import com.echothree.util.server.control.CommandSecurityDefinition;
+import com.echothree.util.server.control.PartyTypeDefinition;
+import com.echothree.util.server.control.SecurityRoleDefinition;
 import com.echothree.util.server.persistence.Session;
 import java.util.Collection;
 import java.util.List;
 
 public class GetSearchCheckSpellingActionTypesCommand
-        extends BaseMultipleEntitiesCommand<SearchCheckSpellingActionType, GetSearchCheckSpellingActionTypesForm> {
+        extends BasePaginatedMultipleEntitiesCommand<SearchCheckSpellingActionType, GetSearchCheckSpellingActionTypesForm> {
 
-    // No COMMAND_SECURITY_DEFINITION, anyone may execute this command.
+    private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
 
     static {
+        COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
+                new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
+                new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
+                        new SecurityRoleDefinition(SecurityRoleGroups.SearchCheckSpellingActionType.name(), SecurityRoles.List.name())
+                ))
+        ));
+
         FORM_FIELD_DEFINITIONS = List.of();
     }
 
     /** Creates a new instance of GetSearchCheckSpellingActionTypesCommand */
     public GetSearchCheckSpellingActionTypesCommand() {
-        super(null, FORM_FIELD_DEFINITIONS, true);
+        super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
+    }
+
+    @Override
+    protected void handleForm() {
+        // no form fields
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        var searchControl = Session.getModelController(SearchControl.class);
+
+        return searchControl.countSearchCheckSpellingActionTypes();
     }
 
     @Override
@@ -57,13 +81,12 @@ public class GetSearchCheckSpellingActionTypesCommand
 
         if(entities != null) {
             var searchControl = Session.getModelController(SearchControl.class);
-            var userVisit = getUserVisit();
 
             if(session.hasLimit(SearchCheckSpellingActionTypeFactory.class)) {
-                result.setSearchCheckSpellingActionTypeCount(searchControl.countSearchCheckSpellingActionTypes());
+                result.setSearchCheckSpellingActionTypeCount(getTotalEntities());
             }
 
-            result.setSearchCheckSpellingActionTypes(searchControl.getSearchCheckSpellingActionTypeTransfers(userVisit, entities));
+            result.setSearchCheckSpellingActionTypes(searchControl.getSearchCheckSpellingActionTypeTransfers(getUserVisit(), entities));
         }
 
         return result;
