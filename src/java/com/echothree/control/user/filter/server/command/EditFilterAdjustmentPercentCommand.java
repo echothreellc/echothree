038a25a370cd35a04f95c6053e459b4e8953130a
Richard Harms <rich@echothree.com>
2026-03-31 18:53:18 -0500

    #996: Converted EditFilterAdjustmentPercent UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/filter/server/command/EditFilterAdjustmentPercentCommand.java
+++ src/java/com/echothree/control/user/filter/server/command/EditFilterAdjustmentPercentCommand.java
@@ -18,7 +18,7 @@ package com.echothree.control.user.filter.server.command;
 
 import com.echothree.control.user.filter.common.edit.FilterAdjustmentPercentEdit;
 import com.echothree.control.user.filter.common.edit.FilterEditFactory;
-import com.echothree.control.user.filter.common.form.EditFilterAdjustmentPercentForm;
+import com.echothree.control.user.filter.common.result.EditFilterAdjustmentPercentResult;
 import com.echothree.control.user.filter.common.result.FilterResultFactory;
 import com.echothree.control.user.filter.common.spec.FilterAdjustmentPercentSpec;
 import com.echothree.model.control.accounting.server.control.AccountingControl;
@@ -28,27 +28,27 @@ import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.control.uom.server.control.UomControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.command.BaseResult;
+import com.echothree.model.data.filter.server.entity.FilterAdjustment;
+import com.echothree.model.data.filter.server.entity.FilterAdjustmentPercent;
 import com.echothree.util.common.command.EditMode;
 import com.echothree.util.common.form.BaseForm;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.server.control.BaseEditCommand;
+import com.echothree.util.server.control.BaseAbstractEditCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import com.echothree.util.server.string.PercentUtils;
 import com.echothree.util.server.validation.Validator;
 import com.google.common.base.Splitter;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class EditFilterAdjustmentPercentCommand
-        extends BaseEditCommand<FilterAdjustmentPercentSpec, FilterAdjustmentPercentEdit> {
+        extends BaseAbstractEditCommand<FilterAdjustmentPercentSpec, FilterAdjustmentPercentEdit, EditFilterAdjustmentPercentResult, FilterAdjustmentPercent, FilterAdjustment> {
 
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> SPEC_FIELD_DEFINITIONS;
@@ -75,105 +75,87 @@ public class EditFilterAdjustmentPercentCommand
                 new FieldDefinition("Percent", FieldType.FRACTIONAL_PERCENT, true, null, null)
         );
     }
-    
+
+    @Inject
+    AccountingControl accountingControl;
+
+    @Inject
+    FilterControl filterControl;
+
+    @Inject
+    UomControl uomControl;
+
     /** Creates a new instance of EditFilterAdjustmentPercentCommand */
     public EditFilterAdjustmentPercentCommand() {
         super(COMMAND_SECURITY_DEFINITION, SPEC_FIELD_DEFINITIONS, EDIT_FIELD_DEFINITIONS);
     }
-    
+
     @Override
     protected void setupValidatorForEdit(Validator validator, BaseForm specForm) {
-        var accountingControl = Session.getModelController(AccountingControl.class);
         var currencyIsoName = spec.getCurrencyIsoName();
+
         validator.setCurrency(accountingControl.getCurrencyByIsoName(currencyIsoName));
     }
-    
+
     @Override
-    protected BaseResult execute() {
-        var filterControl = Session.getModelController(FilterControl.class);
-        var result = FilterResultFactory.getEditFilterAdjustmentPercentResult();
+    public EditFilterAdjustmentPercentResult getResult() {
+        return FilterResultFactory.getEditFilterAdjustmentPercentResult();
+    }
+
+    @Override
+    public FilterAdjustmentPercentEdit getEdit() {
+        return FilterEditFactory.getFilterAdjustmentPercentEdit();
+    }
+
+    @Override
+    public FilterAdjustmentPercent getEntity(EditFilterAdjustmentPercentResult result) {
+        FilterAdjustmentPercent filterAdjustmentPercent = null;
         var filterKindName = spec.getFilterKindName();
         var filterKind = filterControl.getFilterKindByName(filterKindName);
-        
+
         if(filterKind != null) {
             var filterAdjustmentName = spec.getFilterAdjustmentName();
             var filterAdjustment = filterControl.getFilterAdjustmentByName(filterKind, filterAdjustmentName);
-            
+
             if(filterAdjustment != null) {
                 var filterAdjustmentType = filterAdjustment.getLastDetail().getFilterAdjustmentType();
-                
+
                 if(filterAdjustmentType != null && filterAdjustmentType.getFilterAdjustmentTypeName().equals(FilterAdjustmentTypes.PERCENT.name())) {
-                    var uomControl = Session.getModelController(UomControl.class);
                     var unitOfMeasureName = spec.getUnitOfMeasureName();
                     String unitOfMeasureKindName = null;
                     String unitOfMeasureTypeName = null;
-                    
+
                     if(unitOfMeasureName == null) {
                         unitOfMeasureKindName = spec.getUnitOfMeasureKindName();
                         unitOfMeasureTypeName = spec.getUnitOfMeasureTypeName();
                     } else {
-                        String splitUomName[] = Splitter.on(':').trimResults().omitEmptyStrings().splitToList(unitOfMeasureName).toArray(new String[0]);
-                        
+                        var splitUomName = Splitter.on(':').trimResults().omitEmptyStrings().splitToList(unitOfMeasureName).toArray(new String[0]);
+
                         if(splitUomName.length == 2) {
                             unitOfMeasureKindName = splitUomName[0];
                             unitOfMeasureTypeName = splitUomName[1];
                         }
                     }
-                    
+
                     if(unitOfMeasureKindName != null && unitOfMeasureTypeName != null) {
                         var unitOfMeasureKind = uomControl.getUnitOfMeasureKindByName(unitOfMeasureKindName);
-                        
+
                         if(unitOfMeasureKind != null) {
-                            var unitOfMeasureType = uomControl.getUnitOfMeasureTypeByName(unitOfMeasureKind,
-                                    unitOfMeasureTypeName);
-                            
+                            var unitOfMeasureType = uomControl.getUnitOfMeasureTypeByName(unitOfMeasureKind, unitOfMeasureTypeName);
+
                             if(unitOfMeasureType != null) {
-                                var accountingControl = Session.getModelController(AccountingControl.class);
                                 var currencyIsoName = spec.getCurrencyIsoName();
                                 var currency = accountingControl.getCurrencyByIsoName(currencyIsoName);
-                                
+
                                 if(currency != null) {
-                                    if(editMode.equals(EditMode.LOCK)) {
-                                        var filterAdjustmentPercent = filterControl.getFilterAdjustmentPercent(filterAdjustment,
-                                                unitOfMeasureType, currency);
-                                        
-                                        if(filterAdjustmentPercent != null) {
-                                            result.setFilterAdjustmentPercent(filterControl.getFilterAdjustmentPercentTransfer(getUserVisit(), filterAdjustmentPercent));
-                                            
-                                            if(lockEntity(filterAdjustmentPercent)) {
-                                                edit = FilterEditFactory.getFilterAdjustmentPercentEdit();
-                                                result.setEdit(edit);
-                                                edit.setPercent(PercentUtils.getInstance().formatFractionalPercent(filterAdjustmentPercent.getPercent()));
-                                            } else {
-                                                addExecutionError(ExecutionErrors.EntityLockFailed.name());
-                                            }
-                                            
-                                            result.setEntityLock(getEntityLockTransfer(filterAdjustmentPercent));
-                                        } else {
-                                            addExecutionError(ExecutionErrors.UnknownFilterAdjustmentPercent.name());
-                                        }
-                                    } else if(editMode.equals(EditMode.UPDATE)) {
-                                        var filterAdjustmentPercent = filterControl.getFilterAdjustmentPercentForUpdate(filterAdjustment,
-                                                unitOfMeasureType, currency);
-                                        
-                                        if(filterAdjustmentPercent != null) {
-                                            if(lockEntityForUpdate(filterAdjustmentPercent)) {
-                                                try {
-                                                    var partyPK = getPartyPK();
-                                                    var filterAdjustmentPercentValue = filterControl.getFilterAdjustmentPercentValue(filterAdjustmentPercent);
-                                                    
-                                                    filterAdjustmentPercentValue.setPercent(Integer.valueOf(edit.getPercent()));
-                                                    
-                                                    filterControl.updateFilterAdjustmentPercentFromValue(filterAdjustmentPercentValue, partyPK);
-                                                } finally {
-                                                    unlockEntity(filterAdjustmentPercent);
-                                                }
-                                            } else {
-                                                addExecutionError(ExecutionErrors.EntityLockStale.name());
-                                            }
-                                        } else {
-                                            addExecutionError(ExecutionErrors.UnknownFilterAdjustmentPercent.name());
-                                        }
+                                    if(editMode.equals(EditMode.LOCK) || editMode.equals(EditMode.ABANDON)) {
+                                        filterAdjustmentPercent = filterControl.getFilterAdjustmentPercent(filterAdjustment, unitOfMeasureType, currency);
+                                    } else { // EditMode.UPDATE
+                                        filterAdjustmentPercent = filterControl.getFilterAdjustmentPercentForUpdate(filterAdjustment, unitOfMeasureType, currency);
+                                    }
+
+                                    if(filterAdjustmentPercent == null) {
+                                        addExecutionError(ExecutionErrors.UnknownFilterAdjustmentPercent.name());
                                     }
                                 } else {
                                     addExecutionError(ExecutionErrors.UnknownCurrencyIsoName.name(), currencyIsoName);
@@ -196,8 +178,32 @@ public class EditFilterAdjustmentPercentCommand
         } else {
             addExecutionError(ExecutionErrors.UnknownFilterKindName.name(), filterKindName);
         }
-        
-        return result;
+
+        return filterAdjustmentPercent;
     }
-    
+
+    @Override
+    public FilterAdjustment getLockEntity(FilterAdjustmentPercent filterAdjustmentPercent) {
+        return filterAdjustmentPercent.getFilterAdjustment();
+    }
+
+    @Override
+    public void fillInResult(EditFilterAdjustmentPercentResult result, FilterAdjustmentPercent filterAdjustmentPercent) {
+        result.setFilterAdjustmentPercent(filterControl.getFilterAdjustmentPercentTransfer(getUserVisit(), filterAdjustmentPercent));
+    }
+
+    @Override
+    public void doLock(FilterAdjustmentPercentEdit edit, FilterAdjustmentPercent filterAdjustmentPercent) {
+        edit.setPercent(PercentUtils.getInstance().formatFractionalPercent(filterAdjustmentPercent.getPercent()));
+    }
+
+    @Override
+    public void doUpdate(FilterAdjustmentPercent filterAdjustmentPercent) {
+        var filterAdjustmentPercentValue = filterControl.getFilterAdjustmentPercentValue(filterAdjustmentPercent);
+
+        filterAdjustmentPercentValue.setPercent(Integer.valueOf(edit.getPercent()));
+
+        filterControl.updateFilterAdjustmentPercentFromValue(filterAdjustmentPercentValue, getPartyPK());
+    }
+
 }
