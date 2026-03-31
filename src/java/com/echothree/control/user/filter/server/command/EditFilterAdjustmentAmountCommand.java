447d81c81d5529e18035e76cf513e2d67aa22bfc
Richard Harms <rich@echothree.com>
2026-03-31 18:54:50 -0500

    #999: Converted EditFilterAdjustmentAmount UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/filter/server/command/EditFilterAdjustmentAmountCommand.java
+++ src/java/com/echothree/control/user/filter/server/command/EditFilterAdjustmentAmountCommand.java
@@ -18,7 +18,7 @@ package com.echothree.control.user.filter.server.command;
 
 import com.echothree.control.user.filter.common.edit.FilterAdjustmentAmountEdit;
 import com.echothree.control.user.filter.common.edit.FilterEditFactory;
-import com.echothree.control.user.filter.common.form.EditFilterAdjustmentAmountForm;
+import com.echothree.control.user.filter.common.result.EditFilterAdjustmentAmountResult;
 import com.echothree.control.user.filter.common.result.FilterResultFactory;
 import com.echothree.control.user.filter.common.spec.FilterAdjustmentAmountSpec;
 import com.echothree.model.control.accounting.server.control.AccountingControl;
@@ -29,27 +29,27 @@ import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.control.uom.server.control.UomControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.command.BaseResult;
+import com.echothree.model.data.filter.server.entity.FilterAdjustment;
+import com.echothree.model.data.filter.server.entity.FilterAdjustmentAmount;
 import com.echothree.util.common.command.EditMode;
 import com.echothree.util.common.form.BaseForm;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.server.control.BaseEditCommand;
+import com.echothree.util.server.control.BaseAbstractEditCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import com.echothree.util.server.string.AmountUtils;
 import com.echothree.util.server.validation.Validator;
 import com.google.common.base.Splitter;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class EditFilterAdjustmentAmountCommand
-        extends BaseEditCommand<FilterAdjustmentAmountSpec, FilterAdjustmentAmountEdit> {
+        extends BaseAbstractEditCommand<FilterAdjustmentAmountSpec, FilterAdjustmentAmountEdit, EditFilterAdjustmentAmountResult, FilterAdjustmentAmount, FilterAdjustment> {
 
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> SPEC_FIELD_DEFINITIONS;
@@ -81,7 +81,16 @@ public class EditFilterAdjustmentAmountCommand
                 new FieldDefinition("Amount:CurrencyIsoName,CurrencyIsoName", FieldType.PRICE_UNIT, true, null, null)
         );
     }
-    
+
+    @Inject
+    AccountingControl accountingControl;
+
+    @Inject
+    FilterControl filterControl;
+
+    @Inject
+    UomControl uomControl;
+
     /** Creates a new instance of EditFilterAdjustmentAmountCommand */
     public EditFilterAdjustmentAmountCommand() {
         super(COMMAND_SECURITY_DEFINITION, SPEC_FIELD_DEFINITIONS, null);
@@ -89,7 +98,6 @@ public class EditFilterAdjustmentAmountCommand
     
     @Override
     protected void setupValidatorForEdit(Validator validator, BaseForm specForm) {
-        var accountingControl = Session.getModelController(AccountingControl.class);
         var filterKindName = spec.getFilterKindName();
         var currencyIsoName = spec.getCurrencyIsoName();
         
@@ -103,98 +111,69 @@ public class EditFilterAdjustmentAmountCommand
             addExecutionError(ExecutionErrors.UnknownFilterKindName.name(), filterKindName);
         }
     }
-    
+
     @Override
-    protected BaseResult execute() {
-        var filterControl = Session.getModelController(FilterControl.class);
-        var result = FilterResultFactory.getEditFilterAdjustmentAmountResult();
+    public EditFilterAdjustmentAmountResult getResult() {
+        return FilterResultFactory.getEditFilterAdjustmentAmountResult();
+    }
+
+    @Override
+    public FilterAdjustmentAmountEdit getEdit() {
+        return FilterEditFactory.getFilterAdjustmentAmountEdit();
+    }
+
+    @Override
+    public FilterAdjustmentAmount getEntity(EditFilterAdjustmentAmountResult result) {
+        FilterAdjustmentAmount filterAdjustmentAmount = null;
         var filterKindName = spec.getFilterKindName();
         var filterKind = filterControl.getFilterKindByName(filterKindName);
-        
+
         if(filterKind != null) {
             var filterAdjustmentName = spec.getFilterAdjustmentName();
             var filterAdjustment = filterControl.getFilterAdjustmentByName(filterKind, filterAdjustmentName);
-            
+
             if(filterAdjustment != null) {
                 var filterAdjustmentType = filterAdjustment.getLastDetail().getFilterAdjustmentType();
-                
+
                 if(filterAdjustmentType != null && filterAdjustmentType.getFilterAdjustmentTypeName().equals(FilterAdjustmentTypes.AMOUNT.name())) {
-                    var uomControl = Session.getModelController(UomControl.class);
                     var unitOfMeasureName = spec.getUnitOfMeasureName();
                     String unitOfMeasureKindName = null;
                     String unitOfMeasureTypeName = null;
-                    
+
                     if(unitOfMeasureName == null) {
                         unitOfMeasureKindName = spec.getUnitOfMeasureKindName();
                         unitOfMeasureTypeName = spec.getUnitOfMeasureTypeName();
                     } else {
-                        String splitUomName[] = Splitter.on(':').trimResults().omitEmptyStrings().splitToList(unitOfMeasureName).toArray(new String[0]);
-                        
+                        String[] splitUomName = Splitter.on(':').trimResults().omitEmptyStrings().splitToList(unitOfMeasureName).toArray(new String[0]);
+
                         if(splitUomName.length == 2) {
                             unitOfMeasureKindName = splitUomName[0];
                             unitOfMeasureTypeName = splitUomName[1];
                         }
                     }
-                    
+
                     if(unitOfMeasureKindName != null && unitOfMeasureTypeName != null) {
                         var unitOfMeasureKind = uomControl.getUnitOfMeasureKindByName(unitOfMeasureKindName);
-                        
+
                         if(unitOfMeasureKind != null) {
                             var unitOfMeasureType = uomControl.getUnitOfMeasureTypeByName(unitOfMeasureKind,
                                     unitOfMeasureTypeName);
-                            
+
                             if(unitOfMeasureType != null) {
-                                var accountingControl = Session.getModelController(AccountingControl.class);
                                 var currencyIsoName = spec.getCurrencyIsoName();
                                 var currency = accountingControl.getCurrencyByIsoName(currencyIsoName);
-                                
+
                                 if(currency != null) {
-                                    if(editMode.equals(EditMode.LOCK)) {
-                                        var filterAdjustmentAmount = filterControl.getFilterAdjustmentAmount(filterAdjustment,
+                                    if(editMode.equals(EditMode.LOCK) || editMode.equals(EditMode.ABANDON)) {
+                                        filterAdjustmentAmount = filterControl.getFilterAdjustmentAmount(filterAdjustment,
                                                 unitOfMeasureType, currency);
-                                        
-                                        if(filterAdjustmentAmount != null) {
-                                            result.setFilterAdjustmentAmount(filterControl.getFilterAdjustmentAmountTransfer(getUserVisit(), filterAdjustmentAmount));
-                                            
-                                            if(lockEntity(filterAdjustmentAmount)) {
-                                                edit = FilterEditFactory.getFilterAdjustmentAmountEdit();
-                                                result.setEdit(edit);
-                                                
-                                                if(filterKindName.equals(FilterKinds.COST.name())) {
-                                                    edit.setAmount(AmountUtils.getInstance().formatCostUnit(currency, filterAdjustmentAmount.getAmount()));
-                                                } else if(filterKindName.equals(FilterKinds.PRICE.name())) {
-                                                    edit.setAmount(AmountUtils.getInstance().formatPriceUnit(currency, filterAdjustmentAmount.getAmount()));
-                                                }
-                                            } else {
-                                                addExecutionError(ExecutionErrors.EntityLockFailed.name());
-                                            }
-                                            
-                                            result.setEntityLock(getEntityLockTransfer(filterAdjustmentAmount));
-                                        } else {
-                                            addExecutionError(ExecutionErrors.UnknownFilterAdjustmentAmount.name());
-                                        }
-                                    } else if(editMode.equals(EditMode.UPDATE)) {
-                                        var filterAdjustmentAmount = filterControl.getFilterAdjustmentAmountForUpdate(filterAdjustment,
+                                    } else { // EditMode.UPDATE
+                                        filterAdjustmentAmount = filterControl.getFilterAdjustmentAmountForUpdate(filterAdjustment,
                                                 unitOfMeasureType, currency);
-                                        
-                                        if(filterAdjustmentAmount != null) {
-                                            if(lockEntityForUpdate(filterAdjustmentAmount)) {
-                                                try {
-                                                    var partyPK = getPartyPK();
-                                                    var filterAdjustmentAmountValue = filterControl.getFilterAdjustmentAmountValue(filterAdjustmentAmount);
-                                                    
-                                                    filterAdjustmentAmountValue.setAmount(Long.valueOf(edit.getAmount()));
-                                                    
-                                                    filterControl.updateFilterAdjustmentAmountFromValue(filterAdjustmentAmountValue, partyPK);
-                                                } finally {
-                                                    unlockEntity(filterAdjustmentAmount);
-                                                }
-                                            } else {
-                                                addExecutionError(ExecutionErrors.EntityLockStale.name());
-                                            }
-                                        } else {
-                                            addExecutionError(ExecutionErrors.UnknownFilterAdjustmentAmount.name());
-                                        }
+                                    }
+
+                                    if(filterAdjustmentAmount == null) {
+                                        addExecutionError(ExecutionErrors.UnknownFilterAdjustmentAmount.name());
                                     }
                                 } else {
                                     addExecutionError(ExecutionErrors.UnknownCurrencyIsoName.name(), currencyIsoName);
@@ -217,8 +196,39 @@ public class EditFilterAdjustmentAmountCommand
         } else {
             addExecutionError(ExecutionErrors.UnknownFilterKindName.name(), filterKindName);
         }
-        
-        return result;
+
+        return filterAdjustmentAmount;
     }
-    
+
+    @Override
+    public FilterAdjustment getLockEntity(FilterAdjustmentAmount filterAdjustmentAmount) {
+        return filterAdjustmentAmount.getFilterAdjustment();
+    }
+
+    @Override
+    public void fillInResult(EditFilterAdjustmentAmountResult result, FilterAdjustmentAmount filterAdjustmentAmount) {
+        result.setFilterAdjustmentAmount(filterControl.getFilterAdjustmentAmountTransfer(getUserVisit(), filterAdjustmentAmount));
+    }
+
+    @Override
+    public void doLock(FilterAdjustmentAmountEdit edit, FilterAdjustmentAmount filterAdjustmentAmount) {
+        var filterKindName = spec.getFilterKindName();
+        var currency = filterAdjustmentAmount.getCurrency();
+
+        if(filterKindName.equals(FilterKinds.COST.name())) {
+            edit.setAmount(AmountUtils.getInstance().formatCostUnit(currency, filterAdjustmentAmount.getAmount()));
+        } else if(filterKindName.equals(FilterKinds.PRICE.name())) {
+            edit.setAmount(AmountUtils.getInstance().formatPriceUnit(currency, filterAdjustmentAmount.getAmount()));
+        }
+    }
+
+    @Override
+    public void doUpdate(FilterAdjustmentAmount filterAdjustmentAmount) {
+        var filterAdjustmentAmountValue = filterControl.getFilterAdjustmentAmountValue(filterAdjustmentAmount);
+
+        filterAdjustmentAmountValue.setAmount(Long.valueOf(edit.getAmount()));
+
+        filterControl.updateFilterAdjustmentAmountFromValue(filterAdjustmentAmountValue, getPartyPK());
+    }
+
 }
