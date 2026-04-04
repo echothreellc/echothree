14bddb78ff1b25880252189ee89b917eab894655
Richard Harms <rich@echothree.com>
2026-04-04 14:20:52 -0500

    #1031: Converted EditVendorTypeDescription UC to extend BaseAbstractEditCommand

--- src/java/com/echothree/control/user/vendor/server/command/EditVendorTypeDescriptionCommand.java
+++ src/java/com/echothree/control/user/vendor/server/command/EditVendorTypeDescriptionCommand.java
@@ -18,7 +18,7 @@ package com.echothree.control.user.vendor.server.command;
 
 import com.echothree.control.user.vendor.common.edit.VendorEditFactory;
 import com.echothree.control.user.vendor.common.edit.VendorTypeDescriptionEdit;
-import com.echothree.control.user.vendor.common.form.EditVendorTypeDescriptionForm;
+import com.echothree.control.user.vendor.common.result.EditVendorTypeDescriptionResult;
 import com.echothree.control.user.vendor.common.result.VendorResultFactory;
 import com.echothree.control.user.vendor.common.spec.VendorTypeDescriptionSpec;
 import com.echothree.model.control.party.common.PartyTypes;
@@ -26,23 +26,23 @@ import com.echothree.model.control.party.server.control.PartyControl;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.control.vendor.server.control.VendorControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.vendor.server.entity.VendorType;
+import com.echothree.model.data.vendor.server.entity.VendorTypeDescription;
+import com.echothree.util.common.command.EditMode;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.common.command.EditMode;
-import com.echothree.util.server.control.BaseEditCommand;
+import com.echothree.util.server.control.BaseAbstractEditCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class EditVendorTypeDescriptionCommand
-        extends BaseEditCommand<VendorTypeDescriptionSpec, VendorTypeDescriptionEdit> {
+        extends BaseAbstractEditCommand<VendorTypeDescriptionSpec, VendorTypeDescriptionEdit, EditVendorTypeDescriptionResult, VendorTypeDescription, VendorType> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> SPEC_FIELD_DEFINITIONS;
@@ -53,17 +53,17 @@ public class EditVendorTypeDescriptionCommand
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
                 new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
                         new SecurityRoleDefinition(SecurityRoleGroups.VendorType.name(), SecurityRoles.Description.name())
-                        ))
-                ));
+                ))
+        ));
         
         SPEC_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("VendorTypeName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("LanguageIsoName", FieldType.ENTITY_NAME, true, null, null)
-                );
+        );
         
         EDIT_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("Description", FieldType.STRING, true, 1L, 132L)
-                );
+        );
     }
     
     /** Creates a new instance of EditVendorTypeDescriptionCommand */
@@ -71,58 +71,41 @@ public class EditVendorTypeDescriptionCommand
         super(COMMAND_SECURITY_DEFINITION, SPEC_FIELD_DEFINITIONS, EDIT_FIELD_DEFINITIONS);
     }
     
+    @Inject
+    VendorControl vendorControl;
+
+    @Inject
+    PartyControl partyControl;
+
     @Override
-    protected BaseResult execute() {
-        var vendorControl = Session.getModelController(VendorControl.class);
-        var result = VendorResultFactory.getEditVendorTypeDescriptionResult();
+    protected EditVendorTypeDescriptionResult getResult() {
+        return VendorResultFactory.getEditVendorTypeDescriptionResult();
+    }
+
+    @Override
+    protected VendorTypeDescriptionEdit getEdit() {
+        return VendorEditFactory.getVendorTypeDescriptionEdit();
+    }
+
+    @Override
+    protected VendorTypeDescription getEntity(EditVendorTypeDescriptionResult result) {
+        VendorTypeDescription vendorTypeDescription = null;
         var vendorTypeName = spec.getVendorTypeName();
         var vendorType = vendorControl.getVendorTypeByName(vendorTypeName);
-        
+
         if(vendorType != null) {
-            var partyControl = Session.getModelController(PartyControl.class);
             var languageIsoName = spec.getLanguageIsoName();
             var language = partyControl.getLanguageByIsoName(languageIsoName);
-            
+
             if(language != null) {
-                if(editMode.equals(EditMode.LOCK)) {
-                    var vendorTypeDescription = vendorControl.getVendorTypeDescription(vendorType, language);
-                    
-                    if(vendorTypeDescription != null) {
-                        result.setVendorTypeDescription(vendorControl.getVendorTypeDescriptionTransfer(getUserVisit(), vendorTypeDescription));
-                        
-                        if(lockEntity(vendorType)) {
-                            var edit = VendorEditFactory.getVendorTypeDescriptionEdit();
-                            
-                            result.setEdit(edit);
-                            edit.setDescription(vendorTypeDescription.getDescription());
-                        } else {
-                            addExecutionError(ExecutionErrors.EntityLockFailed.name());
-                        }
-                        
-                        result.setEntityLock(getEntityLockTransfer(vendorType));
-                    } else {
-                        addExecutionError(ExecutionErrors.UnknownVendorTypeDescription.name());
-                    }
-                } else if(editMode.equals(EditMode.UPDATE)) {
-                    var vendorTypeDescriptionValue = vendorControl.getVendorTypeDescriptionValueForUpdate(vendorType, language);
-                    
-                    if(vendorTypeDescriptionValue != null) {
-                        if(lockEntityForUpdate(vendorType)) {
-                            try {
-                                var description = edit.getDescription();
-                                
-                                vendorTypeDescriptionValue.setDescription(description);
-                                
-                                vendorControl.updateVendorTypeDescriptionFromValue(vendorTypeDescriptionValue, getPartyPK());
-                            } finally {
-                                unlockEntity(vendorType);
-                            }
-                        } else {
-                            addExecutionError(ExecutionErrors.EntityLockStale.name());
-                        }
-                    } else {
-                        addExecutionError(ExecutionErrors.UnknownVendorTypeDescription.name());
-                    }
+                if(editMode.equals(EditMode.LOCK) || editMode.equals(EditMode.ABANDON)) {
+                    vendorTypeDescription = vendorControl.getVendorTypeDescription(vendorType, language);
+                } else { // EditMode.UPDATE
+                    vendorTypeDescription = vendorControl.getVendorTypeDescriptionForUpdate(vendorType, language);
+                }
+
+                if(vendorTypeDescription == null) {
+                    addExecutionError(ExecutionErrors.UnknownVendorTypeDescription.name(), vendorTypeName, languageIsoName);
                 }
             } else {
                 addExecutionError(ExecutionErrors.UnknownLanguageIsoName.name(), languageIsoName);
@@ -130,8 +113,32 @@ public class EditVendorTypeDescriptionCommand
         } else {
             addExecutionError(ExecutionErrors.UnknownVendorTypeName.name(), vendorTypeName);
         }
-        
-        return result;
+
+        return vendorTypeDescription;
+    }
+
+    @Override
+    protected VendorType getLockEntity(VendorTypeDescription vendorTypeDescription) {
+        return vendorTypeDescription.getVendorType();
+    }
+
+    @Override
+    protected void fillInResult(EditVendorTypeDescriptionResult result, VendorTypeDescription vendorTypeDescription) {
+        result.setVendorTypeDescription(vendorControl.getVendorTypeDescriptionTransfer(getUserVisit(), vendorTypeDescription));
+    }
+
+    @Override
+    protected void doLock(VendorTypeDescriptionEdit edit, VendorTypeDescription vendorTypeDescription) {
+        edit.setDescription(vendorTypeDescription.getDescription());
+    }
+
+    @Override
+    protected void doUpdate(VendorTypeDescription vendorTypeDescription) {
+        var vendorTypeDescriptionValue = vendorControl.getVendorTypeDescriptionValue(vendorTypeDescription);
+
+        vendorTypeDescriptionValue.setDescription(edit.getDescription());
+
+        vendorControl.updateVendorTypeDescriptionFromValue(vendorTypeDescriptionValue, getPartyPK());
     }
     
 }
