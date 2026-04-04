6a9ff327c4b2ea90bde80e0c35bc8db642e938e0
Richard Harms <rich@echothree.com>
2026-04-04 14:09:50 -0500

    #985: EditJob UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/job/server/command/EditJobCommand.java
+++ src/java/com/echothree/control/user/job/server/command/EditJobCommand.java
@@ -18,30 +18,29 @@ package com.echothree.control.user.job.server.command;
 
 import com.echothree.control.user.job.common.edit.JobEdit;
 import com.echothree.control.user.job.common.edit.JobEditFactory;
-import com.echothree.control.user.job.common.form.EditJobForm;
+import com.echothree.control.user.job.common.result.EditJobResult;
 import com.echothree.control.user.job.common.result.JobResultFactory;
 import com.echothree.control.user.job.common.spec.JobSpec;
 import com.echothree.model.control.job.server.control.JobControl;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.job.server.entity.Job;
+import com.echothree.util.common.command.EditMode;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.common.command.EditMode;
-import com.echothree.util.server.control.BaseEditCommand;
+import com.echothree.util.server.control.BaseAbstractEditCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class EditJobCommand
-        extends BaseEditCommand<JobSpec, JobEdit> {
+        extends BaseAbstractEditCommand<JobSpec, JobEdit, EditJobResult, Job, Job> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> SPEC_FIELD_DEFINITIONS;
@@ -51,19 +50,19 @@ public class EditJobCommand
         COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
                 new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
-                    new SecurityRoleDefinition(SecurityRoleGroups.Job.name(), SecurityRoles.Edit.name())
-                    ))
-                ));
+                        new SecurityRoleDefinition(SecurityRoleGroups.Job.name(), SecurityRoles.Edit.name())
+                ))
+        ));
         
         SPEC_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("JobName", FieldType.ENTITY_NAME, true, null, null)
-                );
+        );
         
         EDIT_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("JobName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("SortOrder", FieldType.SIGNED_INTEGER, true, null, null),
                 new FieldDefinition("Description", FieldType.STRING, false, 1L, 132L)
-                );
+        );
     }
     
     /** Creates a new instance of EditJobCommand */
@@ -71,88 +70,92 @@ public class EditJobCommand
         super(COMMAND_SECURITY_DEFINITION, SPEC_FIELD_DEFINITIONS, EDIT_FIELD_DEFINITIONS);
     }
     
+    @Inject
+    JobControl jobControl;
+
     @Override
-    protected BaseResult execute() {
-        var jobControl = Session.getModelController(JobControl.class);
-        var result = JobResultFactory.getEditJobResult();
-        
-        if(editMode.equals(EditMode.LOCK)) {
-            var jobName = spec.getJobName();
-            var job = jobControl.getJobByName(jobName);
-            
-            if(job != null) {
-                result.setJob(jobControl.getJobTransfer(getUserVisit(), job));
-                
-                if(lockEntity(job)) {
-                    var jobDescription = jobControl.getJobDescription(job, getPreferredLanguage());
-                    var edit = JobEditFactory.getJobEdit();
-                    var jobDetail = job.getLastDetail();
-                    
-                    result.setEdit(edit);
-                    edit.setJobName(jobDetail.getJobName());
-                    edit.setSortOrder(jobDetail.getSortOrder().toString());
-                    
-                    if(jobDescription != null)
-                        edit.setDescription(jobDescription.getDescription());
-                } else {
-                    addExecutionError(ExecutionErrors.EntityLockFailed.name());
-                }
-                
-                result.setEntityLock(getEntityLockTransfer(job));
-            } else {
-                addExecutionError(ExecutionErrors.UnknownJobName.name(), jobName);
-            }
-        } else if(editMode.equals(EditMode.UPDATE)) {
-            var jobName = spec.getJobName();
-            var job = jobControl.getJobByNameForUpdate(jobName);
-            
-            if(job != null) {
-                jobName = edit.getJobName();
-                var duplicateJob = jobControl.getJobByName(jobName);
-                
-                if(duplicateJob == null || job.equals(duplicateJob)) {
-                    if(lockEntityForUpdate(job)) {
-                        try {
-                            var partyPK = getPartyPK();
-                            var jobDetailValue = jobControl.getJobDetailValueForUpdate(job);
-                            var jobDescription = jobControl.getJobDescriptionForUpdate(job, getPreferredLanguage());
-                            var description = edit.getDescription();
-                            
-                            jobDetailValue.setJobName(edit.getJobName());
-                            jobDetailValue.setSortOrder(Integer.valueOf(edit.getSortOrder()));
-                            
-                            jobControl.updateJobFromValue(jobDetailValue, partyPK);
-                            
-                            if(jobDescription == null && description != null) {
-                                jobControl.createJobDescription(job, getPreferredLanguage(), description, partyPK);
-                            } else if(jobDescription != null && description == null) {
-                                jobControl.deleteJobDescription(jobDescription, partyPK);
-                            } else if(jobDescription != null && description != null) {
-                                var jobDescriptionValue = jobControl.getJobDescriptionValue(jobDescription);
-                                
-                                jobDescriptionValue.setDescription(description);
-                                jobControl.updateJobDescriptionFromValue(jobDescriptionValue, partyPK);
-                            }
-                        } finally {
-                            unlockEntity(job);
-                        }
-                    } else {
-                        addExecutionError(ExecutionErrors.EntityLockStale.name());
-                    }
-                } else {
-                    addExecutionError(ExecutionErrors.DuplicateJobName.name(), jobName);
-                }
-            } else {
-                addExecutionError(ExecutionErrors.UnknownJobName.name(), jobName);
-            }
-            
-            if(hasExecutionErrors()) {
-                result.setJob(jobControl.getJobTransfer(getUserVisit(), job));
-                result.setEntityLock(getEntityLockTransfer(job));
-            }
+    protected EditJobResult getResult() {
+        return JobResultFactory.getEditJobResult();
+    }
+
+    @Override
+    protected JobEdit getEdit() {
+        return JobEditFactory.getJobEdit();
+    }
+
+    @Override
+    protected Job getEntity(EditJobResult result) {
+        Job job;
+        var jobName = spec.getJobName();
+
+        if(editMode.equals(EditMode.LOCK) || editMode.equals(EditMode.ABANDON)) {
+            job = jobControl.getJobByName(jobName);
+        } else { // EditMode.UPDATE
+            job = jobControl.getJobByNameForUpdate(jobName);
         }
-        
-        return result;
+
+        if(job == null) {
+            addExecutionError(ExecutionErrors.UnknownJobName.name(), jobName);
+        }
+
+        return job;
+    }
+
+    @Override
+    protected Job getLockEntity(Job job) {
+        return job;
+    }
+
+    @Override
+    protected void fillInResult(EditJobResult result, Job job) {
+        result.setJob(jobControl.getJobTransfer(getUserVisit(), job));
+    }
+
+    @Override
+    protected void doLock(JobEdit edit, Job job) {
+        var jobDescription = jobControl.getJobDescription(job, getPreferredLanguage());
+        var jobDetail = job.getLastDetail();
+
+        edit.setJobName(jobDetail.getJobName());
+        edit.setSortOrder(jobDetail.getSortOrder().toString());
+
+        if(jobDescription != null) {
+            edit.setDescription(jobDescription.getDescription());
+        }
+    }
+
+    @Override
+    protected void canUpdate(Job job) {
+        var jobName = edit.getJobName();
+        var duplicateJob = jobControl.getJobByName(jobName);
+
+        if(duplicateJob != null && !job.equals(duplicateJob)) {
+            addExecutionError(ExecutionErrors.DuplicateJobName.name(), jobName);
+        }
+    }
+
+    @Override
+    protected void doUpdate(Job job) {
+        var partyPK = getPartyPK();
+        var jobDetailValue = jobControl.getJobDetailValueForUpdate(job);
+        var jobDescription = jobControl.getJobDescriptionForUpdate(job, getPreferredLanguage());
+        var description = edit.getDescription();
+
+        jobDetailValue.setJobName(edit.getJobName());
+        jobDetailValue.setSortOrder(Integer.valueOf(edit.getSortOrder()));
+
+        jobControl.updateJobFromValue(jobDetailValue, partyPK);
+
+        if(jobDescription == null && description != null) {
+            jobControl.createJobDescription(job, getPreferredLanguage(), description, partyPK);
+        } else if(jobDescription != null && description == null) {
+            jobControl.deleteJobDescription(jobDescription, partyPK);
+        } else if(jobDescription != null && description != null) {
+            var jobDescriptionValue = jobControl.getJobDescriptionValue(jobDescription);
+
+            jobDescriptionValue.setDescription(description);
+            jobControl.updateJobDescriptionFromValue(jobDescriptionValue, partyPK);
+        }
     }
     
 }
