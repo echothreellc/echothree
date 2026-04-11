a2d21b4da7f6d51acf32e059337ed3b5235b7510
Richard Harms <rich@echothree.com>
2026-04-10 20:16:59 -0500

    #1015: Converted EditUnitOfMeasureTypeWeight UC to extend BaseAbstractEditCommand

--- src/java/com/echothree/control/user/uom/server/command/EditUnitOfMeasureTypeWeightCommand.java
+++ src/java/com/echothree/control/user/uom/server/command/EditUnitOfMeasureTypeWeightCommand.java
@@ -18,26 +18,26 @@ package com.echothree.control.user.uom.server.command;
 
 import com.echothree.control.user.uom.common.edit.UnitOfMeasureTypeWeightEdit;
 import com.echothree.control.user.uom.common.edit.UomEditFactory;
-import com.echothree.control.user.uom.common.form.EditUnitOfMeasureTypeWeightForm;
+import com.echothree.control.user.uom.common.result.EditUnitOfMeasureTypeWeightResult;
 import com.echothree.control.user.uom.common.result.UomResultFactory;
 import com.echothree.control.user.uom.common.spec.UnitOfMeasureTypeSpec;
 import com.echothree.model.control.uom.common.UomConstants;
 import com.echothree.model.control.uom.server.control.UomControl;
 import com.echothree.model.control.uom.server.util.Conversion;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.uom.server.entity.UnitOfMeasureKind;
+import com.echothree.model.data.uom.server.entity.UnitOfMeasureType;
+import com.echothree.model.data.uom.server.entity.UnitOfMeasureTypeWeight;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.common.command.EditMode;
-import com.echothree.util.server.control.BaseEditCommand;
-import com.echothree.util.server.persistence.Session;
+import com.echothree.util.server.control.BaseAbstractEditCommand;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class EditUnitOfMeasureTypeWeightCommand
-        extends BaseEditCommand<UnitOfMeasureTypeSpec, UnitOfMeasureTypeWeightEdit> {
+        extends BaseAbstractEditCommand<UnitOfMeasureTypeSpec, UnitOfMeasureTypeWeightEdit, EditUnitOfMeasureTypeWeightResult, UnitOfMeasureTypeWeight, UnitOfMeasureType> {
     
     private final static List<FieldDefinition> SPEC_FIELD_DEFINITIONS;
     private final static List<FieldDefinition> EDIT_FIELD_DEFINITIONS;
@@ -46,12 +46,12 @@ public class EditUnitOfMeasureTypeWeightCommand
         SPEC_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("UnitOfMeasureKindName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("UnitOfMeasureTypeName", FieldType.ENTITY_NAME, true, null, null)
-                );
+        );
         
         EDIT_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("WeightUnitOfMeasureTypeName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("Weight", FieldType.UNSIGNED_LONG, true, null, null)
-                );
+        );
     }
     
     /** Creates a new instance of EditUnitOfMeasureTypeWeightCommand */
@@ -59,80 +59,34 @@ public class EditUnitOfMeasureTypeWeightCommand
         super(null, SPEC_FIELD_DEFINITIONS, EDIT_FIELD_DEFINITIONS);
     }
     
+    @Inject
+    UomControl uomControl;
+
     @Override
-    protected BaseResult execute() {
-        var uomControl = Session.getModelController(UomControl.class);
-        var result = UomResultFactory.getEditUnitOfMeasureTypeWeightResult();
+    protected EditUnitOfMeasureTypeWeightResult getResult() {
+        return UomResultFactory.getEditUnitOfMeasureTypeWeightResult();
+    }
+
+    @Override
+    protected UnitOfMeasureTypeWeightEdit getEdit() {
+        return UomEditFactory.getUnitOfMeasureTypeWeightEdit();
+    }
+
+    @Override
+    protected UnitOfMeasureTypeWeight getEntity(EditUnitOfMeasureTypeWeightResult result) {
+        UnitOfMeasureTypeWeight unitOfMeasureTypeWeight = null;
         var unitOfMeasureKindName = spec.getUnitOfMeasureKindName();
         var unitOfMeasureKind = uomControl.getUnitOfMeasureKindByName(unitOfMeasureKindName);
-        
+
         if(unitOfMeasureKind != null) {
             var unitOfMeasureTypeName = spec.getUnitOfMeasureTypeName();
             var unitOfMeasureType = uomControl.getUnitOfMeasureTypeByName(unitOfMeasureKind, unitOfMeasureTypeName);
-            
+
             if(unitOfMeasureType != null) {
-                var volumeUnitOfMeasureKind = uomControl.getUnitOfMeasureKindByUnitOfMeasureKindUseTypeUsingNames(UomConstants.UnitOfMeasureKindUseType_WEIGHT);
-                
-                if(volumeUnitOfMeasureKind != null) {
-                    if(editMode.equals(EditMode.LOCK)) {
-                        var unitOfMeasureTypeWeight = uomControl.getUnitOfMeasureTypeWeight(unitOfMeasureType);
-                        
-                        if(unitOfMeasureTypeWeight != null) {
-                            result.setUnitOfMeasureTypeWeight(uomControl.getUnitOfMeasureTypeWeightTransfer(getUserVisit(), unitOfMeasureTypeWeight));
-                            
-                            if(lockEntity(unitOfMeasureType)) {
-                                var edit = UomEditFactory.getUnitOfMeasureTypeWeightEdit();
-                                var weight = unitOfMeasureTypeWeight.getWeight();
-                                var weightConversion = weight == null? null: new Conversion(uomControl, volumeUnitOfMeasureKind, weight).convertToHighestUnitOfMeasureType();
-                                
-                                result.setEdit(edit);
-                                edit.setWeight(weightConversion.getQuantity().toString());
-                                edit.setWeightUnitOfMeasureTypeName(weightConversion.getUnitOfMeasureType().getLastDetail().getUnitOfMeasureTypeName());
-                            } else {
-                                addExecutionError(ExecutionErrors.EntityLockFailed.name());
-                            }
-                            
-                            result.setEntityLock(getEntityLockTransfer(unitOfMeasureType));
-                        } else {
-                            addExecutionError(ExecutionErrors.UnknownUnitOfMeasureTypeWeight.name());
-                        }
-                    } else if(editMode.equals(EditMode.UPDATE)) {
-                        var unitOfMeasureTypeWeightValue = uomControl.getUnitOfMeasureTypeWeightValueForUpdate(unitOfMeasureType);
-                        
-                        if(unitOfMeasureTypeWeightValue != null) {
-                            var weightUnitOfMeasureTypeName = edit.getWeightUnitOfMeasureTypeName();
-                            var weightUnitOfMeasureType = uomControl.getUnitOfMeasureTypeByName(volumeUnitOfMeasureKind,
-                                    weightUnitOfMeasureTypeName);
-                            
-                            if(weightUnitOfMeasureType != null) {
-                                var weight = Long.valueOf(edit.getWeight());
-                                
-                                if(weight > 0) {
-                                    if(lockEntityForUpdate(unitOfMeasureType)) {
-                                        try {
-                                            var weightConversion = new Conversion(uomControl, weightUnitOfMeasureType, weight).convertToLowestUnitOfMeasureType();
-                                            
-                                            unitOfMeasureTypeWeightValue.setWeight(weightConversion.getQuantity());
-                                            
-                                            uomControl.updateUnitOfMeasureTypeWeightFromValue(unitOfMeasureTypeWeightValue, getPartyPK());
-                                        } finally {
-                                            unlockEntity(unitOfMeasureType);
-                                        }
-                                    } else {
-                                        addExecutionError(ExecutionErrors.EntityLockStale.name());
-                                    }
-                                } else {
-                                    addExecutionError(ExecutionErrors.InvalidWeight.name(), weight);
-                                }
-                            } else {
-                                addExecutionError(ExecutionErrors.UnknownWeightUnitOfMeasureTypeName.name(), weightUnitOfMeasureTypeName);
-                            }
-                        } else {
-                            addExecutionError(ExecutionErrors.UnknownUnitOfMeasureTypeWeight.name());
-                        }
-                    }
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownWeightUnitOfMeasureKind.name());
+                unitOfMeasureTypeWeight = uomControl.getUnitOfMeasureTypeWeight(unitOfMeasureType);
+
+                if(unitOfMeasureTypeWeight == null) {
+                    addExecutionError(ExecutionErrors.UnknownUnitOfMeasureTypeWeight.name());
                 }
             } else {
                 addExecutionError(ExecutionErrors.UnknownUnitOfMeasureTypeName.name(), unitOfMeasureTypeName);
@@ -140,8 +94,75 @@ public class EditUnitOfMeasureTypeWeightCommand
         } else {
             addExecutionError(ExecutionErrors.UnknownUnitOfMeasureKindName.name(), unitOfMeasureKindName);
         }
-        
-        return result;
+
+        return unitOfMeasureTypeWeight;
+    }
+
+    @Override
+    protected UnitOfMeasureType getLockEntity(UnitOfMeasureTypeWeight unitOfMeasureTypeWeight) {
+        return unitOfMeasureTypeWeight.getUnitOfMeasureType();
+    }
+
+    @Override
+    protected void fillInResult(EditUnitOfMeasureTypeWeightResult result, UnitOfMeasureTypeWeight unitOfMeasureTypeWeight) {
+        result.setUnitOfMeasureTypeWeight(uomControl.getUnitOfMeasureTypeWeightTransfer(getUserVisit(), unitOfMeasureTypeWeight));
+    }
+
+    UnitOfMeasureKind weightUnitOfMeasureKind;
+
+    private UnitOfMeasureKind getWeightUnitOfMeasureKind() {
+        if(weightUnitOfMeasureKind == null) {
+            weightUnitOfMeasureKind = uomControl.getUnitOfMeasureKindByUnitOfMeasureKindUseTypeUsingNames(UomConstants.UnitOfMeasureKindUseType_WEIGHT);
+        }
+
+        return weightUnitOfMeasureKind;
+    }
+
+    @Override
+    protected void doLock(UnitOfMeasureTypeWeightEdit edit, UnitOfMeasureTypeWeight unitOfMeasureTypeWeight) {
+        var weightUnitOfMeasureKind = getWeightUnitOfMeasureKind();
+        var weight = unitOfMeasureTypeWeight.getWeight();
+        var weightConversion = weight == null? null: new Conversion(uomControl, weightUnitOfMeasureKind, weight).convertToHighestUnitOfMeasureType();
+
+        if(weightConversion != null) {
+            edit.setWeight(weightConversion.getQuantity().toString());
+            edit.setWeightUnitOfMeasureTypeName(weightConversion.getUnitOfMeasureType().getLastDetail().getUnitOfMeasureTypeName());
+        }
+    }
+
+    Long weight;
+
+    @Override
+    protected void canUpdate(UnitOfMeasureTypeWeight unitOfMeasureTypeWeight) {
+        var weightUnitOfMeasureKind = getWeightUnitOfMeasureKind();
+
+        if(weightUnitOfMeasureKind != null) {
+            var weightUnitOfMeasureTypeName = edit.getWeightUnitOfMeasureTypeName();
+            var weightUnitOfMeasureType = uomControl.getUnitOfMeasureTypeByName(weightUnitOfMeasureKind, weightUnitOfMeasureTypeName);
+
+            if(weightUnitOfMeasureType != null) {
+                var weightValue = Long.valueOf(edit.getWeight());
+
+                if(weightValue > 0) {
+                    weight = new Conversion(uomControl, weightUnitOfMeasureType, weightValue).convertToLowestUnitOfMeasureType().getQuantity();
+                } else {
+                    addExecutionError(ExecutionErrors.InvalidWeight.name(), weightValue);
+                }
+            } else {
+                addExecutionError(ExecutionErrors.UnknownWeightUnitOfMeasureTypeName.name(), weightUnitOfMeasureTypeName);
+            }
+        } else {
+            addExecutionError(ExecutionErrors.UnknownWeightUnitOfMeasureKind.name());
+        }
+    }
+
+    @Override
+    protected void doUpdate(UnitOfMeasureTypeWeight unitOfMeasureTypeWeight) {
+        var unitOfMeasureTypeWeightValue = uomControl.getUnitOfMeasureTypeWeightValue(unitOfMeasureTypeWeight);
+
+        unitOfMeasureTypeWeightValue.setWeight(weight);
+
+        uomControl.updateUnitOfMeasureTypeWeightFromValue(unitOfMeasureTypeWeightValue, getPartyPK());
     }
     
 }
