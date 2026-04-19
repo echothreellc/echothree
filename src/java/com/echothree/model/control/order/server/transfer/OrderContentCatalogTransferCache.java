a532d1237bb5174f539e445eb562df3e3c53dce2
Richard Harms <rich@echothree.com>
2026-04-18 21:27:05 -0500

    #1759: Added OrderContentCatalog entity

---
+++ src/java/com/echothree/model/control/order/server/transfer/OrderContentCatalogTransferCache.java
@@ -0,0 +1,51 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2026 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.order.server.transfer;
+
+import com.echothree.model.control.content.server.control.ContentControl;
+import com.echothree.model.control.order.common.transfer.OrderContentCatalogTransfer;
+import com.echothree.model.data.order.server.entity.OrderContentCatalog;
+import com.echothree.model.data.user.server.entity.UserVisit;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+@RequestScoped
+public class OrderContentCatalogTransferCache
+        extends BaseOrderTransferCache<OrderContentCatalog, OrderContentCatalogTransfer> {
+
+    @Inject
+    ContentControl contentControl;
+
+    /** Creates a new instance of OrderContentCatalogTransferCache */
+    protected OrderContentCatalogTransferCache() {
+        super();
+    }
+    
+    public OrderContentCatalogTransfer getOrderContentCatalogTransfer(UserVisit userVisit, OrderContentCatalog orderContentCatalog) {
+        var orderContentCatalogTransfer = get(orderContentCatalog);
+        
+        if(orderContentCatalogTransfer == null) {
+            var contentCatalog = contentControl.getContentCatalogTransfer(userVisit, orderContentCatalog.getContentCatalog());
+
+            orderContentCatalogTransfer = new OrderContentCatalogTransfer(contentCatalog);
+            put(userVisit, orderContentCatalog, orderContentCatalogTransfer);
+        }
+        
+        return orderContentCatalogTransfer;
+    }
+    
+}
