1c5c1ab719943fbb5d934ad2a42817bed6ed9a3d
Richard Harms <rich@echothree.com>
2026-04-19 18:40:52 -0500

    #1132: Converted GetSources UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/offer/server/command/GetSourcesCommand.java
+++ src/java/com/echothree/control/user/offer/server/command/GetSourcesCommand.java
@@ -23,21 +23,21 @@ import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.data.offer.server.entity.Source;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.model.data.offer.server.factory.SourceFactory;
 import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseMultipleEntitiesCommand;
+import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetSourcesCommand
-        extends BaseMultipleEntitiesCommand<Source, GetSourcesForm> {
+        extends BasePaginatedMultipleEntitiesCommand<Source, GetSourcesForm> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -47,31 +47,46 @@ public class GetSourcesCommand
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
                 new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
                         new SecurityRoleDefinition(SecurityRoleGroups.Source.name(), SecurityRoles.List.name())
-                        ))
-                ));
+                ))
+        ));
         
-        FORM_FIELD_DEFINITIONS = List.of(
-                );
+        FORM_FIELD_DEFINITIONS = List.of();
     }
-    
+
+    @Inject
+    SourceControl sourceControl;
+
     /** Creates a new instance of GetSourcesCommand */
     public GetSourcesCommand() {
         super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
     
     @Override
+    protected void handleForm() {
+        // No form fields.
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        return sourceControl.countSources();
+    }
+
+    @Override
     protected Collection<Source> getEntities() {
-        var sourceControl = Session.getModelController(SourceControl.class);
-        
         return sourceControl.getSources();
     }
     
     @Override
     protected BaseResult getResult(Collection<Source> entities) {
         var result = OfferResultFactory.getGetSourcesResult();
-        var sourceControl = Session.getModelController(SourceControl.class);
-        
-        result.setSources(sourceControl.getSourceTransfers(getUserVisit(), entities));
+
+        if(entities != null) {
+            if(session.hasLimit(SourceFactory.class)) {
+                result.setSourceCount(getTotalEntities());
+            }
+
+            result.setSources(sourceControl.getSourceTransfers(getUserVisit(), entities));
+        }
         
         return result;
     }
