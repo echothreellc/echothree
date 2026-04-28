c81a13dbad06f7c22d9e12f1b5f70a0cf03c2f27
Richard Harms <rich@echothree.com>
2026-04-28 18:45:49 -0500

    #1194: Converted GetContentForums UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/content/server/command/GetContentForumsCommand.java
+++ src/java/com/echothree/control/user/content/server/command/GetContentForumsCommand.java
@@ -19,68 +19,75 @@ package com.echothree.control.user.content.server.command;
 import com.echothree.control.user.content.common.form.GetContentForumsForm;
 import com.echothree.control.user.content.common.result.ContentResultFactory;
 import com.echothree.model.control.content.server.control.ContentControl;
+import com.echothree.model.control.content.server.logic.ContentLogic;
 import com.echothree.model.data.content.server.entity.ContentCollection;
 import com.echothree.model.data.content.server.entity.ContentForum;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.message.ExecutionErrors;
+import com.echothree.model.data.content.server.factory.ContentForumFactory;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseMultipleEntitiesCommand;
-import com.echothree.util.server.persistence.Session;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
 import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetContentForumsCommand
-        extends BaseMultipleEntitiesCommand<ContentForum, GetContentForumsForm> {
-    
+        extends BasePaginatedMultipleEntitiesCommand<ContentForum, GetContentForumsForm> {
+
+    @Inject
+    ContentControl contentControl;
+    @Inject
+    ContentLogic contentLogic;
+
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
-    
+
     static {
         FORM_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("ContentCollectionName", FieldType.ENTITY_NAME, true, null, null)
-                );
+        );
     }
-    
+
     /** Creates a new instance of GetContentForumsCommand */
     public GetContentForumsCommand() {
         super(null, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
     private ContentCollection contentCollection;
-    
+
+    @Override
+    protected void handleForm() {
+        contentCollection = contentLogic.getContentCollectionByName(this, form.getContentCollectionName());
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        return hasExecutionErrors() ? null : contentControl.countContentForumsByContentCollection(contentCollection);
+    }
+
     @Override
     protected Collection<ContentForum> getEntities() {
-        var contentControl = Session.getModelController(ContentControl.class);
-        var contentCollectionName = form.getContentCollectionName();
-        Collection<ContentForum> contentForums = null;
-        
-        contentCollection = contentControl.getContentCollectionByName(contentCollectionName);
-        
-        if(contentCollection != null) {
-            contentForums = contentControl.getContentForums(contentCollection);
-        } else {
-            addExecutionError(ExecutionErrors.UnknownContentCollectionName.name(), contentCollectionName);
-        }
-        
-        return contentForums;
+        return hasExecutionErrors() ? null : contentControl.getContentForums(contentCollection);
     }
-    
+
     @Override
     protected BaseResult getResult(Collection<ContentForum> entities) {
         var result = ContentResultFactory.getGetContentForumsResult();
-        
+
         if(entities != null) {
-            var contentControl = Session.getModelController(ContentControl.class);
             var userVisit = getUserVisit();
-            
+
             result.setContentCollection(contentControl.getContentCollectionTransfer(userVisit, contentCollection));
+
+            if(session.hasLimit(ContentForumFactory.class)) {
+                result.setContentForumCount(getTotalEntities());
+            }
+
             result.setContentForums(contentControl.getContentForumTransfers(userVisit, entities));
         }
-        
+
         return result;
     }
-    
+
 }
