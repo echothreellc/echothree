3c78774a0104a0aac1256436a269716d4cd33abc
Richard Harms <rich@echothree.com>
2026-04-30 20:21:27 -0500

    #1991: Converted GetSubscriptionKinds UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/subscription/server/command/GetSubscriptionKindsCommand.java
+++ src/java/com/echothree/control/user/subscription/server/command/GetSubscriptionKindsCommand.java
@@ -22,20 +22,22 @@ import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.control.subscription.server.control.SubscriptionControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.model.data.subscription.server.entity.SubscriptionKind;
+import com.echothree.model.data.subscription.server.factory.SubscriptionKindFactory;
 import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.common.validation.FieldDefinition;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
+import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetSubscriptionKindsCommand
-        extends BaseSimpleCommand<GetSubscriptionKindsForm> {
+        extends BasePaginatedMultipleEntitiesCommand<SubscriptionKind, GetSubscriptionKindsForm> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -44,26 +46,48 @@ public class GetSubscriptionKindsCommand
         COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
                 new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
-                    new SecurityRoleDefinition(SecurityRoleGroups.SubscriptionKind.name(), SecurityRoles.List.name())
-                    ))
-                ));
+                        new SecurityRoleDefinition(SecurityRoleGroups.SubscriptionKind.name(), SecurityRoles.List.name())
+                ))
+        ));
 
-        FORM_FIELD_DEFINITIONS = List.of(
-                );
+        FORM_FIELD_DEFINITIONS = List.of();
     }
+
+    @Inject
+    SubscriptionControl subscriptionControl;
 
     /** Creates a new instance of GetSubscriptionKindsCommand */
     public GetSubscriptionKindsCommand() {
         super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
     @Override
-    protected BaseResult execute() {
-        var subscriptionControl = Session.getModelController(SubscriptionControl.class);
+    protected void handleForm() {
+        // No form fields to handle
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        return subscriptionControl.countSubscriptionKinds();
+    }
+
+    @Override
+    protected Collection<SubscriptionKind> getEntities() {
+        return subscriptionControl.getSubscriptionKinds();
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<SubscriptionKind> entities) {
         var result = SubscriptionResultFactory.getGetSubscriptionKindsResult();
-        
-        result.setSubscriptionKinds(subscriptionControl.getSubscriptionKindTransfers(getUserVisit()));
-        
+
+        if(entities != null) {
+            if(session.hasLimit(SubscriptionKindFactory.class)) {
+                result.setSubscriptionKindCount(getTotalEntities());
+            }
+
+            result.setSubscriptionKinds(subscriptionControl.getSubscriptionKindTransfers(getUserVisit(), entities));
+        }
+
         return result;
     }
     
