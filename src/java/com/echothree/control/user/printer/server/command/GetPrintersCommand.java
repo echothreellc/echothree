95a63dc81b7c993fc6ccf89c7a5bc8f8c67f35f5
Richard Harms <rich@echothree.com>
2026-05-07 20:44:12 -0500

    #1921: Converted GetPrinters UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/printer/server/command/GetPrintersCommand.java
+++ src/java/com/echothree/control/user/printer/server/command/GetPrintersCommand.java
@@ -19,47 +19,82 @@ package com.echothree.control.user.printer.server.command;
 import com.echothree.control.user.printer.common.form.GetPrintersForm;
 import com.echothree.control.user.printer.common.result.PrinterResultFactory;
 import com.echothree.model.control.printer.server.control.PrinterControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.printer.server.entity.Printer;
+import com.echothree.model.data.printer.server.entity.PrinterGroup;
+import com.echothree.model.data.printer.server.factory.PrinterFactory;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
-import com.echothree.util.server.persistence.Session;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
+import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetPrintersCommand
-        extends BaseSimpleCommand<GetPrintersForm> {
-    
-   private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
+        extends BasePaginatedMultipleEntitiesCommand<Printer, GetPrintersForm> {
+
+    private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
 
     static {
         FORM_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("PrinterGroupName", FieldType.ENTITY_NAME, false, null, null)
-                );
+        );
     }
+
+    @Inject
+    PrinterControl printerControl;
 
     /** Creates a new instance of GetPrintersCommand */
     public GetPrintersCommand() {
         super(null, FORM_FIELD_DEFINITIONS, true);
     }
-    
-   @Override
-    protected BaseResult execute() {
-        var printerControl = Session.getModelController(PrinterControl.class);
-       var result = PrinterResultFactory.getGetPrintersResult();
-       var printerGroupName = form.getPrinterGroupName();
-       var printerGroup = printerControl.getPrinterGroupByName(printerGroupName);
-        
-        if(printerGroup == null) {
-            result.setPrinters(printerControl.getPrinterTransfers(getUserVisit()));
-        } else {
-            result.setPrinterGroup(printerControl.getPrinterGroupTransfer(getUserVisit(), printerGroup));
-            result.setPrinters(printerControl.getPrinterTransfersByPrinterGroup(getUserVisit(), printerGroup));
+
+    PrinterGroup printerGroup;
+
+    @Override
+    protected void handleForm() {
+        var printerGroupName = form.getPrinterGroupName();
+
+        if(printerGroupName != null) {
+            printerGroup = printerControl.getPrinterGroupByName(printerGroupName);
+
+            if(printerGroup == null) {
+                addExecutionError(com.echothree.util.common.message.ExecutionErrors.UnknownPrinterGroupName.name(), printerGroupName);
+            }
         }
-        
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        return hasExecutionErrors() ? null :
+                printerGroup == null ? printerControl.countPrinters() : printerControl.countPrintersByPrinterGroup(printerGroup);
+    }
+
+    @Override
+    protected Collection<Printer> getEntities() {
+        return hasExecutionErrors() ? null :
+                printerGroup == null ? printerControl.getPrinters() : printerControl.getPrintersByPrinterGroup(printerGroup);
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<Printer> entities) {
+        var result = PrinterResultFactory.getGetPrintersResult();
+
+        if(entities != null) {
+            if(printerGroup != null) {
+                result.setPrinterGroup(printerControl.getPrinterGroupTransfer(getUserVisit(), printerGroup));
+            }
+
+            if(session.hasLimit(PrinterFactory.class)) {
+                result.setPrinterCount(getTotalEntities());
+            }
+
+            result.setPrinters(printerControl.getPrinterTransfers(getUserVisit(), entities));
+        }
+
         return result;
     }
-    
+
 }
