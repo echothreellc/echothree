3bb4a634cc9bd8b33df183327888c28cbcaa8192
Richard Harms <rich@echothree.com>
2026-05-15 21:11:50 -0500

    #1818: Converted GetPartyTypeContactLists UC to Use BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/contactlist/server/command/GetPartyTypeContactListsCommand.java
+++ src/java/com/echothree/control/user/contactlist/server/command/GetPartyTypeContactListsCommand.java
@@ -19,26 +19,32 @@ package com.echothree.control.user.contactlist.server.command;
 import com.echothree.control.user.contactlist.common.form.GetPartyTypeContactListsForm;
 import com.echothree.control.user.contactlist.common.result.ContactListResultFactory;
 import com.echothree.model.control.contactlist.server.control.ContactListControl;
+import com.echothree.model.control.contactlist.server.logic.ContactListLogic;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.party.server.control.PartyControl;
+import com.echothree.model.control.party.server.logic.PartyLogic;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.contactlist.server.entity.ContactList;
+import com.echothree.model.data.contactlist.server.entity.PartyTypeContactList;
+import com.echothree.model.data.contactlist.server.factory.PartyTypeContactListFactory;
+import com.echothree.model.data.party.server.entity.PartyType;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
 import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
+import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetPartyTypeContactListsCommand
-        extends BaseSimpleCommand<GetPartyTypeContactListsForm> {
+        extends BasePaginatedMultipleEntitiesCommand<PartyTypeContactList, GetPartyTypeContactListsForm> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -47,56 +53,103 @@ public class GetPartyTypeContactListsCommand
         COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
                 new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
-                    new SecurityRoleDefinition(SecurityRoleGroups.ContactList.name(), SecurityRoles.PartyTypeContactList.name())
-                    ))
-                ));
+                        new SecurityRoleDefinition(SecurityRoleGroups.ContactList.name(), SecurityRoles.PartyTypeContactList.name())
+                ))
+        ));
 
         FORM_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("PartyTypeName", FieldType.ENTITY_NAME, false, null, null),
                 new FieldDefinition("ContactListName", FieldType.ENTITY_NAME, false, null, null)
-                );
+        );
     }
+
+    @Inject
+    ContactListControl contactListControl;
+
+    @Inject
+    PartyControl partyControl;
+
+    @Inject
+    ContactListLogic contactListLogic;
+
+    @Inject
+    PartyLogic partyLogic;
     
     /** Creates a new instance of GetPartyTypeContactListsCommand */
     public GetPartyTypeContactListsCommand() {
-        super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, false);
+        super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
+    private PartyType partyType;
+    private ContactList contactList;
+
     @Override
-    protected BaseResult execute() {
-        var result = ContactListResultFactory.getGetPartyTypeContactListsResult();
+    protected void handleForm() {
         var partyTypeName = form.getPartyTypeName();
         var contactListName = form.getContactListName();
-        var parameterCount = (partyTypeName != null? 1: 0) + (contactListName != null? 1: 0);
-        
+        var parameterCount = (partyTypeName != null ? 1 : 0) + (contactListName != null ? 1 : 0);
+
         if(parameterCount == 1) {
-            var contactListControl = Session.getModelController(ContactListControl.class);
-            var userVisit = getUserVisit();
-            
             if(partyTypeName != null) {
-                var partyControl = Session.getModelController(PartyControl.class);
-                var partyType = partyControl.getPartyTypeByName(partyTypeName);
-                
-                if(partyType != null) {
-                    result.setPartyType(partyControl.getPartyTypeTransfer(userVisit, partyType));
-                    result.setPartyTypeContactLists(contactListControl.getPartyTypeContactListTransfersByPartyType(userVisit, partyType));
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownPartyTypeName.name(), partyTypeName);
-                }
-            } else if(contactListName != null) {
-                var contactList = contactListControl.getContactListByName(contactListName);
-                
-                if(contactList != null) {
-                    result.setContactList(contactListControl.getContactListTransfer(userVisit, contactList));
-                    result.setPartyTypeContactLists(contactListControl.getPartyTypeContactListTransfersByContactList(userVisit, contactList));
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownContactListName.name(), contactListName);
-                }
+                partyType = partyLogic.getPartyTypeByName(this, partyTypeName);
+            } else {
+                contactList = contactListLogic.getContactListByName(this, contactListName);
             }
         } else {
             addExecutionError(ExecutionErrors.InvalidParameterCount.name());
         }
-        
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        Long total = null;
+
+        if(!hasExecutionErrors()) {
+            if(partyType != null) {
+                total = contactListControl.countPartyTypeContactListsByPartyType(partyType);
+            } else {
+                total = contactListControl.countPartyTypeContactListsByContactList(contactList);
+            }
+        }
+
+        return total;
+    }
+
+    @Override
+    protected Collection<PartyTypeContactList> getEntities() {
+        Collection<PartyTypeContactList> entities = null;
+
+        if(!hasExecutionErrors()) {
+            if(partyType != null) {
+                entities = contactListControl.getPartyTypeContactListsByPartyType(partyType);
+            } else {
+                entities = contactListControl.getPartyTypeContactListsByContactList(contactList);
+            }
+        }
+
+        return entities;
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<PartyTypeContactList> entities) {
+        var result = ContactListResultFactory.getGetPartyTypeContactListsResult();
+
+        if(!hasExecutionErrors()) {
+            var userVisit = getUserVisit();
+
+            if(session.hasLimit(PartyTypeContactListFactory.class)) {
+                result.setPartyTypeContactListCount(getTotalEntities());
+            }
+
+            if(partyType != null) {
+                result.setPartyType(partyControl.getPartyTypeTransfer(userVisit, partyType));
+            } else {
+                result.setContactList(contactListControl.getContactListTransfer(userVisit, contactList));
+            }
+
+            result.setPartyTypeContactLists(contactListControl.getPartyTypeContactListTransfers(userVisit, entities));
+        }
+
         return result;
     }
     
