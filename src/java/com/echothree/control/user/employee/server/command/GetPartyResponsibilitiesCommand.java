91431842d706703aacd2d767978d377a944fdf69
Richard Harms <rich@echothree.com>
2026-05-22 20:00:29 -0500

    #1849: Converted GetPartyResponsibilities UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/employee/server/command/GetPartyResponsibilitiesCommand.java
+++ src/java/com/echothree/control/user/employee/server/command/GetPartyResponsibilitiesCommand.java
@@ -20,68 +20,120 @@ import com.echothree.control.user.employee.common.form.GetPartyResponsibilitiesF
 import com.echothree.control.user.employee.common.result.EmployeeResultFactory;
 import com.echothree.model.control.employee.server.control.EmployeeControl;
 import com.echothree.model.control.party.server.control.PartyControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.control.party.server.logic.PartyLogic;
+import com.echothree.model.data.employee.server.entity.PartyResponsibility;
+import com.echothree.model.data.employee.server.entity.ResponsibilityType;
+import com.echothree.model.data.employee.server.factory.PartyResponsibilityFactory;
+import com.echothree.model.data.party.server.entity.Party;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
-import com.echothree.util.server.persistence.Session;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
+import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetPartyResponsibilitiesCommand
-        extends BaseSimpleCommand<GetPartyResponsibilitiesForm> {
+        extends BasePaginatedMultipleEntitiesCommand<PartyResponsibility, GetPartyResponsibilitiesForm> {
     
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
     
     static {
         FORM_FIELD_DEFINITIONS = List.of(
-            new FieldDefinition("PartyName", FieldType.ENTITY_NAME, false, null, null),
-            new FieldDefinition("ResponsibilityTypeName", FieldType.ENTITY_NAME, false, null, null)
+                new FieldDefinition("PartyName", FieldType.ENTITY_NAME, false, null, null),
+                new FieldDefinition("ResponsibilityTypeName", FieldType.ENTITY_NAME, false, null, null)
         );
     }
     
+    @Inject
+    EmployeeControl employeeControl;
+
+    @Inject
+    PartyControl partyControl;
+
+    @Inject
+    PartyLogic partyLogic;
+
     /** Creates a new instance of GetPartyResponsibilitiesCommand */
     public GetPartyResponsibilitiesCommand() {
         super(null, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
+    private Party party;
+    private ResponsibilityType responsibilityType;
+
     @Override
-    protected BaseResult execute() {
-        var result = EmployeeResultFactory.getGetPartyResponsibilitiesResult();
+    protected void handleForm() {
         var partyName = form.getPartyName();
         var responsibilityTypeName = form.getResponsibilityTypeName();
         var parameterCount = (partyName == null ? 0 : 1) + (responsibilityTypeName == null ? 0 : 1);
-        
+
         if(parameterCount == 1) {
-            var employeeControl = Session.getModelController(EmployeeControl.class);
-            
             if(partyName != null) {
-                var partyControl = Session.getModelController(PartyControl.class);
-                var party = partyControl.getPartyByName(partyName);
-                
-                if(party != null) {
-                    result.setParty(partyControl.getPartyTransfer(getUserVisit(), party));
-                    result.setPartyResponsibilities(employeeControl.getPartyResponsibilityTransfersByParty(getUserVisit(), party));
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownPartyName.name(), partyName);
-                }
-            } else if(responsibilityTypeName != null) {
-                var responsibilityType = employeeControl.getResponsibilityTypeByName(responsibilityTypeName);
-                
-                if(responsibilityType != null) {
-                    result.setResponsibilityType(employeeControl.getResponsibilityTypeTransfer(getUserVisit(), responsibilityType));
-                    result.setPartyResponsibilities(employeeControl.getPartyResponsibilityTransfersByResponsibilityType(getUserVisit(), responsibilityType));
-                } else {
+                party = partyLogic.getPartyByName(this, partyName);
+            } else {
+                responsibilityType = employeeControl.getResponsibilityTypeByName(responsibilityTypeName);
+
+                if(responsibilityType == null) {
                     addExecutionError(ExecutionErrors.UnknownResponsibilityTypeName.name(), responsibilityTypeName);
                 }
             }
         } else {
             addExecutionError(ExecutionErrors.InvalidParameterCount.name());
         }
-        
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        Long total = null;
+
+        if(!hasExecutionErrors()) {
+            if(party != null) {
+                total = employeeControl.countPartyResponsibilitiesByParty(party);
+            } else {
+                total = employeeControl.countPartyResponsibilitiesByResponsibilityType(responsibilityType);
+            }
+        }
+
+        return total;
+    }
+
+    @Override
+    protected Collection<PartyResponsibility> getEntities() {
+        Collection<PartyResponsibility> entities = null;
+
+        if(!hasExecutionErrors()) {
+            if(party != null) {
+                entities = employeeControl.getPartyResponsibilitiesByParty(party);
+            } else {
+                entities = employeeControl.getPartyResponsibilitiesByResponsibilityType(responsibilityType);
+            }
+        }
+
+        return entities;
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<PartyResponsibility> entities) {
+        var result = EmployeeResultFactory.getGetPartyResponsibilitiesResult();
+
+        if(entities != null) {
+            if(party != null) {
+                result.setParty(partyControl.getPartyTransfer(getUserVisit(), party));
+            } else {
+                result.setResponsibilityType(employeeControl.getResponsibilityTypeTransfer(getUserVisit(), responsibilityType));
+            }
+
+            if(session.hasLimit(PartyResponsibilityFactory.class)) {
+                result.setPartyResponsibilityCount(getTotalEntities());
+            }
+
+            result.setPartyResponsibilities(employeeControl.getPartyResponsibilityTransfers(getUserVisit(), entities));
+        }
+
         return result;
     }
     
