0e1929c1df4b7f0a135d72cacdf02a95808f8353
Richard Harms <rich@echothree.com>
2026-05-23 22:59:31 -0500

    #2031: Converted GetCampaignMedium UC to extend BaseSingleEntityCommand

--- src/java/com/echothree/control/user/campaign/common/spec/CampaignMediumUniversalSpec.java
+++ src/java/com/echothree/control/user/campaign/common/spec/CampaignMediumUniversalSpec.java
@@ -14,13 +14,13 @@
 // limitations under the License.
 // --------------------------------------------------------------------------------
 
-package com.echothree.control.user.campaign.common.form;
+package com.echothree.control.user.campaign.common.spec;
 
-import com.echothree.control.user.campaign.common.spec.CampaignMediumUniversalSpec;
+import com.echothree.control.user.core.common.spec.UniversalEntitySpec;
 
-public interface GetCampaignMediumForm
-        extends CampaignMediumUniversalSpec {
-    
-    // Nothing additional beyond CampaignMediumUniversalSpec
-    
+public interface CampaignMediumUniversalSpec
+        extends CampaignMediumSpec, UniversalEntitySpec {
+
+    // Nothing additional beyond CampaignMediumSpec, UniversalEntitySpec
+
 }
