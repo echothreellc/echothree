c51cbb661cbb35066ee9e8b47220ee468ab23040
Richard Harms <rich@echothree.com>
2026-05-28 22:10:52 -0500

    #2040: Revised additional Campaign Content UCs to accept IDs where appropriate

--- src/java/com/echothree/control/user/campaign/server/command/SetDefaultCampaignContentCommand.java
+++ src/java/com/echothree/control/user/campaign/server/command/SetDefaultCampaignContentCommand.java
@@ -18,21 +18,20 @@ package com.echothree.control.user.campaign.server.command;
 
 import com.echothree.control.user.campaign.common.form.SetDefaultCampaignContentForm;
 import com.echothree.model.control.campaign.server.control.CampaignControl;
+import com.echothree.model.control.campaign.server.logic.CampaignContentLogic;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.message.ExecutionErrors;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.server.control.BaseSimpleCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class SetDefaultCampaignContentCommand
@@ -45,15 +44,23 @@ public class SetDefaultCampaignContentCommand
         COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
                 new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
-                    new SecurityRoleDefinition(SecurityRoleGroups.CampaignContent.name(), SecurityRoles.Edit.name())
-                    ))
-                ));
-        
+                        new SecurityRoleDefinition(SecurityRoleGroups.CampaignContent.name(), SecurityRoles.Edit.name())
+                ))
+        ));
+
         FORM_FIELD_DEFINITIONS = List.of(
-                new FieldDefinition("CampaignContentName", FieldType.ENTITY_NAME, true, null, null)
-                );
+                new FieldDefinition("CampaignContentName", FieldType.ENTITY_NAME, false, null, null),
+                new FieldDefinition("EntityRef", FieldType.ENTITY_REF, false, null, null),
+                new FieldDefinition("Uuid", FieldType.UUID, false, null, null)
+        );
     }
-    
+
+    @Inject
+    CampaignControl campaignControl;
+
+    @Inject
+    CampaignContentLogic campaignContentLogic;
+
     /** Creates a new instance of SetDefaultCampaignContentCommand */
     public SetDefaultCampaignContentCommand() {
         super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, false);
@@ -61,17 +68,15 @@ public class SetDefaultCampaignContentCommand
     
     @Override
     protected BaseResult execute() {
-        var campaignControl = Session.getModelController(CampaignControl.class);
-        var campaignContentName = form.getCampaignContentName();
-        var campaignContentDetailValue = campaignControl.getCampaignContentDetailValueByNameForUpdate(campaignContentName);
-        
-        if(campaignContentDetailValue != null) {
+        var campaignContent = campaignContentLogic.getCampaignContentByUniversalSpecForUpdate(this, form);
+
+        if(!hasExecutionErrors()) {
+            var campaignContentDetailValue = campaignControl.getCampaignContentDetailValueForUpdate(campaignContent);
+
             campaignContentDetailValue.setIsDefault(true);
             campaignControl.updateCampaignContentFromValue(campaignContentDetailValue, getPartyPK());
-        } else {
-            addExecutionError(ExecutionErrors.UnknownCampaignContentName.name(), campaignContentName);
         }
-        
+
         return null;
     }
     
