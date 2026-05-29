5cbae381ee553af32ac0128eac5933f4b69deb65
Richard Harms <rich@echothree.com>
2026-05-28 22:29:24 -0500

    #2041: Added Campaign mutations

--- src/java/com/echothree/control/user/graphql/server/schema/GraphQlMutations.java
+++ src/java/com/echothree/control/user/graphql/server/schema/GraphQlMutations.java
@@ -26,7 +26,9 @@ import com.echothree.control.user.accounting.common.result.EditTransactionTimeTy
 import com.echothree.control.user.authentication.common.AuthenticationUtil;
 import com.echothree.control.user.campaign.common.CampaignUtil;
 import com.echothree.control.user.campaign.common.result.CreateCampaignContentResult;
+import com.echothree.control.user.campaign.common.result.CreateCampaignResult;
 import com.echothree.control.user.campaign.common.result.EditCampaignContentResult;
+import com.echothree.control.user.campaign.common.result.EditCampaignResult;
 import com.echothree.control.user.content.common.ContentUtil;
 import com.echothree.control.user.content.common.result.CreateContentCatalogResult;
 import com.echothree.control.user.content.common.result.CreateContentCategoryResult;
@@ -258,6 +260,7 @@ import com.echothree.control.user.workflow.common.result.EditWorkflowEntranceRes
 import com.echothree.control.user.workflow.common.result.EditWorkflowResult;
 import com.echothree.control.user.workflow.common.result.EditWorkflowStepResult;
 import com.echothree.model.control.campaign.server.graphql.CreateCampaignContentResultObject;
+import com.echothree.model.control.campaign.server.graphql.CreateCampaignResultObject;
 import com.echothree.model.control.graphql.server.graphql.MutationResultObject;
 import com.echothree.model.control.graphql.server.graphql.MutationResultWithIdObject;
 import com.echothree.model.control.graphql.server.util.BaseGraphQl;
@@ -15949,6 +15952,137 @@ public interface GraphQlMutations {
         return mutationResultObject;
     }
 
+    @GraphQLField
+    @GraphQLRelayMutation
+    static CreateCampaignResultObject createCampaign(final DataFetchingEnvironment env,
+            @GraphQLName("value") @GraphQLNonNull final String value,
+            @GraphQLName("isDefault") @GraphQLNonNull final String isDefault,
+            @GraphQLName("sortOrder") @GraphQLNonNull final String sortOrder,
+            @GraphQLName("description") final String description) {
+        var mutationResultObject = new CreateCampaignResultObject();
+
+        try {
+            var commandForm = CampaignUtil.getHome().getCreateCampaignForm();
+
+            commandForm.setValue(value);
+            commandForm.setIsDefault(isDefault);
+            commandForm.setSortOrder(sortOrder);
+            commandForm.setDescription(description);
+
+            var commandResult = CampaignUtil.getHome().createCampaign(BaseGraphQl.getUserVisitPK(env), commandForm);
+            mutationResultObject.setCommandResult(commandResult);
+
+            if(!commandResult.hasErrors()) {
+                mutationResultObject.setCreateCampaignResult((CreateCampaignResult)commandResult.getExecutionResult().getResult());
+            }
+        } catch (NamingException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return mutationResultObject;
+    }
+
+    @GraphQLField
+    @GraphQLRelayMutation
+    static MutationResultObject deleteCampaign(final DataFetchingEnvironment env,
+            @GraphQLName("campaignName") final String campaignName,
+            @GraphQLName("id") @GraphQLID final String id) {
+        var mutationResultObject = new MutationResultObject();
+
+        try {
+            var commandForm = CampaignUtil.getHome().getDeleteCampaignForm();
+
+            commandForm.setCampaignName(campaignName);
+            commandForm.setUuid(id);
+
+            var commandResult = CampaignUtil.getHome().deleteCampaign(BaseGraphQl.getUserVisitPK(env), commandForm);
+            mutationResultObject.setCommandResult(commandResult);
+        } catch (NamingException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return mutationResultObject;
+    }
+
+    @GraphQLField
+    @GraphQLRelayMutation
+    static MutationResultWithIdObject editCampaign(final DataFetchingEnvironment env,
+            @GraphQLName("campaignName") final String campaignName,
+            @GraphQLName("id") @GraphQLID final String id,
+            @GraphQLName("value") final String value,
+            @GraphQLName("isDefault") final String isDefault,
+            @GraphQLName("sortOrder") final String sortOrder,
+            @GraphQLName("description") final String description) {
+        var mutationResultObject = new MutationResultWithIdObject();
+
+        try {
+            var spec = CampaignUtil.getHome().getCampaignUniversalSpec();
+
+            spec.setCampaignName(campaignName);
+            spec.setUuid(id);
+
+            var commandForm = CampaignUtil.getHome().getEditCampaignForm();
+
+            commandForm.setSpec(spec);
+            commandForm.setEditMode(EditMode.LOCK);
+
+            var commandResult = CampaignUtil.getHome().editCampaign(BaseGraphQl.getUserVisitPK(env), commandForm);
+
+            if(!commandResult.hasErrors()) {
+                var executionResult = commandResult.getExecutionResult();
+                var result = (EditCampaignResult)executionResult.getResult();
+                Map<String, Object> arguments = env.getArgument("input");
+                var edit = result.getEdit();
+
+                mutationResultObject.setEntityInstance(result.getCampaign().getEntityInstance());
+
+                if(arguments.containsKey("value"))
+                    edit.setValue(value);
+                if(arguments.containsKey("isDefault"))
+                    edit.setIsDefault(isDefault);
+                if(arguments.containsKey("sortOrder"))
+                    edit.setSortOrder(sortOrder);
+                if(arguments.containsKey("description"))
+                    edit.setDescription(description);
+
+                commandForm.setEdit(edit);
+                commandForm.setEditMode(EditMode.UPDATE);
+
+                commandResult = CampaignUtil.getHome().editCampaign(BaseGraphQl.getUserVisitPK(env), commandForm);
+            }
+
+            mutationResultObject.setCommandResult(commandResult);
+        } catch (NamingException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return mutationResultObject;
+    }
+
+    @GraphQLField
+    @GraphQLRelayMutation
+    @GraphQLName("setCampaignStatus")
+    static MutationResultObject setCampaignStatus(final DataFetchingEnvironment env,
+            @GraphQLName("campaignName") final String campaignName,
+            @GraphQLName("id") @GraphQLID final String id,
+            @GraphQLName("campaignStatusChoice") @GraphQLNonNull final String campaignStatusChoice) {
+        var mutationResultObject = new MutationResultObject();
+
+        try {
+            var commandForm = CampaignUtil.getHome().getSetCampaignStatusForm();
+
+            commandForm.setCampaignName(campaignName);
+            commandForm.setUuid(id);
+            commandForm.setCampaignStatusChoice(campaignStatusChoice);
+
+            mutationResultObject.setCommandResult(CampaignUtil.getHome().setCampaignStatus(BaseGraphQl.getUserVisitPK(env), commandForm));
+        } catch (NamingException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return mutationResultObject;
+    }
+    
     @GraphQLField
     @GraphQLRelayMutation
     static CreateCampaignContentResultObject createCampaignContent(final DataFetchingEnvironment env,
---
+++ src/java/com/echothree/model/control/campaign/server/graphql/CreateCampaignResultObject.java
@@ -0,0 +1,44 @@
+// --------------------------------------------------------------------------------
+// Copyright 2002-2026 Echo Three, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// --------------------------------------------------------------------------------
+
+package com.echothree.model.control.campaign.server.graphql;
+
+import com.echothree.control.user.campaign.common.result.CreateCampaignResult;
+import com.echothree.model.control.graphql.server.graphql.MutationResultWithIdObject;
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+
+@GraphQLDescription("create campaign content result object")
+@GraphQLName("CreateCampaignResult")
+public class CreateCampaignResultObject
+        extends MutationResultWithIdObject {
+
+    String campaignName;
+
+    public void setCreateCampaignResult(final CreateCampaignResult result) {
+        setEntityInstanceFromEntityRef(result.getEntityRef());
+
+        this.campaignName = result.getCampaignName();
+    }
+
+    @GraphQLField
+    @GraphQLDescription("campaign name")
+    public String getCampaignName() {
+        return campaignName;
+    }
+
+}
