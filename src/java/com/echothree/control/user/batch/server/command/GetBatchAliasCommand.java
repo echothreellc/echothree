0ba29ed7ec6373850238ac2c0dfea794848ff213
Richard Harms <rich@echothree.com>
2026-06-07 13:52:38 -0500

    #1770: Converted GetBatchAlias UC to extend BaseSingleEntityCommand

--- src/java/com/echothree/control/user/batch/server/command/GetBatchAliasCommand.java
+++ src/java/com/echothree/control/user/batch/server/command/GetBatchAliasCommand.java
@@ -20,34 +20,42 @@ import com.echothree.control.user.batch.common.form.GetBatchAliasForm;
 import com.echothree.control.user.batch.common.result.BatchResultFactory;
 import com.echothree.control.user.batch.server.command.util.BatchAliasUtil;
 import com.echothree.model.control.batch.server.control.BatchControl;
+import com.echothree.model.control.batch.server.logic.BatchLogic;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoles;
+import com.echothree.model.data.batch.server.entity.BatchAlias;
 import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetBatchAliasCommand
-        extends BaseSimpleCommand<GetBatchAliasForm> {
-    
+        extends BaseSingleEntityCommand<BatchAlias, GetBatchAliasForm> {
+
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
-    
+
     static {
         FORM_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("BatchTypeName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("BatchName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("BatchAliasTypeName", FieldType.ENTITY_NAME, true, null, null)
-                );
+        );
     }
-    
+
+    @Inject
+    BatchControl batchControl;
+
+    @Inject
+    BatchLogic batchLogic;
+
     /** Creates a new instance of GetBatchAliasCommand */
     public GetBatchAliasCommand() {
         super(null, FORM_FIELD_DEFINITIONS, false);
@@ -64,39 +72,42 @@ public class GetBatchAliasCommand
     }
 
     @Override
-    protected BaseResult execute() {
-        var batchControl = Session.getModelController(BatchControl.class);
-        var result = BatchResultFactory.getGetBatchAliasResult();
+    protected BatchAlias getEntity() {
         var batchTypeName = form.getBatchTypeName();
-        var batchType = batchControl.getBatchTypeByName(batchTypeName);
+        var batchType = batchLogic.getBatchTypeByName(this, batchTypeName);
+        BatchAlias batchAlias = null;
 
-        if(batchType != null) {
+        if(!hasExecutionErrors()) {
             var batchName = form.getBatchName();
-            var batch = batchControl.getBatchByName(batchType, batchName);
+            var batch = batchLogic.getBatchByName(this, batchType, batchName);
 
-            if(batch != null) {
+            if(!hasExecutionErrors()) {
                 var batchAliasTypeName = form.getBatchAliasTypeName();
-                var batchAliasType = batchControl.getBatchAliasTypeByName(batchType, batchAliasTypeName);
+                var batchAliasType = batchLogic.getBatchAliasTypeByName(this, batchType, batchAliasTypeName);
 
-                if(batchAliasType != null) {
-                    var batchAlias = batchControl.getBatchAlias(batch, batchAliasType);
+                if(!hasExecutionErrors()) {
+                    batchAlias = batchControl.getBatchAlias(batch, batchAliasType);
 
-                    if(batchAlias != null) {
-                        result.setBatchAlias(batchControl.getBatchAliasTransfer(getUserVisit(), batchAlias));
-                    } else {
-                        addExecutionError(ExecutionErrors.UnknownBatchAlias.name(), batchName, batchAliasTypeName);
+                    if(batchAlias == null) {
+                        addExecutionError(ExecutionErrors.UnknownBatchAlias.name(),
+                                batch.getLastDetail().getBatchName(), batchAliasType.getLastDetail().getBatchAliasTypeName());
                     }
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownBatchAliasTypeName.name(), batchTypeName, batchAliasTypeName);
                 }
-            } else {
-                addExecutionError(ExecutionErrors.UnknownBatchName.name(), batchTypeName, batchName);
             }
-        } else {
-            addExecutionError(ExecutionErrors.UnknownBatchTypeName.name(), batchTypeName);
+        }
+
+        return batchAlias;
+    }
+
+    @Override
+    protected BaseResult getResult(BatchAlias batchAlias) {
+        var result = BatchResultFactory.getGetBatchAliasResult();
+
+        if(batchAlias != null) {
+            result.setBatchAlias(batchControl.getBatchAliasTransfer(getUserVisit(), batchAlias));
         }
 
         return result;
     }
-    
+
 }
