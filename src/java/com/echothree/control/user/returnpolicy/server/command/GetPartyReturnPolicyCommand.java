8660153fa5f967d5955fb8b91527b41605746fcc
Richard Harms <rich@echothree.com>
2026-06-08 20:12:17 -0500

    #1924: Converted GetPartyReturnPolicies UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/returnpolicy/server/command/GetPartyReturnPolicyCommand.java
+++ src/java/com/echothree/control/user/returnpolicy/server/command/GetPartyReturnPolicyCommand.java
@@ -19,22 +19,23 @@ package com.echothree.control.user.returnpolicy.server.command;
 import com.echothree.control.user.returnpolicy.common.form.GetPartyReturnPolicyForm;
 import com.echothree.control.user.returnpolicy.common.result.ReturnPolicyResultFactory;
 import com.echothree.model.control.party.common.PartyTypes;
-import com.echothree.model.control.party.server.control.PartyControl;
+import com.echothree.model.control.party.server.logic.PartyLogic;
 import com.echothree.model.control.returnpolicy.server.control.ReturnPolicyControl;
+import com.echothree.model.control.returnpolicy.server.logic.ReturnKindLogic;
+import com.echothree.model.control.returnpolicy.server.logic.ReturnPolicyLogic;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.server.control.BaseSimpleCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetPartyReturnPolicyCommand
@@ -47,16 +48,28 @@ public class GetPartyReturnPolicyCommand
         COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
                 new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
-                    new SecurityRoleDefinition(SecurityRoleGroups.PartyReturnPolicy.name(), SecurityRoles.Review.name())
-                    ))
-                ));
+                        new SecurityRoleDefinition(SecurityRoleGroups.PartyReturnPolicy.name(), SecurityRoles.Review.name())
+                ))
+        ));
 
         FORM_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("PartyName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("ReturnKindName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("ReturnPolicyName", FieldType.ENTITY_NAME, true, null, null)
-                );
+        );
     }
+
+    @Inject
+    ReturnPolicyControl returnPolicyControl;
+
+    @Inject
+    PartyLogic partyLogic;
+
+    @Inject
+    ReturnKindLogic returnKindLogic;
+
+    @Inject
+    ReturnPolicyLogic returnPolicyLogic;
 
     /** Creates a new instance of GetPartyReturnPolicyCommand */
     public GetPartyReturnPolicyCommand() {
@@ -65,21 +78,19 @@ public class GetPartyReturnPolicyCommand
 
     @Override
     protected BaseResult execute() {
-        var partyControl = Session.getModelController(PartyControl.class);
         var result = ReturnPolicyResultFactory.getGetPartyReturnPolicyResult();
         var partyName = form.getPartyName();
-        var party = partyControl.getPartyByName(partyName);
+        var party = partyLogic.getPartyByName(this, partyName);
 
-        if(party != null) {
-            var returnPolicyControl = Session.getModelController(ReturnPolicyControl.class);
+        if(!hasExecutionErrors()) {
             var returnKindName = form.getReturnKindName();
-            var returnKind = returnPolicyControl.getReturnKindByName(returnKindName);
+            var returnKind = returnKindLogic.getReturnKindByName(this, returnKindName);
 
-            if(returnKind != null) {
+            if(!hasExecutionErrors()) {
                 var returnPolicyName = form.getReturnPolicyName();
-                var returnPolicy = returnPolicyControl.getReturnPolicyByName(returnKind, returnPolicyName);
+                var returnPolicy = returnPolicyLogic.getReturnPolicyByName(this, returnKind, returnPolicyName);
 
-                if(returnPolicy != null) {
+                if(!hasExecutionErrors()) {
                     var partyReturnPolicy = returnPolicyControl.getPartyReturnPolicy(party, returnPolicy);
 
                     if(partyReturnPolicy != null) {
@@ -87,14 +98,8 @@ public class GetPartyReturnPolicyCommand
                     } else {
                         addExecutionError(ExecutionErrors.UnknownPartyReturnPolicy.name(), partyName, returnKindName, returnPolicyName);
                     }
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownReturnPolicyName.name(), returnKindName, returnPolicyName);
                 }
-            } else {
-                addExecutionError(ExecutionErrors.UnknownReturnKindName.name(), returnKindName);
             }
-        } else {
-            addExecutionError(ExecutionErrors.UnknownPartyName.name(), partyName);
         }
 
         return result;
