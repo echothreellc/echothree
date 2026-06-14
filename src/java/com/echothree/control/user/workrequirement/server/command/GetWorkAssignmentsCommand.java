17832a05596c75121b055983895f87c697430f58
Richard Harms <rich@echothree.com>
2026-06-13 19:10:06 -0500

    #2014: Converted GetWorkAssignments UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/workrequirement/server/command/GetWorkAssignmentsCommand.java
+++ src/java/com/echothree/control/user/workrequirement/server/command/GetWorkAssignmentsCommand.java
@@ -18,24 +18,26 @@ package com.echothree.control.user.workrequirement.server.command;
 
 import com.echothree.control.user.workrequirement.common.form.GetWorkAssignmentsForm;
 import com.echothree.control.user.workrequirement.common.result.WorkRequirementResultFactory;
-import com.echothree.model.control.employee.server.control.EmployeeControl;
 import com.echothree.model.control.party.server.control.PartyControl;
+import com.echothree.model.control.party.server.logic.EmployeeLogic;
+import com.echothree.model.control.party.server.logic.PartyLogic;
 import com.echothree.model.control.workrequirement.server.control.WorkRequirementControl;
 import com.echothree.model.data.party.server.entity.Party;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.workrequirement.server.entity.WorkAssignment;
 import com.echothree.model.data.workrequirement.server.factory.WorkAssignmentFactory;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
-import com.echothree.util.server.persistence.Session;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
+import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetWorkAssignmentsCommand
-        extends BaseSimpleCommand<GetWorkAssignmentsForm> {
+        extends BasePaginatedMultipleEntitiesCommand<WorkAssignment, GetWorkAssignmentsForm> {
     
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
     
@@ -43,59 +45,77 @@ public class GetWorkAssignmentsCommand
         FORM_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("EmployeeName", FieldType.ENTITY_NAME, false, null, null),
                 new FieldDefinition("PartyName", FieldType.ENTITY_NAME, false, null, null)
-                );
+        );
     }
-    
+
+    @Inject
+    PartyControl partyControl;
+
+    @Inject
+    WorkRequirementControl workRequirementControl;
+
+    @Inject
+    EmployeeLogic employeeLogic;
+
+    @Inject
+    PartyLogic partyLogic;
+
     /** Creates a new instance of GetWorkAssignmentsCommand */
     public GetWorkAssignmentsCommand() {
         super(null, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
+    private Party party;
+
     @Override
-    protected BaseResult execute() {
-        var partyControl = Session.getModelController(PartyControl.class);
-        var result = WorkRequirementResultFactory.getGetWorkAssignmentsResult();
+    protected void handleForm() {
         var employeeName = form.getEmployeeName();
         var partyName = form.getPartyName();
         var parameterCount = (employeeName == null ? 0 : 1) + (partyName == null ? 0 : 1);
 
         if(parameterCount < 2) {
-            Party party = null;
-
             if(employeeName != null) {
-                var employeeControl = Session.getModelController(EmployeeControl.class);
-                var partyEmployee = employeeControl.getPartyEmployeeByName(employeeName);
+                var partyEmployee = employeeLogic.getPartyEmployeeByName(this, employeeName, null);
 
-                if(partyEmployee != null) {
+                if(!hasExecutionErrors()) {
                     party = partyEmployee.getParty();
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownEmployeeName.name(), employeeName);
                 }
             } else if(partyName != null) {
-                party = partyControl.getPartyByName(partyName);
-
-                if(party == null) {
-                    addExecutionError(ExecutionErrors.UnknownPartyName.name(), partyName);
-                }
+                party = partyLogic.getPartyByName(this, partyName);
             } else {
                 party = getParty();
-            }
-
-            if(!hasExecutionErrors()) {
-                var workRequirementControl = Session.getModelController(WorkRequirementControl.class);
-                var userVisit = getUserVisit();
-
-                if(session.hasLimit(WorkAssignmentFactory.class)) {
-                    result.setWorkAssignmentCount(workRequirementControl.countWorkAssignmentsByParty(party));
-                }
-
-                result.setParty(partyControl.getPartyTransfer(userVisit, party));
-                result.setWorkAssignments(workRequirementControl.getWorkAssignmentTransfersByParty(userVisit, party));
             }
         } else {
             addExecutionError(ExecutionErrors.InvalidParameterCount.name());
         }
-        
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        return hasExecutionErrors() ? null : workRequirementControl.countWorkAssignmentsByParty(party);
+    }
+
+    @Override
+    protected Collection<WorkAssignment> getEntities() {
+        return hasExecutionErrors() ? null : workRequirementControl.getWorkAssignmentsByParty(party);
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<WorkAssignment> entities) {
+        var result = WorkRequirementResultFactory.getGetWorkAssignmentsResult();
+
+        if(entities != null) {
+            var userVisit = getUserVisit();
+
+            result.setParty(partyControl.getPartyTransfer(userVisit, party));
+
+            if(session.hasLimit(WorkAssignmentFactory.class)) {
+                result.setWorkAssignmentCount(getTotalEntities());
+            }
+
+            result.setWorkAssignments(workRequirementControl.getWorkAssignmentTransfers(userVisit, entities));
+        }
+
         return result;
     }
     
