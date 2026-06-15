07e2d45c92424e61b9d1127db7b880627dfab276
Richard Harms <rich@echothree.com>
2026-06-14 19:31:36 -0500

    #2098: Converted GetPaymentMethod UC to extend BaseSingleEntityCommand

--- src/java/com/echothree/control/user/payment/server/command/GetPaymentMethodCommand.java
+++ src/java/com/echothree/control/user/payment/server/command/GetPaymentMethodCommand.java
@@ -21,23 +21,24 @@ import com.echothree.control.user.payment.common.result.PaymentResultFactory;
 import com.echothree.model.control.core.common.EventTypes;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.payment.server.control.PaymentMethodControl;
+import com.echothree.model.control.payment.server.logic.PaymentMethodLogic;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
+import com.echothree.model.data.payment.server.entity.PaymentMethod;
 import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetPaymentMethodCommand
-        extends BaseSimpleCommand<GetPaymentMethodForm> {
+        extends BaseSingleEntityCommand<PaymentMethod, GetPaymentMethodForm> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -54,27 +55,38 @@ public class GetPaymentMethodCommand
                 new FieldDefinition("PaymentMethodName", FieldType.ENTITY_NAME, true, null, null)
         );
     }
-    
+
+    @Inject
+    PaymentMethodControl paymentMethodControl;
+
+    @Inject
+    PaymentMethodLogic paymentMethodLogic;
+
     /** Creates a new instance of GetPaymentMethodCommand */
     public GetPaymentMethodCommand() {
         super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
     
     @Override
-    protected BaseResult execute() {
-        var paymentMethodControl = Session.getModelController(PaymentMethodControl.class);
-        var result = PaymentResultFactory.getGetPaymentMethodResult();
+    protected PaymentMethod getEntity() {
         var paymentMethodName = form.getPaymentMethodName();
-        var paymentMethod = paymentMethodControl.getPaymentMethodByName(paymentMethodName);
-        
+        var paymentMethod = paymentMethodLogic.getPaymentMethodByName(this, paymentMethodName);
+
+        if(paymentMethod != null) {
+            sendEvent(paymentMethod.getPrimaryKey(), EventTypes.READ, null, null, getPartyPK());
+        }
+
+        return paymentMethod;
+    }
+
+    @Override
+    protected BaseResult getResult(PaymentMethod paymentMethod) {
+        var result = PaymentResultFactory.getGetPaymentMethodResult();
+
         if(paymentMethod != null) {
             result.setPaymentMethod(paymentMethodControl.getPaymentMethodTransfer(getUserVisit(), paymentMethod));
-            
-            sendEvent(paymentMethod.getPrimaryKey(), EventTypes.READ, null, null, getPartyPK());
-        } else {
-            addExecutionError(ExecutionErrors.UnknownPaymentMethodName.name(), paymentMethodName);
         }
-        
+
         return result;
     }
     
