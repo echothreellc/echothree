2305d2ca989d1629bc2ea690afc90d2ac2649d56
Richard Harms <rich@echothree.com>
2026-06-28 18:20:37 -0500

    #2011: Converted GetWishlistLines UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/wishlist/server/command/GetWishlistLinesCommand.java
+++ src/java/com/echothree/control/user/wishlist/server/command/GetWishlistLinesCommand.java
@@ -19,74 +19,138 @@ package com.echothree.control.user.wishlist.server.command;
 import com.echothree.control.user.wishlist.common.form.GetWishlistLinesForm;
 import com.echothree.control.user.wishlist.common.result.WishlistResultFactory;
 import com.echothree.model.control.item.server.control.ItemControl;
+import com.echothree.model.control.item.server.logic.ItemLogic;
 import com.echothree.model.control.order.common.OrderTypes;
 import com.echothree.model.control.order.server.control.OrderControl;
 import com.echothree.model.control.order.server.control.OrderTypeControl;
 import com.echothree.model.control.wishlist.server.control.WishlistControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.item.server.entity.Item;
+import com.echothree.model.data.order.server.entity.Order;
+import com.echothree.model.data.order.server.entity.OrderLine;
+import com.echothree.model.data.order.server.factory.OrderLineFactory;
 import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.server.control.BaseSimpleCommand;
-import com.echothree.util.server.persistence.Session;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
+import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetWishlistLinesCommand
-        extends BaseSimpleCommand<GetWishlistLinesForm> {
+        extends BasePaginatedMultipleEntitiesCommand<OrderLine, GetWishlistLinesForm> {
     
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
     
     static {
         FORM_FIELD_DEFINITIONS = List.of(
-            new FieldDefinition("WishlistName", FieldType.ENTITY_NAME, false, null, null),
-            new FieldDefinition("ItemName", FieldType.ENTITY_NAME, false, null, null)
+                new FieldDefinition("WishlistName", FieldType.ENTITY_NAME, false, null, null),
+                new FieldDefinition("ItemName", FieldType.ENTITY_NAME, false, null, null)
         );
     }
-    
+
+    @Inject
+    ItemControl itemControl;
+
+    @Inject
+    OrderControl orderControl;
+
+    @Inject
+    OrderTypeControl orderTypeControl;
+
+    @Inject
+    WishlistControl wishlistControl;
+
+    @Inject
+    ItemLogic itemLogic;
+
     /** Creates a new instance of GetWishlistLinesCommand */
     public GetWishlistLinesCommand() {
         super(null, FORM_FIELD_DEFINITIONS, true);
     }
     
+    private Order order;
+    private Item item;
+
     @Override
-    protected BaseResult execute() {
-        var result = WishlistResultFactory.getGetWishlistLinesResult();
+    protected void handleForm() {
         var wishlistName = form.getWishlistName();
         var itemName = form.getItemName();
         var parameterCount = (wishlistName == null ? 0 : 1) + (itemName == null ? 0 : 1);
-        
+
         if(parameterCount == 1) {
-            var wishlistControl = Session.getModelController(WishlistControl.class);
-            
             if(wishlistName != null) {
-                var orderControl = Session.getModelController(OrderControl.class);
-                var orderTypeControl = Session.getModelController(OrderTypeControl.class);
-                var order = orderControl.getOrderByName(orderTypeControl.getOrderTypeByName(OrderTypes.WISHLIST.name()), wishlistName);
+                var orderType = orderTypeControl.getOrderTypeByName(OrderTypes.WISHLIST.name());
                 
-                if(order != null && order.getLastDetail().getOrderType().getLastDetail().getOrderTypeName().equals(OrderTypes.WISHLIST.name())) {
-                    result.setWishlistLines(wishlistControl.getWishlistLineTransfersByOrder(getUserVisit(), order));
+                if(orderType != null) {
+                    order = orderControl.getOrderByName(orderType, wishlistName);
+
+                    if(order == null || !order.getLastDetail().getOrderType().getLastDetail().getOrderTypeName().equals(OrderTypes.WISHLIST.name())) {
+                        addExecutionError(ExecutionErrors.UnknownWishlistName.name(), wishlistName);
+                    }
                 } else {
-                    addExecutionError(ExecutionErrors.UnknownWishlistName.name(), wishlistName);
+                    addExecutionError(ExecutionErrors.UnknownOrderTypeName.name(), OrderTypes.WISHLIST.name());
                 }
-            }
-            
-            if(itemName != null) {
-                var itemControl = Session.getModelController(ItemControl.class);
-                var item = itemControl.getItemByName(itemName);
-                
-                if(item != null) {
-                    result.setWishlistLines(wishlistControl.getWishlistLineTransfersByItem(getUserVisit(), item));
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownItemName.name(), itemName);
-                }
+            } else {
+                item = itemLogic.getItemByName(this, itemName);
             }
         } else {
             addExecutionError(ExecutionErrors.InvalidParameterCount.name());
         }
-        
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        Long total = null;
+
+        if(!hasExecutionErrors()) {
+            if(order != null) {
+                total = wishlistControl.countWishlistLinesByOrder(order);
+            } else {
+                total = wishlistControl.countWishlistLinesByItem(item);
+            }
+        }
+
+        return total;
+    }
+
+    @Override
+    protected Collection<OrderLine> getEntities() {
+        Collection<OrderLine> entities = null;
+
+        if(!hasExecutionErrors()) {
+            if(order != null) {
+                entities = wishlistControl.getWishlistLinesByOrder(order);
+            } else {
+                entities = wishlistControl.getWishlistLinesByItem(item);
+            }
+        }
+
+        return entities;
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<OrderLine> entities) {
+        var result = WishlistResultFactory.getGetWishlistLinesResult();
+
+        if(entities != null) {
+            var userVisit = getUserVisit();
+
+            if(order != null) {
+                result.setWishlist(wishlistControl.getWishlistTransfer(userVisit, order));
+            } else {
+                result.setItem(itemControl.getItemTransfer(userVisit, item));
+            }
+
+            if(session.hasLimit(OrderLineFactory.class)) {
+                result.setWishlistLineCount(getTotalEntities());
+            }
+
+            result.setWishlistLines(wishlistControl.getWishlistLineTransfers(userVisit, entities));
+        }
+
         return result;
     }
     
