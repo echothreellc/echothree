5f5a84de986b6c2607ff51e945abed938b138b51
Richard Harms <rich@echothree.com>
2026-06-29 22:23:21 -0500

    #2051: Added Chain Type object and queries

--- src/java/com/echothree/model/control/chain/server/graphql/ChainKindObject.java
+++ src/java/com/echothree/model/control/chain/server/graphql/ChainKindObject.java
@@ -18,8 +18,14 @@ package com.echothree.model.control.chain.server.graphql;
 
 import com.echothree.model.control.chain.server.control.ChainControl;
 import com.echothree.model.control.graphql.server.graphql.BaseEntityInstanceObject;
+import com.echothree.model.control.graphql.server.graphql.count.Connections;
+import com.echothree.model.control.graphql.server.graphql.count.CountedObjects;
+import com.echothree.model.control.graphql.server.graphql.count.CountingDataConnectionFetcher;
+import com.echothree.model.control.graphql.server.graphql.count.CountingPaginatedData;
 import com.echothree.model.control.graphql.server.util.BaseGraphQl;
+import com.echothree.model.control.graphql.server.util.count.ObjectLimiter;
 import com.echothree.model.control.user.server.control.UserControl;
+import com.echothree.model.data.chain.common.ChainTypeConstants;
 import com.echothree.model.data.chain.server.entity.ChainKind;
 import com.echothree.model.data.chain.server.entity.ChainKindDetail;
 import com.echothree.util.server.persistence.Session;
@@ -27,7 +33,10 @@ import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.annotations.connection.GraphQLConnection;
 import graphql.schema.DataFetchingEnvironment;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @GraphQLDescription("chain kind object")
 @GraphQLName("ChainKind")
@@ -83,24 +92,24 @@ public class ChainKindObject
         return chainControl.getBestChainKindDescription(chainKind, userControl.getPreferredLanguageFromUserVisit(BaseGraphQl.getUserVisit(env)));
     }
 
-//    @GraphQLField
-//    @GraphQLDescription("chain types")
-//    @GraphQLNonNull
-//    @GraphQLConnection(connectionFetcher = CountingDataConnectionFetcher.class)
-//    public CountingPaginatedData<ChainTypeObject> getChainTypes(final DataFetchingEnvironment env) {
-//        if(ChainSecurityUtils.getHasChainTypesAccess(env)) {
-//            var chainControl = Session.getModelController(ChainControl.class);
-//            var totalCount = chainControl.countChainTypesByChainKind(chainKind);
-//
-//            try(var objectLimiter = new ObjectLimiter(env, ChainTypeConstants.COMPONENT_VENDOR_NAME, ChainTypeConstants.ENTITY_TYPE_NAME, totalCount)) {
-//                var entities = chainControl.getChainTypes(chainKind);
-//                var chainTypes = entities.stream().map(ChainTypeObject::new).collect(Collectors.toCollection(() -> new ArrayList<>(entities.size())));
-//
-//                return new CountedObjects<>(objectLimiter, chainTypes);
-//            }
-//        } else {
-//            return Connections.emptyConnection();
-//        }
-//    }
+    @GraphQLField
+    @GraphQLDescription("chain types")
+    @GraphQLNonNull
+    @GraphQLConnection(connectionFetcher = CountingDataConnectionFetcher.class)
+    public CountingPaginatedData<ChainTypeObject> getChainTypes(final DataFetchingEnvironment env) {
+        if(ChainSecurityUtils.getHasChainTypesAccess(env)) {
+            var chainControl = Session.getModelController(ChainControl.class);
+            var totalCount = chainControl.countChainTypesByChainKind(chainKind);
+
+            try(var objectLimiter = new ObjectLimiter(env, ChainTypeConstants.COMPONENT_VENDOR_NAME, ChainTypeConstants.ENTITY_TYPE_NAME, totalCount)) {
+                var entities = chainControl.getChainTypesByChainKind(chainKind);
+                var chainTypes = entities.stream().map(ChainTypeObject::new).collect(Collectors.toCollection(() -> new ArrayList<>(entities.size())));
+
+                return new CountedObjects<>(objectLimiter, chainTypes);
+            }
+        } else {
+            return Connections.emptyConnection();
+        }
+    }
     
 }
