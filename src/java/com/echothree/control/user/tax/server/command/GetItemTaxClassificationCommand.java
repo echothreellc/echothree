5fa89e351697ef908f8a7ca2ac5244498799a6cc
Richard Harms <rich@echothree.com>
2026-07-25 12:47:14 -0500

    #1996: Converted GetItemTaxClassification UC to extend BaseSingleEntityCommand

--- src/java/com/echothree/control/user/tax/server/command/GetItemTaxClassificationCommand.java
+++ src/java/com/echothree/control/user/tax/server/command/GetItemTaxClassificationCommand.java
@@ -19,27 +19,27 @@ package com.echothree.control.user.tax.server.command;
 import com.echothree.control.user.tax.common.form.GetItemTaxClassificationForm;
 import com.echothree.control.user.tax.common.result.TaxResultFactory;
 import com.echothree.model.control.geo.server.control.GeoControl;
-import com.echothree.model.control.item.server.control.ItemControl;
+import com.echothree.model.control.item.server.logic.ItemLogic;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
 import com.echothree.model.control.tax.server.control.TaxControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.tax.server.entity.ItemTaxClassification;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
-import com.echothree.util.server.persistence.Session;
 import java.util.List;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class GetItemTaxClassificationCommand
-        extends BaseSimpleCommand<GetItemTaxClassificationForm> {
+        extends BaseSingleEntityCommand<ItemTaxClassification, GetItemTaxClassificationForm> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -48,15 +48,24 @@ public class GetItemTaxClassificationCommand
         COMMAND_SECURITY_DEFINITION = new CommandSecurityDefinition(List.of(
                 new PartyTypeDefinition(PartyTypes.UTILITY.name(), null),
                 new PartyTypeDefinition(PartyTypes.EMPLOYEE.name(), List.of(
-                    new SecurityRoleDefinition(SecurityRoleGroups.ItemTaxClassification.name(), SecurityRoles.Review.name())
-                    ))
-                ));
+                        new SecurityRoleDefinition(SecurityRoleGroups.ItemTaxClassification.name(), SecurityRoles.Review.name())
+                ))
+        ));
 
         FORM_FIELD_DEFINITIONS = List.of(
                 new FieldDefinition("ItemName", FieldType.ENTITY_NAME, true, null, null),
                 new FieldDefinition("CountryName", FieldType.ENTITY_NAME, true, null, null)
-                );
+        );
     }
+
+    @Inject
+    GeoControl geoControl;
+
+    @Inject
+    TaxControl taxControl;
+
+    @Inject
+    ItemLogic itemLogic;
     
     /** Creates a new instance of GetItemTaxClassificationCommand */
     public GetItemTaxClassificationCommand() {
@@ -64,33 +73,37 @@ public class GetItemTaxClassificationCommand
     }
     
     @Override
-    protected BaseResult execute() {
-        var itemControl = Session.getModelController(ItemControl.class);
-        var result = TaxResultFactory.getGetItemTaxClassificationResult();
+    protected ItemTaxClassification getEntity() {
         var itemName = form.getItemName();
-        var item = itemControl.getItemByName(itemName);
-        
+        var item = itemLogic.getItemByName(this, itemName);
+        ItemTaxClassification itemTaxClassification = null;
+
         if(item != null) {
-            var geoControl = Session.getModelController(GeoControl.class);
             var countryName = form.getCountryName();
             var countryGeoCode = geoControl.getCountryByAlias(countryName);
             
             if(countryGeoCode != null) {
-                var taxControl = Session.getModelController(TaxControl.class);
-                var itemTaxClassification = taxControl.getItemTaxClassification(item, countryGeoCode);
+                itemTaxClassification = taxControl.getItemTaxClassification(item, countryGeoCode);
 
-                if(itemTaxClassification != null) {
-                    result.setItemTaxClassification(taxControl.getItemTaxClassificationTransfer(getUserVisit(), itemTaxClassification));
-                } else {
+                if(itemTaxClassification == null) {
                     addExecutionError(ExecutionErrors.UnknownItemTaxClassification.name(), itemName, countryName);
                 }
             } else {
                 addExecutionError(ExecutionErrors.UnknownCountryName.name(), countryName);
             }
-        } else {
-            addExecutionError(ExecutionErrors.UnknownItemName.name(), itemName);
         }
-        
+
+        return itemTaxClassification;
+    }
+
+    @Override
+    protected BaseResult getResult(ItemTaxClassification itemTaxClassification) {
+        var result = TaxResultFactory.getGetItemTaxClassificationResult();
+
+        if(itemTaxClassification != null) {
+            result.setItemTaxClassification(taxControl.getItemTaxClassificationTransfer(getUserVisit(), itemTaxClassification));
+        }
+
         return result;
     }
     
