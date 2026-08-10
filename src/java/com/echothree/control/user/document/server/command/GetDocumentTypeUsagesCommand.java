40f31a4017a73c069ed6255f0bfd1e8405e9f929
Richard Harms <rich@echothree.com>
2026-08-10 18:29:58 -0500

    #1841: Converted GetDocumentTypeUsages UC to extend BasePaginatedMultipleEntitiesCommand

--- src/java/com/echothree/control/user/document/server/command/GetDocumentTypeUsagesCommand.java
+++ src/java/com/echothree/control/user/document/server/command/GetDocumentTypeUsagesCommand.java
@@ -19,25 +19,31 @@ package com.echothree.control.user.document.server.command;
 import com.echothree.control.user.document.common.form.GetDocumentTypeUsagesForm;
 import com.echothree.control.user.document.common.result.DocumentResultFactory;
 import com.echothree.model.control.document.server.control.DocumentControl;
+import com.echothree.model.control.document.server.logic.DocumentTypeLogic;
+import com.echothree.model.control.document.server.logic.DocumentTypeUsageTypeLogic;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.document.server.entity.DocumentType;
+import com.echothree.model.data.document.server.entity.DocumentTypeUsage;
+import com.echothree.model.data.document.server.entity.DocumentTypeUsageType;
+import com.echothree.model.data.document.server.factory.DocumentTypeUsageFactory;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
 import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BasePaginatedMultipleEntitiesCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
+import java.util.Collection;
 import java.util.List;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 @Dependent
 public class GetDocumentTypeUsagesCommand
-        extends BaseSimpleCommand<GetDocumentTypeUsagesForm> {
+        extends BasePaginatedMultipleEntitiesCommand<DocumentTypeUsage, GetDocumentTypeUsagesForm> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -59,45 +65,73 @@ public class GetDocumentTypeUsagesCommand
     @Inject
     DocumentControl documentControl;
 
-    
+    @Inject
+    DocumentTypeLogic documentTypeLogic;
+
+    @Inject
+    DocumentTypeUsageTypeLogic documentTypeUsageTypeLogic;
+
     /** Creates a new instance of GetDocumentTypeUsagesCommand */
     public GetDocumentTypeUsagesCommand() {
-        super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, false);
+        super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
-    
+
+    private DocumentTypeUsageType documentTypeUsageType;
+    private DocumentType documentType;
+
     @Override
-    protected BaseResult execute() {
-        var result = DocumentResultFactory.getGetDocumentTypeUsagesResult();
+    protected void handleForm() {
         var documentTypeUsageTypeName = form.getDocumentTypeUsageTypeName();
         var documentTypeName = form.getDocumentTypeName();
         var parameterCount = (documentTypeUsageTypeName == null ? 0 : 1) + (documentTypeName == null ? 0 : 1);
-        
+
         if(parameterCount == 1) {
-            var userVisit = getUserVisit();
-
             if(documentTypeUsageTypeName != null) {
-                var documentTypeUsageType = documentControl.getDocumentTypeUsageTypeByName(documentTypeUsageTypeName);
-
-                if(documentTypeUsageType != null) {
-                    result.setDocumentTypeUsageType(documentControl.getDocumentTypeUsageTypeTransfer(userVisit, documentTypeUsageType));
-                    result.setDocumentTypeUsages(documentControl.getDocumentTypeUsageTransfersByDocumentTypeUsageType(userVisit, documentTypeUsageType));
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownDocumentTypeUsageTypeName.name(), documentTypeUsageTypeName);
-                }
-            } else if(documentTypeName != null) {
-                var documentType = documentControl.getDocumentTypeByName(documentTypeName);
-
-                if(documentType != null) {
-                    result.setDocumentType(documentControl.getDocumentTypeTransfer(userVisit, documentType));
-                    result.setDocumentTypeUsages(documentControl.getDocumentTypeUsageTransfersByDocumentType(userVisit, documentType));
-                } else {
-                    addExecutionError(ExecutionErrors.UnknownDocumentTypeName.name(), documentTypeName);
-                }
+                documentTypeUsageType = documentTypeUsageTypeLogic.getDocumentTypeUsageTypeByName(this,
+                        documentTypeUsageTypeName);
+            } else {
+                documentType = documentTypeLogic.getDocumentTypeByName(this, documentTypeName);
             }
         } else {
             addExecutionError(ExecutionErrors.InvalidParameterCount.name());
         }
-        
+    }
+
+    @Override
+    protected Long getTotalEntities() {
+        return hasExecutionErrors() ? null : documentTypeUsageType == null
+                ? documentControl.countDocumentTypeUsagesByDocumentType(documentType)
+                : documentControl.countDocumentTypeUsagesByDocumentTypeUsageType(documentTypeUsageType);
+    }
+
+    @Override
+    protected Collection<DocumentTypeUsage> getEntities() {
+        return hasExecutionErrors() ? null : documentTypeUsageType == null
+                ? documentControl.getDocumentTypeUsagesByDocumentType(documentType)
+                : documentControl.getDocumentTypeUsagesByDocumentTypeUsageType(documentTypeUsageType);
+    }
+
+    @Override
+    protected BaseResult getResult(Collection<DocumentTypeUsage> entities) {
+        var result = DocumentResultFactory.getGetDocumentTypeUsagesResult();
+
+        if(entities != null) {
+            var userVisit = getUserVisit();
+
+            if(documentTypeUsageType == null) {
+                result.setDocumentType(documentControl.getDocumentTypeTransfer(userVisit, documentType));
+            } else {
+                result.setDocumentTypeUsageType(documentControl.getDocumentTypeUsageTypeTransfer(userVisit,
+                        documentTypeUsageType));
+            }
+
+            if(session.hasLimit(DocumentTypeUsageFactory.class)) {
+                result.setDocumentTypeUsageCount(getTotalEntities());
+            }
+
+            result.setDocumentTypeUsages(documentControl.getDocumentTypeUsageTransfers(userVisit, entities));
+        }
+
         return result;
     }
     
