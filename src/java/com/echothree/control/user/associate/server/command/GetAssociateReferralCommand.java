fe92fa6b9bdf987a1ad5d26d81369a71a1cd3120
Richard Harms <rich@echothree.com>
2026-08-11 20:29:37 -0500

    #2213: Converted GetAssociateReferral UC to extend BaseSingleEntityCommand

--- src/java/com/echothree/control/user/associate/server/command/GetAssociateReferralCommand.java
+++ src/java/com/echothree/control/user/associate/server/command/GetAssociateReferralCommand.java
@@ -19,49 +19,62 @@ package com.echothree.control.user.associate.server.command;
 import com.echothree.control.user.associate.common.form.GetAssociateReferralForm;
 import com.echothree.control.user.associate.common.result.AssociateResultFactory;
 import com.echothree.model.control.associate.server.control.AssociateControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.message.ExecutionErrors;
+import com.echothree.model.control.associate.server.logic.AssociateReferralLogic;
+import com.echothree.model.control.core.common.EventTypes;
+import com.echothree.model.data.associate.server.entity.AssociateReferral;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
 import java.util.List;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 @Dependent
 public class GetAssociateReferralCommand
-        extends BaseSimpleCommand<GetAssociateReferralForm> {
+        extends BaseSingleEntityCommand<AssociateReferral, GetAssociateReferralForm> {
     
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
     
     static {
         FORM_FIELD_DEFINITIONS = List.of(
-                new FieldDefinition("AssociateReferralName", FieldType.ENTITY_NAME, true, null, null)
+                new FieldDefinition("AssociateReferralName", FieldType.ENTITY_NAME, false, null, null),
+                new FieldDefinition("EntityRef", FieldType.ENTITY_REF, false, null, null),
+                new FieldDefinition("Uuid", FieldType.UUID, false, null, null)
         );
     }
 
     @Inject
     AssociateControl associateControl;
 
-    
+    @Inject
+    AssociateReferralLogic associateReferralLogic;
+
     /** Creates a new instance of GetAssociateReferralCommand */
     public GetAssociateReferralCommand() {
         super(null, FORM_FIELD_DEFINITIONS, true);
     }
     
     @Override
-    protected BaseResult execute() {
-        var result = AssociateResultFactory.getGetAssociateReferralResult();
-        var associateReferralName = form.getAssociateReferralName();
-        var associateReferral = associateControl.getAssociateReferralByName(associateReferralName);
-        
+    protected AssociateReferral getEntity() {
+        var associateReferral = associateReferralLogic.getAssociateReferralByUniversalSpec(this, form);
+
         if(associateReferral != null) {
-            result.setAssociateReferral(associateControl.getAssociateReferralTransfer(getUserVisit(), associateReferral));
-        } else {
-            addExecutionError(ExecutionErrors.UnknownAssociateReferralName.name(), associateReferralName);
+            sendEvent(associateReferral.getPrimaryKey(), EventTypes.READ, null, null, getPartyPK());
         }
-        
+
+        return associateReferral;
+    }
+
+    @Override
+    protected BaseResult getResult(final AssociateReferral associateReferral) {
+        var result = AssociateResultFactory.getGetAssociateReferralResult();
+
+        if(associateReferral != null) {
+            result.setAssociateReferral(associateControl.getAssociateReferralTransfer(getUserVisit(),
+                    associateReferral));
+        }
+
         return result;
     }
     
