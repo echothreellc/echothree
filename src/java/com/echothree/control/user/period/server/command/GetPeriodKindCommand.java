345866919eeba70d4bf01c0c459e0065a76756f4
Richard Harms <rich@echothree.com>
2026-09-05 09:25:36 -0500

    #2271: Converted GetPeriodKind UC to extend BaseSingleEntityCommand

--- src/java/com/echothree/control/user/period/server/command/GetPeriodKindCommand.java
+++ src/java/com/echothree/control/user/period/server/command/GetPeriodKindCommand.java
@@ -21,14 +21,14 @@ import com.echothree.control.user.period.common.result.PeriodResultFactory;
 import com.echothree.model.control.core.common.EventTypes;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.period.server.control.PeriodControl;
+import com.echothree.model.control.period.server.logic.PeriodKindLogic;
 import com.echothree.model.control.security.common.SecurityRoleGroups;
 import com.echothree.model.control.security.common.SecurityRoles;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
-import com.echothree.util.common.message.ExecutionErrors;
+import com.echothree.model.data.period.server.entity.PeriodKind;
+import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.common.command.BaseResult;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
 import com.echothree.util.server.control.CommandSecurityDefinition;
 import com.echothree.util.server.control.PartyTypeDefinition;
 import com.echothree.util.server.control.SecurityRoleDefinition;
@@ -38,7 +38,7 @@ import javax.inject.Inject;
 
 @Dependent
 public class GetPeriodKindCommand
-        extends BaseSimpleCommand<GetPeriodKindForm> {
+        extends BaseSingleEntityCommand<PeriodKind, GetPeriodKindForm> {
     
     private final static CommandSecurityDefinition COMMAND_SECURITY_DEFINITION;
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
@@ -52,31 +52,40 @@ public class GetPeriodKindCommand
         ));
         
         FORM_FIELD_DEFINITIONS = List.of(
-                new FieldDefinition("PeriodKindName", FieldType.ENTITY_NAME, true, null, null)
+                new FieldDefinition("PeriodKindName", FieldType.ENTITY_NAME, false, null, null),
+                new FieldDefinition("EntityRef", FieldType.ENTITY_REF, false, null, null),
+                new FieldDefinition("Uuid", FieldType.UUID, false, null, null)
         );
     }
 
     @Inject
     PeriodControl periodControl;
 
-    
+    @Inject
+    PeriodKindLogic periodKindLogic;
+
     /** Creates a new instance of GetPeriodKindCommand */
     public GetPeriodKindCommand() {
         super(COMMAND_SECURITY_DEFINITION, FORM_FIELD_DEFINITIONS, true);
     }
     
     @Override
-    protected BaseResult execute() {
-        var result = PeriodResultFactory.getGetPeriodKindResult();
-        var periodKindName = form.getPeriodKindName();
-        var periodKind = periodControl.getPeriodKindByName(periodKindName);
+    protected PeriodKind getEntity() {
+        var periodKind = periodKindLogic.getPeriodKindByUniversalSpec(this, form, true);
         
         if(periodKind != null) {
-            result.setPeriodKind(periodControl.getPeriodKindTransfer(getUserVisit(), periodKind));
-            
             sendEvent(periodKind.getPrimaryKey(), EventTypes.READ, null, null, getPartyPK());
-        } else {
-            addExecutionError(ExecutionErrors.UnknownPeriodKindName.name(), periodKindName);
+        }
+
+        return periodKind;
+    }
+
+    @Override
+    protected BaseResult getResult(PeriodKind periodKind) {
+        var result = PeriodResultFactory.getGetPeriodKindResult();
+
+        if(periodKind != null) {
+            result.setPeriodKind(periodControl.getPeriodKindTransfer(getUserVisit(), periodKind));
         }
         
         return result;
