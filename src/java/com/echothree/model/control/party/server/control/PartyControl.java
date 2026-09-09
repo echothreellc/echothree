08796225c676b5ac01403102b97e7d7653b0ccea
Richard Harms <rich@echothree.com>
2026-09-08 19:48:09 -0500

    #2187: Call deleteInventoryTransactionRolesByParty(...) when deleteParty(...) is called

--- src/java/com/echothree/model/control/party/server/control/PartyControl.java
+++ src/java/com/echothree/model/control/party/server/control/PartyControl.java
@@ -25,6 +25,7 @@ import com.echothree.model.control.document.server.control.DocumentControl;
 import com.echothree.model.control.employee.server.control.EmployeeControl;
 import com.echothree.model.control.inventory.server.control.BucketControl;
 import com.echothree.model.control.inventory.server.control.InventoryLocationControl;
+import com.echothree.model.control.inventory.server.control.InventoryTransactionRoleControl;
 import com.echothree.model.control.party.common.PartyTypes;
 import com.echothree.model.control.party.common.choice.BirthdayFormatChoicesBean;
 import com.echothree.model.control.party.common.choice.CompanyChoicesBean;
@@ -247,6 +248,9 @@ public class PartyControl
 
     @Inject
     protected InventoryLocationControl inventoryLocationControl;
+
+    @Inject
+    protected InventoryTransactionRoleControl inventoryTransactionRoleControl;
 
     @Inject
     protected PartyApplicationEditorUseControl partyApplicationEditorUseControl;
@@ -2448,6 +2452,7 @@ public class PartyControl
         contactListControl.deletePartyContactListsByParty(party, deletedBy);
         documentControl.deletePartyDocumentsByParty(party, deletedBy);
         inventoryLocationControl.deleteInventoryLocationsByOwnerParty(party, deletedBy);
+        inventoryTransactionRoleControl.deleteInventoryTransactionRolesByParty(party, deletedBy);
         printerControl.deletePartyPrinterGroupUsesByParty(party, deletedBy);
         scaleControl.deletePartyScaleUsesByParty(party, deletedBy);
         searchControl.deletePartySearchTypePreferencesByParty(party, deletedBy);
