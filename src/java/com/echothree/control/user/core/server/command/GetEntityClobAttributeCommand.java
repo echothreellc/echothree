017370a7d890a31174946df0dd8be396beb6fe5a
Richard Harms <rich@echothree.com>
2026-09-13 14:56:37 -0500

    #2239: Converted GetEntityClobAttribute UC to extend BaseSingleEntityCommand

--- src/java/com/echothree/control/user/core/server/command/GetEntityClobAttributeCommand.java
+++ src/java/com/echothree/control/user/core/server/command/GetEntityClobAttributeCommand.java
@@ -19,20 +19,21 @@ package com.echothree.control.user.core.server.command;
 import com.echothree.control.user.core.common.form.GetEntityClobAttributeForm;
 import com.echothree.control.user.core.common.result.CoreResultFactory;
 import com.echothree.model.control.core.server.control.EntityInstanceControl;
+import com.echothree.model.control.core.server.logic.EntityAttributeLogic;
 import com.echothree.model.control.party.server.control.PartyControl;
-import com.echothree.model.data.user.common.pk.UserVisitPK;
+import com.echothree.model.data.core.server.entity.EntityClobAttribute;
 import com.echothree.util.common.command.BaseResult;
 import com.echothree.util.common.message.ExecutionErrors;
 import com.echothree.util.common.validation.FieldDefinition;
 import com.echothree.util.common.validation.FieldType;
-import com.echothree.util.server.control.BaseSimpleCommand;
+import com.echothree.util.server.control.BaseSingleEntityCommand;
 import java.util.List;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 @Dependent
 public class GetEntityClobAttributeCommand
-        extends BaseSimpleCommand<GetEntityClobAttributeForm> {
+        extends BaseSingleEntityCommand<EntityClobAttribute, GetEntityClobAttributeForm> {
     
     private final static List<FieldDefinition> FORM_FIELD_DEFINITIONS;
     
@@ -50,33 +51,33 @@ public class GetEntityClobAttributeCommand
     @Inject
     PartyControl partyControl;
 
-    
+    @Inject
+    EntityAttributeLogic entityAttributeLogic;
+
     /** Creates a new instance of GetEntityClobAttributeCommand */
     public GetEntityClobAttributeCommand() {
         super(null, FORM_FIELD_DEFINITIONS, false);
     }
     
     @Override
-    protected BaseResult execute() {
-        var result = CoreResultFactory.getGetEntityClobAttributeResult();
+    protected EntityClobAttribute getEntity() {
+        EntityClobAttribute entityClobAttribute = null;
         var entityRef = form.getEntityRef();
         var entityInstance = entityInstanceControl.getEntityInstanceByEntityRef(entityRef);
         
         if(entityInstance != null) {
             var entityAttributeName = form.getEntityAttributeName();
-            var entityAttribute = coreControl.getEntityAttributeByName(entityInstance.getEntityType(), entityAttributeName);
+            var entityAttribute = entityAttributeLogic.getEntityAttributeByName(this, entityInstance.getEntityType(), entityAttributeName);
             
-            if(entityAttribute != null) {
+            if(!hasExecutionErrors()) {
                 var languageIsoName = form.getLanguageIsoName();
                 var language = languageIsoName == null ? null : partyControl.getLanguageByIsoName(languageIsoName);
                 
-                if(language != null) {
-                    var entityClobAttribute = language == null ? coreControl.getBestEntityClobAttribute(entityAttribute, entityInstance, getPreferredLanguage())
+                if(languageIsoName == null || language != null) {
+                    entityClobAttribute = language == null ? coreControl.getBestEntityClobAttribute(entityAttribute, entityInstance, getPreferredLanguage())
                             : coreControl.getEntityClobAttribute(entityAttribute, entityInstance, language);
                     
-                    if(entityClobAttribute != null) {
-                        result.setEntityClobAttribute(coreControl.getEntityClobAttributeTransfer(getUserVisit(), entityClobAttribute, entityInstance));
-                    } else {
+                    if(entityClobAttribute == null) {
                         var entityTypeDetail = entityInstance.getEntityType().getLastDetail();
 
                         addExecutionError(ExecutionErrors.UnknownEntityClobAttribute.name(), entityRef,
@@ -86,16 +87,23 @@ public class GetEntityClobAttributeCommand
                 } else {
                     addExecutionError(ExecutionErrors.UnknownLanguageIsoName.name(), languageIsoName);
                 }
-            } else {
-                var entityTypeDetail = entityInstance.getEntityType().getLastDetail();
-                
-                addExecutionError(ExecutionErrors.UnknownEntityAttributeName.name(), entityTypeDetail.getComponentVendor().getLastDetail().getComponentVendorName(),
-                        entityTypeDetail.getEntityTypeName(), entityAttributeName);
             }
         } else {
             addExecutionError(ExecutionErrors.UnknownEntityRef.name(), entityRef);
         }
         
+        return entityClobAttribute;
+    }
+
+    @Override
+    protected BaseResult getResult(EntityClobAttribute entityClobAttribute) {
+        var result = CoreResultFactory.getGetEntityClobAttributeResult();
+
+        if(entityClobAttribute != null) {
+            result.setEntityClobAttribute(coreControl.getEntityClobAttributeTransfer(getUserVisit(), entityClobAttribute,
+                    entityClobAttribute.getEntityInstance()));
+        }
+
         return result;
     }
     
